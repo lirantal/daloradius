3aed3bee3ec55c7986e527df4b3e5ee1e55460d6
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-06-19 20:20:52 +0000

    removed radpostauth section from management which is already implemented in index page and should be available in reports page anyway.

--- trunk/mng-main.php
+++ trunk/mng-main.php
@@ -54,7 +54,6 @@
 						<li><a href="mng-rad-usergroup.php"><em>U</em>ser-group Mapping</a></li>
 						<li><a href="mng-rad-groupreply.php"><em>G</em>roup-reply</a></li>
 						<li><a href="mng-rad-groupcheck.php"><em>G</em>roup-check</a></li>
-						<li><a href="mng-rad-postauth.php"><em>P</em>ostauth</a></li>
 						<li><a href="mng-rad-operators.php"><em>O</em>perators</a></li>
 
 						<li>Welcome, <?php echo $operator; ?></li>
