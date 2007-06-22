9e67919cc84e45c2ed9d17628ef477d010717b06
Liran Tal <liran.tal@gmail.com>
2007-06-22 14:27:13 +0000

    fixed bug where the page would fail loading because it required the old php date library which we removed

--- gis-viewmap.php
+++ gis-viewmap.php
@@ -3,22 +3,6 @@
     include ("library/checklogin.php");
     $operator = $_SESSION['operator_user'];
 
-    require "library/datepicker/class.datepicker.php";
-
-    // instantiate the object
-    $db=new datepicker();
-
-    // set the selectable range
-    // users will not be able to select dates outside those ranges
-
-    // set the first day of week to Monday
-    // users from the United States will set this to 0 (Sunday)
-    $db->firstDayOfWeek = 0;
-
-    // set the format in which the date to be returned
-    $db->dateFormat = "Y-m-d";
-
-
 ?>
 
 
