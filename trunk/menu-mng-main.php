33bb3630266adcc0079d89367fa997a8e8c63c93
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-06-23 15:49:01 +0000

    added the new subnav links

--- trunk/menu-mng-main.php
+++ trunk/menu-mng-main.php
@@ -36,6 +36,11 @@
 				
 				</ul>
 				<ul id="subnav">
+						<li><a href="mng-rad-nas.php"><em>N</em>as</a></li>
+                        <li><a href="mng-rad-usergroup.php"><em>U</em>ser-group Mapping</a></li>
+                        <li><a href="mng-rad-groupreply.php"><em>G</em>roup-reply</a></li>
+						<li><a href="mng-rad-groupcheck.php"><em>G</em>roup-check</a></li>
+                        <li><a href="mng-rad-operators.php"><em>O</em>perators</a></li>
 				
 						<li>Welcome, <?php echo $operator; ?></li>
 
