07a5cda4c2c8247adf12c2b14b9be1a93813ed3a
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-06-23 15:54:14 +0000

    altered radius nas management (subnav) files so that they make use of the menu header

--- trunk/mng-rad-nas.php
+++ trunk/mng-rad-nas.php
@@ -13,81 +13,9 @@
 
 </head>
  
-<body>
-
-<div id="wrapper">
-<div id="innerwrapper">
-
-		<div id="header">
-		
-				<form action="">
-				<input value="Search" />
-				</form>
-				
-				<h1><a href="index.php">daloRADIUS</a></h1>
-				
-				<h2>
-				
-						Radius Reporting, Billing and Management by <a href="http://www.enginx.com">Enginx</a>
-				
-				</h2>
-				
-				<ul id="nav">
-				
-						<li><a href="index.php"><em>H</em>ome</a></li>
-						
-						<li><a href="mng-main.php" class="active"><em>M</em>anagment</a></li>
-						
-						<li><a href="rep-main.php"><em>R</em>eports</a></li>
-						
-						<li><a href="acct-main.php"><em>A</em>ccounting</a></li>
-
-						<li><a href="bill-main.php"><em>B</em>illing</a></li>
-						<li><a href="gis-main.php"><em>GIS</em></a></li>
-						<li><a href="graph-main.php"><em>G</em>raphs</a></li>
-
-						<li><a href="help-main.php"><em>H</em>elp</a></li>
-				
-				</ul>
-				<ul id="subnav">
-						<li><a href="mng-rad-nas.php" class="active"><em>N</em>as</a></li>
-						<li><a href="mng-rad-usergroup.php"><em>U</em>ser-group Mapping</a></li>
-						<li><a href="mng-rad-groupreply.php"><em>G</em>roup-reply</a></li>
-						<li><a href="mng-rad-groupcheck.php"><em>G</em>roup-check</a></li>
-						<li><a href="mng-rad-postauth.php"><em>P</em>ostauth</a></li>
-						<li><a href="mng-rad-operators.php"><em>O</em>perators</a></li>
-
-						<li>Welcome, <?php echo $operator; ?></li>
-
-						<li><a href="logout.php">[logout]</a></li>
-				
-				</ul>
-		
-		</div>
-		
-		<div id="sidebar">
-		
-				<h2>Management</h2>
-				
-				<h3>NAS Management</h3>
-				<ul class="subnav">
-				
-						<li><a href="mng-rad-nas-list.php"><b>&raquo;</b>List NAS</a></li>
-						<li><a href="mng-rad-nas-new.php"><b>&raquo;</b>New NAS</a></li>
-						<li><a href="javascript:document.mngradnasedit.submit();""><b>&raquo;</b>Edit NAS<a>
-							<form name="mngradnasedit" action="mng-rad-nas-edit.php" method="get" class="sidebar">
-							<input name="nashost" type="text">
-							</form></li>
-
-
-						<li><a href="mng-rad-nas-del.php"><b>&raquo;</b>Remove NAS</a></li>	
-				</ul>
-		
-	
-		</div>
-		
-		
-		
+<?php
+	include ("menu-mng-rad-nas.php");
+?>
 		<div id="contentnorightbar">
 		
 				<h2 id="Intro"><a href="#">NAS Configuration</a></h2>
