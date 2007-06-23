20bebffc9eaadd356ed05fe3b57996a7f2d11fae
Liran Tal <liran.tal@gmail.com>
2007-06-23 19:49:34 +0000

    adding new subnav to management page for managing radgroupreply table (this is still incomplete)

---
+++ mng-rad-groupreply.php
@@ -0,0 +1,48 @@
+<?php
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+
+</head>
+ 
+ 
+<?php
+	include ("menu-mng-rad-groupreply.php");
+?>
+
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#">User-Group Configuration</a></h2>
+				
+				<p>
+	<table><center><br/>
+	<img src="library/exten-get_total_users.php" />
+	</table></center>
+
+				</p>
+				
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
