7d132d6306ced9ce078b3a55de5ae293e93fa9b1
ugenk <ugenk@xdsl.by>
2007-06-24 10:38:20 +0000

    localization data, test version

---
+++ lang/en.php
@@ -0,0 +1,41 @@
+<?
+$l[all][ID] = "ID";
+$l[all][HotSpot] = "HotSpot";
+$l[all][Username] = "Username";
+$l[all][Password] = "Password";
+$l[all][IPAddress] = "IP Address";
+$l[all][Attribute] = "Attribute";
+$l[all][MaxTimeExpiration] = "Max Time / Expiration";
+$l[all][UsedTime] = "Used Time";
+$l[all][Status] = "Status";
+$l[all][Usage] = "Usage";
+$l[all][StartTime] = "Start Time";
+$l[all][StopTime] = "Stop Time";
+$l[all][TotalTime] = "Total Time";
+$l[all][Bytes] = "Bytes";
+$l[all][Upload] = "Upload";
+$l[all][Download] = "Download";
+$l[all][Termination] = "Termination";
+$l[all][NASIPAddress] = "NAS IP Address";
+$l[all][Action] = "Action";
+$l[all][UniqueUsers] = "Unique Users";
+$l[all][TotalHits] = "Total Hits";
+$l[all][AverageTime] = "Average Time";
+$l[all][Records] = "Records";
+$l[all][Statistics] = "Statistics";
+$l[all][Credit] = "Credit";
+$l[all][Used] = "Used";
+$l[all][LeftTime] = "Left *T";
+$l[all][LeftPercent] = "Left *%";
+$l[all][TotalSessions] = "Total Sessions";
+
+$l[Intro][acctactive.php] = "Active Records Accounting";
+$l[Intro][acctall.php] = "Users Accounting";
+$l[Intro][acctdate.php] = "Users Accounting";
+$l[Intro][accthotspot.php] = "Hotspot Accounting";
+$l[Intro][acctipaddress.php] = "Users Accounting";
+$l[Intro][accthotspotcompare.php] = "Hotspot Comparison";
+$l[Intro][acctmain.php] = "Accounting Page";
+$l[Intro][acctnasipaddress.php] = "Users Accounting";
+$l[Intro][acctusername.php] = "Users Accounting";
+?>
