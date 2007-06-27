60af812babd11f7e95f4a6ccb961918f75f427fb
Liran Tal <liran.tal@gmail.com>
2007-06-27 20:44:17 +0000

    adding new files related to the new configuration page of daloradius (this is still in beta, please avoid using it for now)

---
+++ config-db.php
@@ -0,0 +1,42 @@
+<?php
+
+    include ("menu-config.php");
+	include ("library/config_read.php");
+	
+?>		
+		
+		
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#">Database Configuration</a></h2>
+				<p>
+				Below are the settings that daloRADIUS will make use of to connect to your
+				MySQL database server and manage it.
+
+				<br/><br/>
+<?php
+
+	echo $configValues['CONFIG_DB_NAME'];
+	echo "<br/>";
+	echo $configValues['CONFIG_LANG'];
+	
+?>				
+				<br/><br/>
+				
+				</p>
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
---
+++ config_main.php
@@ -0,0 +1,33 @@
+<?php
+
+    include ("menu-config.php");
+	
+?>
+		
+		
+		
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#"></a></h2>
+				<p>
+
+<?php
+//	include 'library/exten-welcome_page.php';
+?>
+				</p>
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
