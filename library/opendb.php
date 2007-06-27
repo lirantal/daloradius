a6e9b3320d878c10c8ab9c74e4d1a144a9d0bcee
Liran Tal <liran.tal@gmail.com>
2007-06-27 20:55:14 +0000

    fix for config file

--- library/opendb.php
+++ library/opendb.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysql_connect($config_db_host, $config_db_user, $config_db_pass) or die ('Error connecting to mysql');
+$conn = mysql_connect($config_db, $config_db_user, $config_db_pass) or die ('Error connecting to mysql');
 mysql_select_db($config_db_name);
 ?>
