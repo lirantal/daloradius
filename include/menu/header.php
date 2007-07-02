9cf71d05cb4867ad1e5ad179cbbfc55f191d45b6
ugenk <ugenk@xdsl.by>
2007-07-02 07:40:53 +0000

    header for menu files

---
+++ include/menu/header.php
@@ -0,0 +1,42 @@
+                <div id="header">
+
+                                <form action="">
+                                <input value="Search" />
+                                </form>
+
+                                <h1><a href="index.php"><? echo $l[all][daloRADIUS]; ?></a></h1>
+
+                                <h2>
+                                
+                                <? echo $l[all][copyright1]; ?>
+                                
+				                                </h2>
+
+                                <ul id="nav">
+
+                                                <li><a href="index.php"><em>H</em>ome</a></li>
+
+                                                <li><a href="mng-main.php"><em>M</em>anagment</a></li>
+
+                                                <li><a href="rep-main.php"><em>R</em>eports</a></li>
+
+                                                <li><a href="acct-main.php" class="active"><em>A</em>ccounting</a></li>
+
+                                                <li><a href="bill-main.php"><em>B</em>illing</a></li>
+                                                <li><a href="gis-main.php"><em>GIS</em></a></li>
+
+                                                <li><a href="graph-main.php"><em>G</em>raphs</a></li>
+
+                                                <li><a href="help-main.php"><em>H</em>elp</a></li>
+
+                                </ul>
+                                <ul id="subnav">
+
+                                                <li>Welcome, <?php echo $operator; ?></li>
+
+                                                <li><a href="logout.php">[logout]</a></li>
+
+                                </ul>
+
+                </div>
+
