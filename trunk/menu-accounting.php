9f5c6c6843c37a9c6b21bf7c3ecde3cbc4629d54
ugenk <ugenk@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-02 07:40:35 +0000

    modularized

--- trunk/menu-accounting.php
+++ trunk/menu-accounting.php
@@ -23,48 +23,9 @@
 <div id="wrapper">
 <div id="innerwrapper">
 
-		<div id="header">
-		
-				<form action="">
-				<input value="Search" />
-				</form>
-				
-				<h1><a href="index.php"><? echo $l[all][daloRADIUS]; ?></a></h1>
-				
-				<h2>
-				
-				<? echo $l[all][copyright1]; ?>
-				
-				</h2>
-				
-				<ul id="nav">
-				
-						<li><a href="index.php"><em>H</em>ome</a></li>
-						
-						<li><a href="mng-main.php"><em>M</em>anagment</a></li>
-						
-						<li><a href="rep-main.php"><em>R</em>eports</a></li>
-						
-						<li><a href="acct-main.php" class="active"><em>A</em>ccounting</a></li>
-
-						<li><a href="bill-main.php"><em>B</em>illing</a></li>
-                                                <li><a href="gis-main.php"><em>GIS</em></a></li>
-						
-						<li><a href="graph-main.php"><em>G</em>raphs</a></li>
-
-						<li><a href="help-main.php"><em>H</em>elp</a></li>
-				
-				</ul>
-				<ul id="subnav">
-				
-						<li>Welcome, <?php echo $operator; ?></li>
-
-						<li><a href="logout.php">[logout]</a></li>
-				
-				</ul>
-		
-		</div>
-		
+<?php
+    include_once ("include/menu/header.php");
+?>	
 		<div id="sidebar">
 		
 				<h2>Accounting</h2>
--- trunk/menu-gis.php
+++ trunk/menu-gis.php
@@ -16,48 +16,10 @@
 <div id="wrapper">
 <div id="innerwrapper">
 
-		<div id="header">
-		
-				<form action="">
-				<input value="Search" />
-				</form>
-				
-				<h1><a href="index.php">daloRADIUS</a></h1>
-				
-				<h2>
-				
-						Radius Reporting, Billing and Management by <a href="http://www.enginx.com">Enginx</a>
-				
-				</h2>
-				
-				<ul id="nav">
-				
-						<li><a href="index.php"><em>H</em>ome</a></li>
-						
-						<li><a href="mng-main.php"><em>M</em>anagment</a></li>
-						
-						<li><a href="rep-main.php"><em>R</em>eports</a></li>
-						
-						<li><a href="acct-main.php"><em>A</em>ccounting</a></li>
+<?php
+	    include_once ("include/menu/header.php");
+?>      
 
-						<li><a href="bill-main.php"><em>B</em>illing</a></li>
-						
-						<li><a href="gis-main.php" class="active"><em>GIS</em></a></li>
-						<li><a href="graph-main.php"><em>G</em>raphs</a></li>
-
-						<li><a href="help-main.php"><em>H</em>elp</a></li>
-				
-				</ul>
-				<ul id="subnav">
-				
-						<li>Welcome, <?php echo $operator; ?></li>
-
-						<li><a href="logout.php">[logout]</a></li>
-				
-				</ul>
-		
-		</div>
-		
 		<div id="sidebar">
 		
 				<h2>GIS</h2>
