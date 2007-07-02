44b55f533d78b3f1f578f295a963912909a9e78b
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-02 19:33:05 +0000

    initial libchart library import to repository

---
+++ trunk/library/libchart/libchart.php
@@ -0,0 +1,33 @@
+<?
+	/** Libchart - PHP chart library
+	*	
+	* Copyright (C) 2005-2006 Jean Marc Trémeaux (jm.tremeaux at gmail.com)
+	* 	
+	* This library is free software; you can redistribute it and/or
+	* modify it under the terms of the GNU Lesser General Public
+	* License as published by the Free Software Foundation; either
+	* version 2.1 of the License, or (at your option) any later version.
+	* 
+	* This library is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	* Lesser General Public License for more details.
+	* 
+	* You should have received a copy of the GNU Lesser General Public
+	* License along with this library; if not, write to the Free Software
+	* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	* 
+	*/
+
+	require_once 'classes/Point.php';
+	require_once 'classes/Axis.php';
+	require_once 'classes/Color.php';
+	require_once 'classes/Primitive.php';
+	require_once 'classes/Text.php';
+	require_once 'classes/Chart.php';
+	require_once 'classes/PieChart.php';
+	require_once 'classes/BarChart.php';
+	require_once 'classes/LineChart.php';
+	require_once 'classes/VerticalChart.php';
+	require_once 'classes/HorizontalChart.php';
+?>
