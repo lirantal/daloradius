e7eb5bd7bc94cc859ef387a03a64d9ea31aaf537
Liran Tal <liran.tal@gmail.com>
2007-07-11 18:16:51 +0000

    changed formatting to use table for form fields and also removed un-required javascript showtogglediv

--- mng-rad-groupreply-del.php
+++ mng-rad-groupreply-del.php
@@ -54,40 +54,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-
-<SCRIPT TYPE="text/javascript">
-<!--
-function toggleShowDiv(pass) {
-
-        var divs = document.getElementsByTagName('div');
-        for(i=0;i<divs.length;i++) {
-                if (divs[i].id.match(pass)) {
-                        if (document.getElementById) {                                         
-                                if (divs[i].style.display=="inline")
-                                        divs[i].style.display="none";
-                                else
-                                        divs[i].style.display="inline";
-                        } else if (document.layers) {                                           
-                                if (document.layers[divs[i]].display=='visible')
-                                        document.layers[divs[i]].display = 'hidden';
-                                else
-                                        document.layers[divs[i]].display = 'visible';
-                        } else {
-                                if (document.all.hideShow.divs[i].visibility=='visible')     
-                                        document.all.hideShow.divs[i].visibility = 'hidden';
-                                else
-                                        document.all.hideShow.divs[i].visibility = 'visible';
-                        }
-                }
-        }
-}
-
-
-
-// -->
-</script>
-
-
 <title>daloRADIUS</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
@@ -108,20 +74,27 @@ function toggleShowDiv(pass) {
                                 <br/><br/>
                                 </p>
                                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-
+<table border='2' class='table1'>
+<tr><td>
                                                 <?php if (trim($groupname) == "") { echo "<font color='#FF0000'>";  }?>
                                                 <b>Groupname</b>
+</td><td>												
                                                 <input value="<?php echo $groupname ?>" name="groupname"/><br/>
                                                 </font>
-
+</td></tr>
+<tr><td>
                                                 <?php if (trim($value) == "") { echo "<font color='#FF0000'>";  }?>
                                                 <b>Value</b>
+</td><td>												
                                                 <input value="<?php echo $value ?>" name="value"/><br/>
 						If you specify value then only the single record that matches both the groupname and the specific value which you have specified will be removed. If you omit the value then all records for that particular groupname will be removed!
                                                 </font>
-
+</td></tr>
+</table>
                                                 <br/><br/>
+<center>												
                                                 <input type="submit" name="submit" value="Apply"/>
+</center>												
 
                                 </form>
 				
