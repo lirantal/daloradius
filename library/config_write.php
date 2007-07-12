9a1ded56a05d36fe7d6c43192560d69a510370de
Liran Tal <liran.tal@gmail.com>
2007-07-12 10:26:49 +0000

    added file permissios check procedure

--- library/config_write.php
+++ library/config_write.php
@@ -7,17 +7,18 @@ $fp = fopen($configFile, "w");
 if ($fp) {
 	foreach ($configValues as $option => $elem) {
         fwrite($fp, $option . " = " . $configValues[$option] . "\n");
-	} else {
-            echo "<font color='#FF0000'>error: could not open the file for writing:<b> $configFile </b><br/></font>";
-			echo "Check file permissions. The file should be writable by the webserver's user/group<br/>";
-            echo "
-                <script language='JavaScript'>
-                <!--
-                alert('could not open the file <b> $configFile </b> for writing!\\nCheck file permissions.');
-                -->
-                </script>
-			";
 	}
+} else {
+        echo "<font color='#FF0000'>error: could not open the file for writing:<b> $configFile </b><br/></font>";
+		echo "Check file permissions. The file should be writable by the webserver's user/group<br/>";
+        echo "
+            <script language='JavaScript'>
+            <!--
+            alert('could not open the file <b> $configFile </b> for writing!\\nCheck file permissions.');
+            -->
+            </script>
+		";
+}
 
 fclose($fp);
 
