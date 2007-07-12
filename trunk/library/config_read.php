daec683cec4624575fa78c2b48af4eae5333d917
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-12 10:22:32 +0000

    added file permissios check procedure

--- trunk/library/config_read.php
+++ trunk/library/config_read.php
@@ -4,17 +4,28 @@ $configFile = "library/daloradius.conf";
 $commentChar = "#";
 
 $fp = fopen($configFile, "r");
-
-while (!feof($fp)) {
-  $line = trim(fgets($fp));
-  if ($line && !ereg("^$commentChar", $line)) {
-    $pieces = explode("=", $line);
-    $option = trim($pieces[0]);
-    $value = trim($pieces[1]);
-    $configValues[$option] = $value;
-  }
-}
-
+if ($fp)) {
+			while (!feof($fp)) {
+				$line = trim(fgets($fp));
+				if ($line && !ereg("^$commentChar", $line)) {
+					$pieces = explode("=", $line);
+					$option = trim($pieces[0]);
+					$value = trim($pieces[1]);
+					$configValues[$option] = $value;
+				}
+			}
+	} else {
+            echo "<font color='#FF0000'>error: could not open the file for reading:<b> $configFile </b><br/></font>";
+			echo "Check file permissions. The file should be readable by the webserver's user/group<br/>";
+                        echo "
+                                <script language='JavaScript'>
+                                <!--
+                                alert('could not open the file <b> $configFile </b> for reading!\\nCheck file permissions.');
+                                -->
+                                </script>
+                                ";
+	}
+	
 fclose($fp);
 
 ?>
