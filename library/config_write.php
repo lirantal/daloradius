7fa9145ace20277e1cfca0512aa751a05057abdc
Liran Tal <liran.tal@gmail.com>
2007-07-12 10:28:41 +0000

    fixed a small bug in fclose() function being outside the ifs.

--- library/config_write.php
+++ library/config_write.php
@@ -8,20 +8,17 @@ if ($fp) {
 	foreach ($configValues as $option => $elem) {
         fwrite($fp, $option . " = " . $configValues[$option] . "\n");
 	}
+	fclose($fp);
 } else {
         echo "<font color='#FF0000'>error: could not open the file for writing:<b> $configFile </b><br/></font>";
 		echo "Check file permissions. The file should be writable by the webserver's user/group<br/>";
         echo "
             <script language='JavaScript'>
             <!--
-            alert('could not open the file <b> $configFile </b> for writing!\\nCheck file permissions.');
+            alert('could not open the file $configFile for writing!\\nCheck file permissions.');
             -->
             </script>
 		";
 }
-
-fclose($fp);
-
-
 
 ?>
