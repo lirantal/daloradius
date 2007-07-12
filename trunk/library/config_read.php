01851f0649f011816694345848a76c6c1088c24b
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-12 10:26:49 +0000

    added file permissios check procedure

--- trunk/library/config_read.php
+++ trunk/library/config_read.php
@@ -5,26 +5,26 @@ $commentChar = "#";
 
 $fp = fopen($configFile, "r");
 if ($fp) {
-			while (!feof($fp)) {
-				$line = trim(fgets($fp));
-				if ($line && !ereg("^$commentChar", $line)) {
-					$pieces = explode("=", $line);
-					$option = trim($pieces[0]);
-					$value = trim($pieces[1]);
-					$configValues[$option] = $value;
-				}
-			}
-	} else {
-            echo "<font color='#FF0000'>error: could not open the file for reading:<b> $configFile </b><br/></font>";
-			echo "Check file permissions. The file should be readable by the webserver's user/group<br/>";
-                        echo "
-                                <script language='JavaScript'>
-                                <!--
-                                alert('could not open the file <b> $configFile </b> for reading!\\nCheck file permissions.');
-                                -->
-                                </script>
-                                ";
+	while (!feof($fp)) {
+		$line = trim(fgets($fp));
+		if ($line && !ereg("^$commentChar", $line)) {
+			$pieces = explode("=", $line);
+			$option = trim($pieces[0]);
+			$value = trim($pieces[1]);
+			$configValues[$option] = $value;
+		}
 	}
+} else {
+        echo "<font color='#FF0000'>error: could not open the file for reading:<b> $configFile </b><br/></font>";
+		echo "Check file permissions. The file should be readable by the webserver's user/group<br/>";
+        echo "
+            <script language='JavaScript'>
+            <!--
+            alert('could not open the file <b> $configFile </b> for reading!\\nCheck file permissions.');
+            -->
+            </script>
+            ";
+}
 	
 fclose($fp);
 
