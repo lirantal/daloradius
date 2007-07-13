ab0db7537bf16d48b1761995952f7224f196b961
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-13 07:59:22 +0000

    fixed up configuration variables used in these files as they were causing conflicts with variables of the same names (like $value or $option) in other php files

--- trunk/library/config_read.php
+++ trunk/library/config_read.php
@@ -1,27 +1,27 @@
 <?php
 
-$configFile = "library/daloradius.conf";
-$commentChar = "#";
+$_configFile = "library/daloradius.conf";
+$_configCommentChar = "#";
 
-$fp = fopen($configFile, "r");
-if ($fp) {
-	while (!feof($fp)) {
-		$line = trim(fgets($fp));
-		if ($line && !ereg("^$commentChar", $line)) {
-			$pieces = explode("=", $line);
-			$option = trim($pieces[0]);
-			$value = trim($pieces[1]);
-			$configValues[$option] = $value;
+$_configFp = fopen($_configFile, "r");
+if ($_configFp) {
+	while (!feof($_configFp)) {
+		$_configLine = trim(fgets($_configFp));
+		if ($_configLine && !ereg("^$_configCommentChar", $_configLine)) {
+			$_configPieces = explode("=", $_configLine);
+			$_configOption = trim($_configPieces[0]);
+			$_configValue = trim($_configPieces[1]);
+			$configValues[$_configOption] = $_configValue;
 		}
 	}
-	fclose($fp);
+	fclose($_configFp);
 } else {
-        echo "<font color='#FF0000'>error: could not open the file for reading:<b> $configFile </b><br/></font>";
+        echo "<font color='#FF0000'>error: could not open the file for reading:<b> $_configFile </b><br/></font>";
 		echo "Check file permissions. The file should be readable by the webserver's user/group<br/>";
         echo "
             <script language='JavaScript'>
             <!--
-            alert('could not open the file $configFile for reading!\\nCheck file permissions.');
+            alert('could not open the file $_configFile for reading!\\nCheck file permissions.');
             -->
             </script>
             ";
