390a50139fee3b0a39d6bfe65870ba16f36bdf1a
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-14 17:35:20 +0000

    fixed up wrong path for config files

--- trunk/library/config_write.php
+++ trunk/library/config_write.php
@@ -1,6 +1,6 @@
 <?php
 
-$configFile = "library/daloradius.conf";
+$configFile = "daloradius.conf";
 $commentChar = "#";
 
 $fp = fopen($configFile, "w");
--- trunk/library/opendb.php
+++ trunk/library/opendb.php
@@ -1,5 +1,5 @@
 <?php
-	include ("library/config_read.php");
+	include ("config_read.php");
 	
 	$mysql_conn = mysql_connect($configValues['CONFIG_DB_HOST'], $configValues['CONFIG_DB_USER'], $configValues['CONFIG_DB_PASS']) or die ('Error connecting to MySQL Server');
 	mysql_select_db($configValues['CONFIG_DB_NAME']);
