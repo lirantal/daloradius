229eed9d9388fd09fc29221707fddf8bc25e77e3
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-14 10:15:16 +0000

    updated management subnavs with correct items

--- trunk/include/menu/management-subnav.php
+++ trunk/include/menu/management-subnav.php
@@ -1,11 +1,14 @@
 
-                                <ul id="subnav">
+				<ul id="subnav">
+						<li><a href="mng-rad-nas.php"><em>N</em>as</a></li>
+                        <li><a href="mng-rad-usergroup.php"><em>U</em>ser-group Mapping</a></li>
+                        <li><a href="mng-rad-groupreply.php"><em>G</em>roup-reply</a></li>
+						<li><a href="mng-rad-groupcheck.php"><em>G</em>roup-check</a></li>
+				
+						<li>Welcome, <?php echo $operator; ?></li>
 
-                                                <li>Welcome, <?php echo $operator; ?></li>
-
-                                                <li><a href="logout.php">[logout]</a></li>
-
-                                </ul>
-								
-                </div>
-
+						<li><a href="logout.php">[logout]</a></li>
+				
+				</ul>
+		
+		</div>
