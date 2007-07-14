37ed7e7ec30b1f9e1759671f98e25a79f354be10
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-14 10:05:33 +0000

    introducing new file naming conventions and modularizing the menu items and it's subnavs

---
+++ trunk/include/menu/config-subnav.php
@@ -0,0 +1,11 @@
+
+                                <ul id="subnav">
+
+                                                <li>Welcome, <?php echo $operator; ?></li>
+
+                                                <li><a href="logout.php">[logout]</a></li>
+
+                                </ul>
+								
+                </div>
+
