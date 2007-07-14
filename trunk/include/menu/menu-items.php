75f2c71f87c86aead6ea1fd6fb796941f6ed01c4
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-07-14 09:54:01 +0000

    introducing new file naming conventions and modularizing the menu items and it's subnavs

---
+++ trunk/include/menu/menu-items.php
@@ -0,0 +1,28 @@
+                <div id="header">
+
+                                <form action="">
+                                <input value="Search" />
+                                </form>
+
+                                <h1><a href="index.php"><? echo $l[all][daloRADIUS]; ?></a></h1>
+
+                                <h2>
+                                
+                                <? echo $l[all][copyright1]; ?>
+                                
+				                                </h2>
+
+                                <ul id="nav">
+
+				<li><a href="index.php" <?php echo ($m_active == "Home") ? "class=\"active\"" : ""?>><?php echo $l[menu][Home]; ?></a></li>
+				<li><a href="mng-main.php" <?php echo ($m_active == "Managment") ? "class=\"active\"" : "" ?>><?php echo $l[menu][Managment]; ?></a></li>
+				<li><a href="rep-main.php" <?php echo ($m_active == "Reports") ? "class=\"active\"" : "" ?>><?php echo $l[menu][Reports]; ?></a></li>
+				<li><a href="acct-main.php" <?php echo ($m_active == "Accounting") ? "class=\"active\"" : "" ?>><?php echo $l[menu][Accounting]; ?></a></li>
+				<li><a href="bill-main.php" <?php echo ($m_active == "Billing") ? "class=\"active\"" : "" ?>><?php echo $l[menu][Billing]; ?></a></li>
+				<li><a href="gis-main.php" <?php echo ($m_active == "Gis") ? "class=\"active\"" : ""?>><?php echo $l[menu][Gis]; ?></a></li>
+				<li><a href="graph-main.php" <?php echo ($m_active == "Graphs") ? "class=\"active\"" : ""?>><?php echo $l[menu][Graphs]; ?></li>
+				<li><a href="config-main.php" <?php echo ($m_active == "Config") ? "class=\"active\"" : ""?>><?php echo $l[menu][Config]; ?></li>
+				<li><a href="help-main.php" <?php echo ($m_active == "Help") ? "class=\"active\"" : ""?>><?php echo $l[menu][Help]; ?></a></li>
+
+                                </ul>
+
