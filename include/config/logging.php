c4642b478e0673138be850b0ab6d3368a8d6a868
Liran Tal <liran.tal@gmail.com>
2007-07-23 18:32:50 +0000

    added support in function to receive the filename to write the log to

--- include/config/logging.php
+++ include/config/logging.php
@@ -2,35 +2,35 @@
 /*********************************************************************
 * Name: logging.php
 * Author: Liran tal <liran.tal@gmail.com>
-* 
+*
 * This file is used for controlling the logging actions
-* 
+*
 *********************************************************************/
 
 
-function logMessage($msg) {
-/* 
-* @param $msg		The message string which should be logged to the file
-* @return $table		The table name, either radcheck or radreply
+function logMessage($msg, $logFile) {
+/*
+* @param $msg           The message string which should be logged to the file
+* @param $logFile               The full path for the filename to write logs to
+* @return $table                The table name, either radcheck or radreply
 */
 
-	$logFile = $configValues['CONFIG_LOG_FILE'];
-	$fp = fopen($logFile, "w");
-	if ($fp) {
+        $fp = fopen($logFile, "a");
+        if ($fp) {
         fwrite($fp, $msg . "\n");
-		fclose($fp);
-	} else {
-	        echo "<font color='#FF0000'>error: could not open the file for writing:<b> $logFile </b><br/></font>";
-			echo "Check file permissions. The file should be writable by the webserver's user/group<br/>";
-	        echo "
-	            <script language='JavaScript'>
-	            <!--
-	            alert('could not open the file $logFile for writing!\\nCheck file permissions.');
-	            -->
-	            </script>
-			";
-	}	
-	
+                fclose($fp);
+        } else {
+                echo "<font color='#FF0000'>error: could not open the file for writing:<b> $logFile </b><br/></font>";
+                        echo "Check file permissions. The file should be writable by the webserver's user/group<br/>";
+                echo "
+                    <script language='JavaScript'>
+                    <!--
+                    alert('could not open the file $logFile for writing!\\nCheck file permissions.');
+                    -->
+                    </script>
+                        ";
+        }
+
 }
 
 
