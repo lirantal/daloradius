4b61f89582cf2b516b5f0876a88c69b0eee6f7ea
Liran Tal <liran.tal@gmail.com>
2007-07-23 22:16:26 +0000

    added logging support to bill* pages

--- bill-persecond.php
+++ bill-persecond.php
@@ -2,7 +2,10 @@
 
     include ("library/checklogin.php");
     $operator = $_SESSION['operator_user'];
-
+        
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+    include('include/config/logging.php');
 
     $hotspot = !empty($_REQUEST['ps-hotspot']) ? $_REQUEST['ps-hotspot'] : '';
 	$startdate = $_GET['ps-startdate'];
