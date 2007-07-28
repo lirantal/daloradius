436c8cbf99e203f8b045a641beac9b2ca849401b
Liran Tal <liran.tal@gmail.com>
2007-07-28 11:52:45 +0000

    added link for new page config-interface

--- menu-config.php
+++ menu-config.php
@@ -33,6 +33,7 @@
                                                 <li><a href="config-db.php"><b>&raquo;</b>Database Settings</a></li>
 						<li><a href="config-lang.php"><b>&raquo;</b>Language Settings</a></li>
 						<li><a href="config-logging.php"><b>&raquo;</b>Logging Settings</a></li>
+						<li><a href="config-interface.php"><b>&raquo;</b>Interface Settings</a></li>
 		
 		</div>
 		
