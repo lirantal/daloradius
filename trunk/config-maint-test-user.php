2710707dec2079cc4b065ed221919841ebc0c80a
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-08-04 10:30:46 +0000

    introduced first maintenance page

---
+++ trunk/config-maint-test-user.php
@@ -0,0 +1,139 @@
+<?php
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+        
+
+	$username = $_REQUEST['username'];
+	$password = $_REQUEST['password'];
+	$radius = $_REQUEST['radius'];
+	$radiusport = $_REQUEST['radiusport'];
+	$nasport = $_REQUEST['nasport'];
+	$secret = $_REQUEST['secret'];
+		
+    if (isset($_REQUEST['submit'])) {
+
+		include_once('library/exten-maint-test-user.php');	
+		
+		$username = $_REQUEST['username'];
+		$password = $_REQUEST['password'];
+		$radius = $_REQUEST['radius'];
+		$radiusport = $_REQUEST['radiusport'];
+		$nasport = $_REQUEST['nasport'];
+		$secret = $_REQUEST['secret'];
+
+		$actionStatus = "success";
+		$actionMsg = user_login_test($username, $password, $radius, $radiusport, $nasport, $secret);
+		$logAction = "Successfully performed user_login_test on user [$username] on page: ";	
+    }
+
+	
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+    include('include/config/logging.php');
+
+	
+?>		
+
+<?php
+
+    include ("menu-config.php");
+
+?>		
+		
+		
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#"><?php echo $l[Intro][configmainttestuser.php] ?></a></h2>
+				<p>
+				<?php echo $l[captions][configmainttestuser] ?>
+
+				<br/><br/>
+
+				<form name="mainttestuser" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+
+<table border='2' class='table1'>
+<tr><td>
+						<?php if (trim($username) == "") { echo "<font color='#FF0000'>";  }?>
+
+						<b><?php echo $l[FormField][all][Username] ?></b>
+</td><td>
+						<input value="<?php echo $username ?>" name="username" />
+						</font>
+</td></tr>
+<tr><td>
+
+						<?php if (trim($password) == "") { echo "<font color='#FF0000'>";  }?>
+
+						<b><?php echo $l[FormField][all][Password] ?></b>
+</td><td>
+						<input value="<?php echo $password ?>" name="password" />
+						</font>
+
+
+</td></tr>
+<tr><td>
+						<?php if (trim($radius) == "") { echo "<font color='#FF0000'>";  }?>
+
+						<b><?php echo $l[FormField][configmainttestuser.php][RadiusServer] ?></b>
+</td><td>
+						<input value="<?php echo $radius ?>" name="radius" />
+						</font>
+</td></tr>
+<tr><td>
+						<?php if (trim($radiusport) == "") { echo "<font color='#FF0000'>";  }?>
+
+						<b><?php echo $l[FormField][configmainttestuser.php][RadiusPort] ?></b>
+</td><td>
+						<input value="<?php echo $radiusport ?>" name="radiusport" />
+						</font>
+</td></tr>
+<tr><td>
+						<?php if (trim($nasport) == "") { echo "<font color='#FF0000'>";  }?>
+
+						<b><?php echo $l[FormField][configmainttestuser.php][NASPort] ?></b>
+</td><td>
+						<input value="<?php echo $nasport ?>" name="nasport" />
+						</font>
+</td></tr>
+<tr><td>
+
+						<?php if (trim($secret) == "") { echo "<font color='#FF0000'>";  }?>
+
+						<b><?php echo $l[FormField][configmainttestuser.php][Secret] ?></b>
+</td><td>
+						<input value="<?php echo $secret ?>" name="secret" />
+						</font>
+</td></tr>
+
+
+</table>
+
+						<center>						
+						<br/>
+						<input type="submit" name="submit" value="Apply" />
+						</center>						
+
+
+				</form>
+
+	
+				<br/><br/>
+				
+				</p>
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
