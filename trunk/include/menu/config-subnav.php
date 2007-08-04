b24c034d853aeafa2242bec8fef8b25fe6e1e3be
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-08-04 09:29:53 +0000

    added maintenance subnav link

--- trunk/include/menu/config-subnav.php
+++ trunk/include/menu/config-subnav.php
@@ -1,8 +1,8 @@
 
                                 <ul id="subnav">
-
+												<li><a href="config-maint.php"><em>M</em>aintenance</a></li>
+												
                                                 <li>Welcome, <?php echo $operator; ?></li>
-
                                                 <li><a href="logout.php">[logout]</a></li>
 
                                 </ul>
