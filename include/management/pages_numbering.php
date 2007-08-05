b39304ddeaebf0faf565fbbe5c3efbfa6083f995
Liran Tal <liran.tal@gmail.com>
2007-08-05 08:05:34 +0000

    added php code to support spanning of table results (listing) across several pages

---
+++ include/management/pages_numbering.php
@@ -0,0 +1,76 @@
+<?php
+/*********************************************************************
+* Name: pages_numbering.php
+* Author: Liran tal <liran.tal@gmail.com>
+*
+* This script provides support for spanning alot of table results across several
+* pages with full numbering support, first and last links, etc...
+*
+*********************************************************************/
+
+
+/* Should be called after the include for opendb and before the $sql decleration
+and execution. */
+
+$rowsPerPage = 5;
+$pageNum = 1;
+
+if(isset($_REQUEST['page'])) {
+	$pageNum = $_GET['page'];
+}
+
+$offset = ($pageNum - 1) * $rowsPerPage;
+
+
+
+
+function setupLinks($pageNum, $maxPage) {
+
+	// print 'previous' link only if we're not
+	// on page one
+	if ($pageNum > 1)       {
+		$page = $pageNum - 1;
+		$prev = " <a href=\"$self?page=$page\">[Prev]</a> ";
+
+		$first = " <a href=\"$self?page=1\">[First Page]</a> ";
+	} else {
+		$prev  = ' [Prev] ';       // we're on page one, don't enable 'previous' link
+		$first = ' [First Page] '; // nor 'first page' link
+	}
+
+
+	if ($pageNum < $maxPage) {
+		$page = $pageNum + 1;
+		$next = " <a href=\"$self?page=$page\">[Next]</a> ";
+
+		$last = " <a href=\"$self?page=$maxPage\">[Last Page]</a> ";
+	} else {
+		$next = ' [Next] ';      // we're on the last page, don't enable 'next' link
+		$last = ' [Last Page] '; // nor 'last page' link
+	}
+
+
+// print the page navigation link
+echo $first . $prev . " &nbsp;&nbsp;&nbsp; page &nbsp; <strong>$pageNum</strong> of <strong>$maxPage</strong> &nbsp;&nbsp;&nbsp; " . $next . $last;
+echo "<br/><br/>";
+
+}
+
+
+
+
+function setupNumbering($numrows, $rowsPerPage, $pageNum) {
+
+	$numofpages = $numrows / $rowsPerPage;
+	for ($i = 1; $i <= $numofpages; $i++) {
+		if($i == $pageNum) {
+			echo("&nbsp;".$i."&nbsp;");
+		} else {
+			echo("&nbsp; <a href=\"$PHP_SELF?page=$i\">$i</a>&nbsp;");
+		}
+	}
+
+}
+
+?>
+
