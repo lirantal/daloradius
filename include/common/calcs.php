0b67f1b568bd9caf238daa97a6eac90e433ff6ad
Liran Tal <liran.tal@gmail.com>
2007-08-10 09:28:36 +0000

    added function to convert from bytes to mb for more readable tables listing

--- include/common/calcs.php
+++ include/common/calcs.php
@@ -23,45 +23,13 @@ return $myTime;
 
 }
 
-/*
-no permission was given to use this code yet...
-function sec2hms ($sec, $padHours = false) {
-// this code was adopted from Jon: http://www.laughing-buddha.net/jon/php/sec2hms/
 
-	// holds formatted string
-	$hms = "";
 
-	// there are 3600 seconds in an hour, so if we
-	// divide total seconds by 3600 and throw away
-	// the remainder, we've got the number of hours
-	$hours = intval(intval($sec) / 3600); 
 
-	// add to $hms, with a leading 0 if asked for
-	$hms .= ($padHours) 
-		  ? str_pad($hours, 2, "0", STR_PAD_LEFT). ':'
-		  : $hours. ':';
-	 
-	// dividing the total seconds by 60 will give us
-	// the number of minutes, but we're interested in 
-	// minutes past the hour: to get that, we need to 
-	// divide by 60 again and keep the remainder
-	$minutes = intval(($sec / 60) % 60); 
 
-	// then add to $hms (with a leading 0 if needed)
-	$hms .= str_pad($minutes, 2, "0", STR_PAD_LEFT). ':';
+function bytes2megabytes ($bytes) {
 
-	// seconds are simple - just divide the total
-	// seconds by 60 and keep the remainder
-	$seconds = intval($sec % 60); 
-
-	// add to $hms, again with a leading 0 if needed
-	$hms .= str_pad($seconds, 2, "0", STR_PAD_LEFT);
-
-	// done!
-	return $hms;
-    
-}
-*/
+return intval(($bytes/1024/1024));
 
 ?>
 
