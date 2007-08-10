7a47967817cbec5b87bb1ac2d669e55b2a5fc966
Liran Tal <liran.tal@gmail.com>
2007-08-10 09:31:05 +0000

    forgot to add closing bracket to function, also aligned function code with correc identation

--- include/common/calcs.php
+++ include/common/calcs.php
@@ -10,17 +10,18 @@
 
 function seconds2time ($seconds) {
 
-$hours = 3600;
-$minutes = 60;
+	$hours = 3600;
+	$minutes = 60;
 
-$h = intval($seconds / $hours);
-$s_remaining = ($seconds - ($h * $hours));
-$m = intval($s_remaining / $minutes);
-$s = ($s_remaining - ($m * $minutes));
+	$h = intval($seconds / $hours);
+	$s_remaining = ($seconds - ($h * $hours));
+	$m = intval($s_remaining / $minutes);
+	$s = ($s_remaining - ($m * $minutes));
 
-$myTime = $h.":".$m.":".$s;
-return $myTime;
-
+	$myTime = $h.":".$m.":".$s;
+	
+	
+	return $myTime;
 }
 
 
@@ -29,7 +30,9 @@ return $myTime;
 
 function bytes2megabytes ($bytes) {
 
-return intval(($bytes/1024/1024));
+	return intval(($bytes/1024/1024));
+
+}
 
 ?>
 
