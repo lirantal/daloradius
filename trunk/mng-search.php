4836e7dc7f15c53bac59427fc9aa3367eef7ab5e
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-08-13 18:19:27 +0000

    fixed passing of $nt[1] value

--- trunk/mng-search.php
+++ trunk/mng-search.php
@@ -104,7 +104,7 @@
 					<td> $nt[1] </td>
 					<td> <a href='mng-edit.php?username=$nt[UserName]'> ".$l[all][edit]." </a>
 					 <a href='mng-del.php?username=$nt[UserName]'> ".$l[all][del]." </a>
-					 <a href='config-maint-test-user.php?username=$nt[UserName]&password=$nt[Value]'> Test User </a>
+					 <a href='config-maint-test-user.php?username=$nt[UserName]&password=$nt[1]'> Test User </a>
 					 <a href='acct-username.php?username=$nt[UserName]'> Accounting </a>
 			 </td>
 
