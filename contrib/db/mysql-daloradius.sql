7895e6c001ee928ea194f0c20a37e788aba1dd1a
Liran Tal <liran.tal@gmail.com>
2007-08-23 17:25:08 +0000

    applied new naming conventions appropriately to db files

---
+++ contrib/db/mysql-daloradius.sql
@@ -0,0 +1,96 @@
+-- MySQL dump 10.10
+--
+-- Host: localhost    Database: radius
+-- ------------------------------------------------------
+-- Server version	5.0.22-Debian_0ubuntu6.06-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hotspots`
+--
+
+DROP TABLE IF EXISTS `hotspots`;
+CREATE TABLE `hotspots` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `name` varchar(32) default NULL,
+  `mac` varchar(32) default NULL,
+  `geocode` varchar(128) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hotspots`
+--
+
+
+/*!40000 ALTER TABLE `hotspots` DISABLE KEYS */;
+LOCK TABLES `hotspots` WRITE;
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `hotspots` ENABLE KEYS */;
+
+--
+-- Table structure for table `operators`
+--
+
+DROP TABLE IF EXISTS `operators`;
+CREATE TABLE `operators` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(32) default NULL,
+  `password` varchar(32) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `operators`
+--
+
+
+/*!40000 ALTER TABLE `operators` DISABLE KEYS */;
+LOCK TABLES `operators` WRITE;
+INSERT INTO `operators` VALUES (1,'administrator','radius');
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `operators` ENABLE KEYS */;
+
+--
+-- Table structure for table `rates`
+--
+
+DROP TABLE IF EXISTS `rates`;
+CREATE TABLE `rates` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `type` varchar(32) default NULL,
+  `cardbank` double default NULL,
+  `rate` double default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rates`
+--
+
+
+/*!40000 ALTER TABLE `rates` DISABLE KEYS */;
+LOCK TABLES `rates` WRITE;
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `rates` ENABLE KEYS */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
