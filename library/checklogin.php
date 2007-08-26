207b6f793a513b1d07b4ea2572ea3e6844809b34
Liran Tal <liran.tal@gmail.com>
2007-08-26 21:05:56 +0000

    fixed issue of checklogin logic not working right by showing the actual page instead of redirecting to a user login screen...

--- library/checklogin.php
+++ library/checklogin.php
@@ -3,7 +3,7 @@ session_start();
 
 // is the one accessing this page logged in or not?
 if (!isset($_SESSION['logged_in'])
-   || $_SESSION['logged_in'] !== true) {
+   || $_SESSION['logged_in'] != true) {
 
    // not logged in, move to login page
    header('Location: login.php?error=please login first');
