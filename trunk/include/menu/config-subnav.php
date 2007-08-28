c3f5a587c835222abbe2afe59fb58328d60df58a
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-08-28 13:50:09 +0000

    added sub menu link for configuring operators details

--- trunk/include/menu/config-subnav.php
+++ trunk/include/menu/config-subnav.php
@@ -1,7 +1,8 @@
 
                                 <ul id="subnav">
-												<li><a href="config-maint.php"><em>M</em>aintenance</a></li>
-												
+
+					<li><a href="config-maint.php"><em>M</em>aintenance</a></li>
+					<li><a href="config-operators.php"><em>O</em>operators</a></li>
                                                 <li>Welcome, <?php echo $operator; ?></li>
                                                 <li><a href="logout.php">[logout]</a></li>
 
