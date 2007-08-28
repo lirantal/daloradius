9097dc8fe915c7c325c37b76357fed6c9262adea
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-08-28 14:41:46 +0000

    fixed typo in link text

--- trunk/include/menu/config-subnav.php
+++ trunk/include/menu/config-subnav.php
@@ -2,7 +2,7 @@
                                 <ul id="subnav">
 
 					<li><a href="config-maint.php"><em>M</em>aintenance</a></li>
-					<li><a href="config-operators.php"><em>O</em>operators</a></li>
+					<li><a href="config-operators.php"><em>O</em>perators</a></li>
                                                 <li>Welcome, <?php echo $operator; ?></li>
                                                 <li><a href="logout.php">[logout]</a></li>
 
