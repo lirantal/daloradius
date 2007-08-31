c7ad6f2ee2bf8a8b71edd5b778abfcc604edd4d7
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-08-31 10:12:46 +0000

    changed layout- moved logout/login information window to the right

--- trunk/include/menu/management-subnav.php
+++ trunk/include/menu/management-subnav.php
@@ -4,11 +4,11 @@
                         <li><a href="mng-rad-usergroup.php"><em>U</em>ser-group Mapping</a></li>
                         <li><a href="mng-rad-groupreply.php"><em>G</em>roup-reply</a></li>
 						<li><a href="mng-rad-groupcheck.php"><em>G</em>roup-check</a></li>
-				
-						<li>Welcome, <?php echo $operator; ?></li>
 
+<div id="logindiv" style="text-align: right;">
+
+						<li>Welcome, <?php echo $operator; ?></li>
 						<li><a href="logout.php">[logout]</a></li>
-				
 				</ul>
 		
 		</div>
