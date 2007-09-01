14a1b4ad97b4215417ec6812c86afe7b030cc2dc
Liran Tal <liran.tal@gmail.com>
2007-09-01 11:20:44 +0000

    added library and files to handle the operator permissions

---
+++ msg-error-permissions.php
@@ -0,0 +1,51 @@
+<?php
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+
+</head>
+ 
+<?php
+
+        include ("menu-home.php");
+
+?>
+				
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#">Error</a></h2>
+				
+				<p>
+
+				You do not have permissions to access the page. <br/>
+				Please consult with your System Administrator. <br/>
+				
+				
+				</p>
+				
+		
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
