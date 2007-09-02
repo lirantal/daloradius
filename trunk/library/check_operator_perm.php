8652b75be28fa7c9e89ecc8e59e5002e99ad1c5f
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-09-02 05:47:10 +0000

    added security and bug-fix to handling undefined pages while attempting to access an index in the pages array

--- trunk/library/check_operator_perm.php
+++ trunk/library/check_operator_perm.php
@@ -9,8 +9,12 @@
 	$currFile = str_replace("-", "_", $currFile);
 	$currFile = str_replace(".php", "", $currFile);
 
-    $test = $row[$currFile];
-
+	// the importance of the following is not to be discarded.
+	// the following tests if the page is defined and valid in the  include/management/operator_tables.php array and if it isn't
+	// it will force the page to not be displayed. meaning that all pages (for example newer pages) must be defined in that array
+	// otherwise they will not be accessible.
+	isset($row[$currFile]) ? $test = $row[$currFile] : $test="no";
+	
 	if (!( (strcasecmp($test, "y") == 0) || (strcasecmp($test, "yes") == 0) || (strcasecmp($test, "on") == 0)   )) {
 //		echo "<br/><br/> permission denied! <br/><br/>";
 		header('Location: msg-error-permissions.php');
