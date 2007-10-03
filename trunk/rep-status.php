63d499ed7d3bc0ed54b5a9e4891db2c4e0f04553
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-10-03 13:47:15 +0000

    added new files to reports page - logs subnav and status subnav

---
+++ trunk/rep-status.php
@@ -0,0 +1,45 @@
+<?php
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+        
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+
+
+?>
+
+<?php
+
+    include ("menu-reports-status.php");
+
+?>
+		
+		<div id="contentnorightbar">
+		
+		<h2 id="Intro"><a href="#"><? echo $l[Intro][repmain.php]; ?></a></h2>
+				
+
+
+
+<?php
+	include('include/config/logging.php');
+?>
+
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
