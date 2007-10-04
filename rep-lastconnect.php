d01a7a8cb32379c5e2dbec663210ca0b4561642e
Liran Tal <liran.tal@gmail.com>
2007-10-04 10:36:27 +0000

    added reporting logs and reporting status pages - these are the same files from the index-* filenames before

---
+++ rep-lastconnect.php
@@ -0,0 +1,49 @@
+<?php
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+	include('library/check_operator_perm.php');
+
+
+
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+    $logQuery = "performed query on page: ";
+    include('include/config/logging.php');
+
+
+?>
+
+<?php
+
+    include ("menu-reports.php");
+        	
+?>		
+		
+		
+		<div id="contentnorightbar">
+		
+		<h2 id="Intro"><a href="#"><? echo $l[Intro][indexlastconnect.php]; ?></a></h2>
+				<p>
+
+<?php
+	include 'library/table-last_connection_attempts.php';
+?>
+				</p>
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
