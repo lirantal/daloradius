04afb1c1fcc3911ad8272398d9c22f083d9f4a1d
Liran Tal <liran.tal@gmail.com>
2007-10-08 18:17:59 +0000

    added actual support for language files

--- lang/main.php
+++ lang/main.php
@@ -1,3 +1,18 @@
 <?
-include ("lang/en.php");
+
+	include_once('library/config_read.php');
+	
+	switch($configValues['CONFIG_LANG']) {
+	
+		case "en":
+			include ("lang/en.php");
+			break;
+		case "ru":
+			include ("lang/ru.php");
+			break;
+		default:
+			include ("lang/en.php");
+			break;
+	}
+
 ?>
