b827c110bcd7de5222477a026ab707cf845714d2
Liran Tal <liran.tal@gmail.com>
2007-11-03 16:50:59 +0000

    changed the group management to have better support for multiple attributes when creating a new groupreply or groupcheck mapping

--- mng-rad-groupreply-new.php
+++ mng-rad-groupreply-new.php
@@ -7,128 +7,86 @@
 
 	// declaring variables
 	$groupname = "";
-	$op = "";
-	$attribute = "";
-	$value = "";	
+
 	$logDebugSQL = "";
-	
 
     if (isset($_POST['submit'])) {
 	
         
         include 'library/opendb.php';	
-	
+        include 'include/management/attributes.php';                            // required for checking if an attribute belongs to the
+
 	    $groupname = $_REQUEST['groupname'];
-	    $op = $_REQUEST['op'];
-	    $attribute = $_REQUEST['attribute'];
-		$value = $_REQUEST['value'];
 
+	if ($groupname) {
+		
 		$counter = 0;
-		foreach ($groupname as $group) {
+		foreach ($_POST as $attribute=>$value) {
 
-			if ($group == "")
+                                        // switch case to rise the flag for several $attribute which we do not
+                                        // wish to process (ie: do any sql related stuff in the db)
+                                        switch ($attribute) {
+
+                                                case "groupname":
+                                                case "submit":
+                                                        $skipLoopFlag = 1;      // if any of the cases above has been met we set a flag
+                                                                                // to skip the loop (continue) without entering it as
+                                                                                // we do not want to process this $attribute in the following
+                                                                                // code block
+                                                        break;
+
+                                        }
+
+                                        if ($skipLoopFlag == 1) {
+                                                $skipLoopFlag = 0;              // resetting the loop flag
+                                                continue;
+                                        }
+
+
+
+			if ($value[0] == "")				// we don't process attribtues with no values
 					continue;
 					
-			// echo "$group $attribute[$counter] $op[$counter] $value[$counter] <br/> "; 	// for debugging purposes
-				
-			$sql = "SELECT * FROM ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY']." WHERE GroupName='$group' AND Value='$value[$counter]'";
+			$sql = "SELECT * FROM ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY']." WHERE GroupName='$groupname' AND Attribute='$attribute'";
 			$res = $dbSocket->query($sql);
 			$logDebugSQL .= $sql . "\n";
 				
+
 			if ($res->numRows() == 0) {
-				if (trim($group) != "" and trim($value[$counter]) != "" and trim($op[$counter]) != "" and trim($attribute[$counter]) != "") {								
 					// insert usergroup details
-					$sql = "INSERT INTO ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY']." values (0,'$group', '$attribute[$counter]', '$op[$counter]', '$value[$counter]')";
+					// assuming there's no groupname with that attribute in the table
+					$sql = "INSERT INTO ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY']." values (0,'$groupname', '$attribute', '$value[1]', '$value[0]')";
 					$res = $dbSocket->query($sql);
 					$logDebugSQL .= $sql . "\n";
 					$counter++;
 					
 					$actionStatus = "success";
-					$actionMsg = "Added to database new group: <b> $group </b>";
-					$logAction = "Successfully added group [$group] on page: ";
-				} else {
-					$actionStatus = "failure";
-					$actionMsg = "you are missing possible values for Groupname, Attribute, Operator or Value";	
-					$logAction = "Failed adding (missing values) group [$group] on page: ";
-				}
+					$actionMsg = "Added to database new group: <b> $groupname </b> with attribute: <b> $attribute </b> and value: <b> $value[0] </b>";
+					$logAction = "Successfully added group [$groupname] with attribute: <b> $attribute </b> and value: <b> $value[0] </b> on page: ";
 			} else { 
 				$actionStatus = "failure";
-				$actionMsg = "The group <b> $groupname[$counter] </b> already exists in the database with value <b> $value[$counter] </b>";
-				$logAction = "Failed adding already existing group [$group] with value [$value[$counter]] on page: ";
+				$actionMsg = "The group <b> $groupname </b> already exists in the database with attribute <b> $attribute </b>";
+				$logAction = "Failed adding already existing group [$groupname] with attribute [$attribute] on page: ";
 
-            } // end else if mysql
+		        } // end else if mysql
+
+
 				
-		}
+		    }
 
-		if (isset($_REQUEST['groupnameExtra']))
-			$groupnameExtra = $_REQUEST['groupnameExtra'];
-		else
-			$groupnameExtra = "";
-		
-		if (isset($_REQUEST['opExtra']))
-			$opExtra = $_REQUEST['opExtra'];
-		else
-			$opExtra = "";
-			
-		if (isset($_REQUEST['attributeExtra']))
-			$attributeExtra = $_REQUEST['attributeExtra'];
-		else
-			$attributeExtra = "";
-			
-		if (isset($_REQUEST['valueExtra']))
-			$valueExtra = $_REQUEST['valueExtra'];
-		else
-			$valueExtra = "";
-			
-		if ($groupnameExtra) {
-		
-			$counter = 0;
-		
-			foreach ($groupnameExtra as $groupExtra) {
-			
-				if ($groupExtra == "")
-					continue; 
+   		} else { // if groupname isset
+			$actionStatus = "failure";
+			$actionMsg = "No groupname was defined";
+			$logAction = "Failed adding missing values for groupname on page: ";
+   		}
 
-				// echo "$group $attribute[$counter] $op[$counter] $value[$counter] <br/> "; 	// for debugging purposes
-					
-				$sql = "SELECT * FROM ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY']." WHERE GroupName='$groupExtra' AND Value='$valueExtra[$counter]'";
-				$res = $dbSocket->query($sql);
-				$logDebugSQL .= $sql . "\n";
-					
-				if ($res->numRows() == 0) {
-					if (trim($groupExtra) != "" and trim($valueExtra[$counter]) != "" and trim($opExtra[$counter]) != "" and trim($attributeExtra[$counter]) != "") {								
-						// insert usergroup details
-						$sql = "INSERT INTO ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY']." values (0,'$groupExtra', '$attributeExtra[$counter]', '$opExtra[$counter]', '$valueExtra[$counter]')";
-						$res = $dbSocket->query($sql);
-						$logDebugSQL .= $sql . "\n";
-						$counter++;
-					} // end if trim
-					
-					$actionStatus = "success";
-					$actionMsg = "Added to database new group: <b> $groupExtra </b>";
-					$logAction = "Successfully added group [$groupExtra] on page: ";
-					
-				} else { 
-					$actionStatus = "failure";
-					$actionMsg = "The group <b> $groupnameExtra[$counter] </b> already exists in the database with value <b> $valueExtra[$counter] </b>";
-					$logAction = "Failed addin already existing group [$groupnameExtra[$counter]] with value [$valueExtra[$counter]] on page: ";
-				}
-		
-			}	
 
         include 'library/closedb.php';
-		
-    }
 	
 	}
 
 
-
-        isset($groupname[0]) ? $groupname = $groupname[0] : $groupname = "";
-        isset($attribute[0]) ? $attribute = $attribute[0] : $attribute = "";
-        isset($value[0]) ? $value = $value[0] : $value = "";
-        isset($op[0]) ? $op = $op[0] : $op = "";
-
+        isset($groupname) ? $groupname = $groupname : $groupname = "";
 
 	include_once('library/config_read.php');
     $log = "visited page: ";
@@ -167,50 +125,23 @@
                                                 <?php if (trim($groupname) == "") { echo "<font color='#FF0000'>"; }?>
                                                 <b><?php echo $l['FormField']['all']['Groupname'] ?></b>
 </td><td>										
-                                                <input value="<?php echo $groupname ?>" name="groupname[]"/>
-                                                </font><br/>
-</td></tr>
-<tr><td>
-                                                <?php if (trim($attribute) == "") { echo "<font color='#FF0000'>";  }?>
-						<b><?php echo $l['FormField']['all']['Attribute'] ?></b>
-</td><td>												
-                                                <input value="<?php echo $attribute ?>" name="attribute[]" /> 
-                                                </font><br/>
-</td></tr>
-<tr><td>
-												
-                                                <?php if (trim($op) == "") { echo "<font color='#FF0000'>";  }?>
-                                                <b><?php echo $l['FormField']['all']['Operator'] ?></b>
-</td><td>																								
-												<select name="op[]" />
-				<?php include ('include/management/op_select_options.php');
-					  drawOptions();
-					  ?>
-												</select>
-                                                </font><br/>
-</td></tr>
-<tr><td>
-                                                <?php if (trim($value) == "") { echo "<font color='#FF0000'>";  }?>
-                                                <b><?php echo $l['FormField']['all']['Value'] ?></b>
-</td><td>												
-                                                <input value="<?php echo $value ?>" name="value[]" />
+                                                <input value="<?php echo $groupname ?>" name="groupname"/>
                                                 </font><br/>
 </td></tr>
 </table>
-
-<br/>
-<center>
-<input type="button" value="Add Groups" onclick="addStuff()"/>
-</center>
-<br/><br/>
-
-<div id="mydiv">
-</div>
 
                                                 <br/><br/>
 <center>												
                                                 <input type="submit" name="submit" value="<?php echo $l['buttons']['apply'] ?>"/>
 </center>											
+
+
+<br/><br/>
+<?php
+        include_once('include/management/attributes.php');
+        drawAttributes();
+?>
+
 
 
 
