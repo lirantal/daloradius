6e43018329ba53312f9c9d0bb8d99dbc149e7840
Liran Tal <liran.tal@gmail.com>
2007-11-06 13:59:50 +0000

    html code clean up (fixed typo, there was an extra > in the html)

--- graphs-overall_download.php
+++ graphs-overall_download.php
@@ -37,7 +37,7 @@
 		
 		<div id="contentnorightbar">
 		
-		<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')">><? echo $l['Intro']['graphsoveralldownload.php']; ?></a></h2>
+		<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><? echo $l['Intro']['graphsoveralldownload.php']; ?></a></h2>
 
                 <div id="helpPage" style="display:none;visibility:visible" >
 			<?php echo $l['helpPage']['graphsoveralldownload'] ?>		
