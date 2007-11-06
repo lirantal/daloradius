ee2d98911bb69431884104385f9a53f6035d7916
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-11-06 13:59:50 +0000

    html code clean up (fixed typo, there was an extra > in the html)

--- trunk/graphs-alltime_traffic_compare.php
+++ trunk/graphs-alltime_traffic_compare.php
@@ -30,7 +30,7 @@
 		
 		<div id="contentnorightbar">
 		
-		<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')">><? echo $l['Intro']['graphsalltimetrafficcompare.php']; ?></a></h2>
+		<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><? echo $l['Intro']['graphsalltimetrafficcompare.php']; ?></a></h2>
 
                 <div id="helpPage" style="display:none;visibility:visible" >
 			<?php echo $l['helpPage']['graphsalltimetrafficcompare'] ?>		
