febab9c40b0016e99290c1be87d3acd39da4627e
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-11-08 08:28:28 +0000

    changed pages_numbering include to reflect new changes to the new layout for tables listing

--- trunk/include/management/pages_numbering.php
+++ trunk/include/management/pages_numbering.php
@@ -20,7 +20,7 @@ if(isset($_REQUEST['page'])) {
 }
 
 $offset = ($pageNum - 1) * $rowsPerPage;
-//isset($_SERVER['PHP_SELF']) ? $self = $_SERVER['PHP_SELF'] : $self = "";
+$self = $_SERVER['PHP_SELF'];
 
 function setupLinks($pageNum, $maxPage, $orderBy, $orderType, $request1="", $request2="", $request3="") {
 
@@ -28,8 +28,8 @@ function setupLinks($pageNum, $maxPage, $orderBy, $orderType, $request1="", $req
 	// on page one
 	if ($pageNum > 1)       {
 		$page = $pageNum - 1;
-		$prev = " <a class='novisit' href=\"?page=$page&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[Prev]</a> ";
-		$first = "<a class='novisit' href=\"?page=1&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[First Page]</a> ";
+		$prev = " <a class=\"table\" class='novisit' href=\"?page=$page&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[Prev]</a> ";
+		$first = "<a class=\"table\" class='novisit' href=\"?page=1&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[First Page]</a> ";
 	} else {
 		$prev  = ' [Prev] ';       // we're on page one, don't enable 'previous' link
 		$first = ' [First Page] '; // nor 'first page' link
@@ -38,51 +38,16 @@ function setupLinks($pageNum, $maxPage, $orderBy, $orderType, $request1="", $req
 
 	if ($pageNum < $maxPage) {
 		$page = $pageNum + 1;
-		$next = " <a class='novisit' href=\"?page=$page&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[Next]</a> ";
-		$last = " <a class='novisit' href=\"?page=$maxPage&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[Last Page]</a> ";
+		$next = " <a class=\"table\" class='novisit' href=\"?page=$page&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[Next]</a> ";
+		$last = " <a class=\"table\" class='novisit' href=\"?page=$maxPage&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">[Last Page]</a> ";
 	} else {
 		$next = ' [Next] ';      // we're on the last page, don't enable 'next' link
 		$last = ' [Last Page] '; // nor 'last page' link
 	}
 
 
-// print the page navigation link (simple view)
-//echo $first . $prev . " &nbsp;&nbsp;&nbsp; page &nbsp; <strong>$pageNum</strong> of <strong>$maxPage</strong> &nbsp;&nbsp;&nbsp; " . $next . $last;
-//echo "<br/><br/>";
-
-
-// print the page navigation links in better looking layout
-echo <<<EOT
-<br/>
-<table border='2' class='table1'>
-<thead>
-                <tr>
-                <th class='info' colspan='10'>Navigation Links</th>
-                </tr>
-</thead>
-<tr><td>
-</td><td>
-</td><td>
-</td><td>
-        $first
-</td><td>
-        $prev
-</td><td>
-        <strong> $pageNum </strong> of <strong> $maxPage </strong>
-</td><td>
-        $next
-</td><td>
-        $last
-</td></tr>
-</table>
-<br/>
-
-
-
-
-EOT;
-
-
+echo "$first $prev $pageNum of $maxPage $next $last";
+echo "<br/>";
 
 }
 
@@ -92,15 +57,13 @@ EOT;
 function setupNumbering($numrows, $rowsPerPage, $pageNum, $orderBy, $orderType, $request1="", $request2="", $request3="") {
 
 	$numofpages = $numrows / $rowsPerPage;
-	echo "<center>";
 	for ($i = 1; $i <= $numofpages + 1; $i++) {
 		if($i == $pageNum) {
-			echo("&nbsp;<strong><font color='#FF0000'>".$i."</font></strong>&nbsp;");
+			echo("&nbsp;<strong><font color='#0099FF'>".$i."</font></strong>&nbsp;");
 		} else {
-			echo("&nbsp; <a href=\"?page=$i&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">$i</a>&nbsp;");
+			echo("&nbsp; <a class=\"table\" href=\"?page=$i&orderBy=$orderBy&orderType=$orderType$request1$request2$request3\">$i</a>&nbsp;");
 		}
 	}
-	echo "</center>";
 
 }
 
