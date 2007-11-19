eb1d76ecb605a6d386a8d540b35ab2198731cd7b
Liran Tal <liran.tal@gmail.com>
2007-11-19 11:45:00 +0000

    put groups support on external file and also added langauge support to the tabs titles

--- lang/en.php
+++ lang/en.php
@@ -81,6 +81,9 @@ $l['table']['Attributes'] = "Attributes";
 $l['table']['HotspotInfo'] = "Hotspot Info";
 $l['table']['ContactInfo'] = "Contact Info";
 
+$l['table']['RADIUSCheck'] = "RADIUS Check";
+$l['table']['RADIUSReply'] = "RADIUS Reply";
+
 /* ********************************************************************************** */
 
 
