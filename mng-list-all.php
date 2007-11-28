d3e1f2623569e471f41df3a7e3ee1177dfd1963c
Liran Tal <liran.tal@gmail.com>
2007-11-28 22:04:03 +0000

    added support for multi-records operations with the additions of checkboxes for each record

--- mng-list-all.php
+++ mng-list-all.php
@@ -29,6 +29,47 @@
 <![endif]-->
 </head>
 
+<SCRIPT LANGUAGE="JavaScript">
+<!-- 	
+// script
+-->
+
+var form='listallusers' //Give the form name here
+
+function SetChecked(val,chkName) {
+	dml=document.forms[form];
+	len = dml.elements.length;
+	var i=0;
+	for( i=0 ; i<len ; i++) {
+		if (dml.elements[i].name==chkName) {
+		dml.elements[i].checked=val;
+		}
+	}
+}
+
+function countChkBoxes() {
+	var count = 0;
+	var form = document.getElementsByTagName('input');
+	for (var i=0; i < form.length; ++i) {
+		var e = form[i];
+		if (e.type == 'checkbox'
+		&& e.checked)
+		++count;
+	}
+
+	if (confirm("You are about to remove " + count + " users\nDo you want to continue?"))  {
+		document.listallusers.submit();
+		return true;
+	}
+
+	return false;
+}
+
+
+//  End -->
+</script>
+
+
 <?php
 
     include ("menu-mng-main.php");
@@ -42,7 +83,6 @@
                 <div id="helpPage" style="display:none;visibility:visible" >
 			<?php echo $l['helpPage']['mnglistall'] ?>		
 		</div>
-		<br/>
 
 <?php
 
@@ -69,6 +109,8 @@
 	/* END */
 
 
+	echo "<form name='listallusers' method='post' action='mng-del.php'>";
+
 	echo "<table border='2' class='table1'>\n";
 	echo "
 					<thead>
@@ -77,8 +119,17 @@
 							</tr>
 							<tr>
 							<th colspan='10' align='left'> 
-				<br/>
-	";
+
+				Select:
+				<a class=\"table\" href=\"javascript:SetChecked(1,'username[]')\">All</a> 
+				
+				<a class=\"table\" href=\"javascript:SetChecked(0,'username[]')\">None</a>
+			<br/>
+				<input class='button' type='button' value='Delete' onClick='javascript:countChkBoxes()' />
+				<br/><br/>
+
+
+		";
 
 
 	/* drawing the number links */
@@ -112,7 +163,8 @@
 
 	while($row = $res->fetchRow()) {
 		echo "<tr>
-				<td> $row[0] </td>
+				<td> <input type='checkbox' name='username[]' value='$row[1]'>
+				$row[0] </td>
 				<td> $row[1] </td>
 				<td> $row[4] </td>
 				<td> <a href='mng-edit.php?username=$row[1]'> ".$l['all']['edit']." </a>
@@ -135,6 +187,9 @@
 		";
 
 	echo "</table>";
+	echo "<br/><br/> <center>";
+	echo "</center>";
+	echo "</form>";
 
 	include 'library/closedb.php';
 	
