44ac00a037f64e7ca8b52b9b88462590ebd5e4da
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2007-12-05 14:12:46 +0000

    finally, added php pear db error handling support for possible errors regarding the db

--- trunk/library/opendb.php
+++ trunk/library/opendb.php
@@ -4,17 +4,19 @@
 	// we introduced support for php's database abstraction layer which simplifies database connections
 	// to different technologies like mysql, oracle, postgresql, etc...
 	// until everything is completely migrated we will leave these commented out
-	
-	include_once ('DB.php');	
+
+	include_once ('DB.php');
+
 	$dbConnectString = $configValues['CONFIG_DB_ENGINE'] . "://".$configValues['CONFIG_DB_USER'].":".$configValues['CONFIG_DB_PASS']."@".$configValues['CONFIG_DB_HOST']."/".$configValues['CONFIG_DB_NAME'];
 	$dbSocket = DB::connect($dbConnectString);
 
-	// error handling support
-    // $dbSocket->setErrorHandling(PEAR_ERROR_PRINT, "Database query error: %s");
+	if (DB::isError ($dbSocket))
+		die ("<b>Datanase connection error</b><br/>
+			<b>Error Message</b>: " . $dbSocket->getMessage () . "<br/>" . 
+			"<b>Debug</b>: " . $dbSocket->getDebugInfo() . "<br/>");
+
 	
-	/*
-	mysql functions are obsolete now
-	$mysql_conn = mysql_connect($configValues['CONFIG_DB_HOST'], $configValues['CONFIG_DB_USER'], $configValues['CONFIG_DB_PASS']) or die ('Error connecting to MySQL Server');
-	mysql_select_db($configValues['CONFIG_DB_NAME']);
-	*/
+	include_once ('errorHandling.php');		// we declare the errorHandler() function in errorHandling.php
+
+	$dbSocket->setErrorHandling(PEAR_ERROR_CALLBACK, 'errorHandler');	// setting errorHandler function for the dbSocket obj
 ?>
