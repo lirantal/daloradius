99ba1b6960037cda8fdfb1b0fd2c0688592416fe
Liran Tal <liran.tal@gmail.com>
2007-12-07 11:21:14 +0000

    added new page accounting custom query, its a subnav for the accounting category

---
+++ acct-custom.php
@@ -0,0 +1,52 @@
+<?php
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+
+?>
+
+<?php
+	
+	include("menu-accounting-custom.php");
+	
+?>
+		
+		
+		
+		<div id="contentnorightbar">
+		
+		<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><? echo $l['Intro']['acctcustom.php']; ?>
+		<h144>+</h144></a></h2>
+				
+                <div id="helpPage" style="display:none;visibility:visible" >
+			<?php echo $l['helpPage']['acctcustom'] ?>
+			<br/>
+		</div>
+		<br/>
+
+
+<?php
+	include('include/config/logging.php');
+?>
+
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
