c3e1be799f41e61ba123a00ff951241177458d92
Liran Tal <liran.tal@gmail.com>
2008-01-02 17:58:37 +0000

    updated managment sub navigation links with introduction of new groups management in a single page

--- include/menu/management-subnav.php
+++ include/menu/management-subnav.php
@@ -2,8 +2,7 @@
 				<ul id="subnav">
 						<li><a href="mng-rad-nas.php"><em>N</em>as</a></li>
                         <li><a href="mng-rad-usergroup.php"><em>U</em>ser-group Mapping</a></li>
-                        <li><a href="mng-rad-groupreply.php"><em>G</em>roup-reply</a></li>
-						<li><a href="mng-rad-groupcheck.php"><em>G</em>roup-check</a></li>
+						<li><a href="mng-rad-groups.php"><em>G</em>roups</a></li>
 
 <div id="logindiv" style="text-align: right;">
 
