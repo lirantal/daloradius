54d4fe2cb77b1e8b6bb5a9554fffb406b3a764d7
Liran Tal <liran.tal@gmail.com>
2008-01-02 17:52:36 +0000

    updated menu pages for groups management

--- menu-mng-rad-groups.php
+++ menu-mng-rad-groups.php
@@ -32,6 +32,24 @@
                                                 <li><a href="mng-rad-groupreply-del.php"><b>&raquo;</b>Remove Groupl Reply Mapping</a></li>
                                 </ul>
 
+                                <h3>Group Check Management</h3>
+                                <ul class="subnav">
+
+                                                <li><a href="mng-rad-groupcheck-list.php"><b>&raquo;</b>List Group Check Mappings</a></li>
+                                                <li><a href="mng-rad-groupcheck-new.php"><b>&raquo;</b>New Group Check Mapping</a></li>
+                                                <li><a href="javascript:document.mngradgrpchkedit.submit();""><b>&raquo;</b>Edit Group Check Mapping<a>
+                                                        <form name="mngradgrpchkedit" action="mng-rad-groupcheck-edit.php" method="get" class="sidebar">
+                                                        <input name="groupname" type="text" value="[groupname]">
+                                                        <input name="value" type="text" value="[attribute]">
+                                                        </form></li>
+
+
+                                                <li><a href="mng-rad-groupcheck-del.php"><b>&raquo;</b>Remove Group Check Mapping</a></li>
+                                </ul>
+
+
+
+
 
 	
 		</div>
