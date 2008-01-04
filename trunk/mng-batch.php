6844d47877a99099c72c743bb4898953a44d8d95
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-01-04 12:54:17 +0000

    added populate_groups.php as a modular approach to populate the groups into a select box and also updated all other pages

--- trunk/mng-batch.php
+++ trunk/mng-batch.php
@@ -224,28 +224,9 @@
 </td><td>
 						<input value="<?php if (isset($group)) echo $group ?>" name="group" id="group" tabindex=104 />
 
-<select onChange="javascript:setStringText(this.id,'group')" id='usergroup' tabindex=105>
-        <option value=''>Select Group</option>
-<?php
-
-
-	include 'library/opendb.php';
-
-	// Grabing the group lists from usergroup table
-
-	$sql = "(SELECT distinct(GroupName) FROM ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY'].") UNION (SELECT distinct(GroupName) FROM ".$configValues['CONFIG_DB_TBL_RADGROUPCHECK'].");";
-	$res = $dbSocket->query($sql);
-
-	while($row = $res->fetchRow()) {
-		echo "  
-				<option value='$row[0]'> $row[0] </option>
-				";
-	}
-
-	include 'library/closedb.php';
+<?php   
+        include 'include/management/populate_groups.php';
 ?>
-
-</select>
 
 
 	
--- trunk/mng-new.php
+++ trunk/mng-new.php
@@ -270,27 +270,9 @@
 </td><td>
                                                 <input value="<?php if (isset($group)) echo $group ?>" name="group" id="group" tabindex=111 />
 
-<select onChange="javascript:setStringText(this.id,'group')" id='usergroup' tabindex=105>
-        <option value=''>Select Group</option>
 <?php
-
-        include 'library/opendb.php';
-
-        // Grabing the group lists from usergroup table
-
-	$sql = "(SELECT distinct(GroupName) FROM ".$configValues['CONFIG_DB_TBL_RADGROUPREPLY'].") UNION (SELECT distinct(GroupName) FROM ".$configValues['CONFIG_DB_TBL_RADGROUPCHECK'].");";
-        $res = $dbSocket->query($sql);
-
-        while($row = $res->fetchRow()) {
-                echo "
-                        <option value='$row[0]'> $row[0] </option>
-                        ";
-
-        }
-
-        include 'library/closedb.php';
+        include 'include/management/populate_groups.php';
 ?>
-</select>
 </td></tr>
 </table>
 
