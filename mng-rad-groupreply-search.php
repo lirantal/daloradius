3a770d6d7f86b405771d4dda0f3b402062a3ea9c
Liran Tal <liran.tal@gmail.com>
2008-01-05 17:45:37 +0000

    completed update support of all pages including database schemas to support the newly added group search pages

--- mng-rad-groupreply-search.php
+++ mng-rad-groupreply-search.php
@@ -3,7 +3,7 @@
     include ("library/checklogin.php");
     $operator = $_SESSION['operator_user'];
 
-//	include('library/check_operator_perm.php');
+	include('library/check_operator_perm.php');
 
 
 	//setting values for the order by and order type variables
