2d9960211f39dcf7de4617cdfc81c78a1d482a56
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-01-17 11:38:35 +0000

    updated interface layout and css settings

--- trunk/include/management/contactinfo.php
+++ trunk/include/management/contactinfo.php
@@ -8,44 +8,45 @@
 <fieldset>
 
 	<h302> Contact Info </h302>
+	<br/>
 
-	<label for='ownername'><?php echo $l['ContactInfo']['OwnerName'] ?></label>
+	<label for='ownername' class='form'><?php echo $l['ContactInfo']['OwnerName'] ?></label>
 	<input name='owner' type='text' id='owner' value='' tabindex=300 />
 	<br/>
 
-	<label for='emailowner'><?php echo $l['ContactInfo']['OwnerEmail'] ?></label>
+	<label for='emailowner' class='form'><?php echo $l['ContactInfo']['OwnerEmail'] ?></label>
 	<input name='email_owner' type='text' id='email_owner' value='' tabindex=301 />
 	<br/>
 
-	<label for='managername'><?php echo $l['ContactInfo']['ManagerName'] ?></label>
+	<label for='managername' class='form'><?php echo $l['ContactInfo']['ManagerName'] ?></label>
 	<input name='manager' type='text' id='manager' value='' tabindex=302 />
 	<br/>
 
-	<label for='emailmanager'><?php echo $l['ContactInfo']['ManagerEmail'] ?></label>
+	<label for='emailmanager' class='form'><?php echo $l['ContactInfo']['ManagerEmail'] ?></label>
 	<input name='email_manager' type='text' id='email_manager' value='' tabindex=303 />
 	<br/>
 
-	<label for='company'><?php echo $l['ContactInfo']['Company'] ?></label>
+	<label for='company' class='form'><?php echo $l['ContactInfo']['Company'] ?></label>
 	<input name='company' type='text' id='company' value='' tabindex=304 />
 	<br/>
 
-	<label for='address'><?php echo $l['ContactInfo']['Address'] ?></label>
+	<label for='address' class='form'><?php echo $l['ContactInfo']['Address'] ?></label>
 	<input name='address' type='text' id='address' value='' tabindex=305 />
 	<br/>
 
-	<label for='phone1'><?php echo $l['ContactInfo']['Phone1'] ?></label>
+	<label for='phone1' class='form'><?php echo $l['ContactInfo']['Phone1'] ?></label>
 	<input name='phone1' type='text' id='phone1' value='' tabindex=306 />
 	<br/>
 
-	<label for='phone2'><?php echo $l['ContactInfo']['Phone2'] ?></label>
+	<label for='phone2' class='form'><?php echo $l['ContactInfo']['Phone2'] ?></label>
 	<input name='phone1' type='text' id='phone2' value='' tabindex=307 />
 	<br/>
 
-	<label for='hotspot_type'><?php echo $l['ContactInfo']['HotspotType'] ?></label>
+	<label for='hotspot_type' class='form'><?php echo $l['ContactInfo']['HotspotType'] ?></label>
 	<input name='hotspot_type' type='text' id='hotspot_type' value='' tabindex=308 />
 	<br/>
 
-	<label for='website'><?php echo $l['ContactInfo']['Website'] ?></label>
+	<label for='website' class='form'><?php echo $l['ContactInfo']['Website'] ?></label>
 	<input name='website' type='text' id='website' value='' tabindex=309 />
 	<br/>
 
