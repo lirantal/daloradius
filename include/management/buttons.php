b2fd629fa4e30e1e538220b48b42ee622533d4c8
Liran Tal <liran.tal@gmail.com>
2008-01-21 08:15:35 +0000

    useful links for management pages

---
+++ include/management/buttons.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+        <input type='button' onclick="self.location='config-maint-test-user.php?username=<?php 
+		echo $username ?>&password=<?php echo $user_password ?>'" value='Test Connectivity' class='button'/>
+
+        <input type='button' onclick="self.location='acct-username.php?username=<?php echo $username ?>'" value='Accounting' class='button'/>
+
+        <input type='button' onclick="self.location='graphs-overall_logins.php?type=monthly&username=<?php
+                echo $username ?>'" value='Graphs - Logins' class='button'/>
+
+        <input type='button' onclick="self.location='graphs-overall_download.php?type=monthly&username=<?php
+                echo $username ?>'" value='Graphs - Downloads' class='button'/>
+
+        <input type='button' onclick="self.location='graphs-overall_upload.php?type=monthly&username=<?php
+                echo $username ?>'" value='Graphs - Uploads' class='button'/>
+
