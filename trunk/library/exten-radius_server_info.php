e04ed051c338f18d286daeff24da7432cd1badf6
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-01-24 20:33:02 +0000

    carlos cesario patches for the status pages

--- trunk/library/exten-radius_server_info.php
+++ trunk/library/exten-radius_server_info.php
@@ -11,19 +11,47 @@
 *                                                                  *
 *******************************************************************/
 
-system("pgrep mysql >/dev/null 2>&1", $ret_mysql);
-system("pgrep radius >/dev/null 2>&1", $ret_rad);
+//system("pgrep mysql >/dev/null 2>&1", $ret_mysql);
+//system("pgrep radius >/dev/null 2>&1", $ret_rad);
 
-if ($ret_rad == 0) {
-        echo "RADIUS is up <br/>";
-} else {
-        echo "RADIUS is down <br/>";
-}
+//if ($ret_rad == 0) {
+//        echo "RADIUS is up <br/>";
+//} else {
+//        echo "RADIUS is down <br/>";
+//}
 
-if ($ret_mysql == 0) {
-        echo "MySQL is up <br/>";
-} else {
-        echo "MySQL is down <br/>";
+//if ($ret_mysql == 0) {
+//        echo "MySQL is up <br/>";
+//} else {
+//      echo "MySQL is down <br/>";
+//}
+
+function check_service($sname) {
+	if ($sname != '') {
+		system("pgrep $sname >/dev/null 2>&1", $ret_service);
+		if ($ret_service == 0) {
+			return "is up and running...";
+		} else {
+			return "down";
+		}
+	} else {
+		return "no service name";
+	}
 }
 
 ?>
+
+<?php
+echo "<h3>Service Status</h3>";
+?>
+
+<table class='summarySection'>
+  <tr>
+    <td class='summaryKey'> Radius </td>
+    <td class='summaryValue'><span class='sleft'><?php echo check_service("radius"); ?></span> </td>
+  </tr>
+  <tr>
+    <td class='summaryKey'> Mysql </td>
+    <td class='summaryValue'><span class='sleft'><?php echo check_service("mysql");  ?></span> </td>
+  </tr>
+</table>
--- trunk/library/exten-server_info.php
+++ trunk/library/exten-server_info.php
@@ -7,46 +7,303 @@
 * displays it                                                      *
 *                                                                  *
 * Author: Liran Tal <liran@enginx.com>                             *
+* 	  Carlos Cesario <carloscesario@gmail.com>		   *
 *                                                                  *
 *******************************************************************/
 
-echo "<b> System Name: </b>";
-echo "<br/>";
-system("uname -a");
-echo "<br/>";
-echo "<br/>";
+//Functions
 
-echo "<b> System Uptime: </b>";
-echo "<br/>";
-$ret = shell_exec("uptime");
-echo $ret;
-echo "<br/>";
-echo "<br/>";
+/**
+ * Display uptime system
+ *
+ * @return string Return uptime system
+ */
+function uptime() {
+        $file_name = "/proc/uptime";
 
-echo "<b> CPU: </b>";
-echo "<br/>";
-$ret = shell_exec("cat /proc/cpuinfo | egrep \"vendor|model name|cpu MHz\"");
-$ret = eregi_replace("\n", "<br>", $ret);
-echo $ret;
-echo "<br/>";
-echo "<br/>";
+        $fopen_file = fopen($file_name, 'r');
+        $buffer = split(' ', fgets($fopen_file, 4096));
+        fclose($fopen_file);
 
-echo "<b> Memory:</b>";
-echo "<br/>";
-$ret = shell_exec("free -m");
-$ret = eregi_replace("\n", "<br>", $ret);
-echo $ret;
-echo "<br/>";
-echo "<br/>";
+        $sys_ticks = trim($buffer[0]);
+        $min = $sys_ticks / 60;
+        $hours = $min / 60;
+        $days = floor($hours / 24);
+        $hours = floor($hours - ($days * 24));
+        $min = floor($min - ($days * 60 * 24) - ($hours * 60));
+        $result = "";
+        if ($days != 0) {
+                if ($days > 1)
+                        $result = "$days " . " days ";
+                else
+                        $result = "$days " . " day ";
+        }
+        if ($hours != 0) {
+                if ($hours > 1)
+                        $result .= "$hours " . " hours ";
+                else
+                        $result .= "$hours " . " hour ";
+        }
+        if ($min > 1 || $min == 0)
+                $result .= "$min " . " minutes ";
+        elseif ($min == 1)
+                $result .= "$min " . " minute ";
+
+        return $result;
+}
 
 
-echo "<b> Disk Usage: </b>";
-echo "<br/>";
-$ret = shell_exec("df -h");
-$ret = eregi_replace("\n", "<br>", $ret);
-echo $ret;
-echo "<br/>";
-echo "<br/>";
+
+
+
+/**
+ * Display hostname system
+ *
+ * @return string System hostname or none
+ */
+
+function get_hostname() {
+        $file_name = "/proc/sys/kernel/hostname";
+
+        if ($fopen_file = fopen($file_name, 'r')) {
+                $result = trim(fgets($fopen_file, 4096));
+                fclose($fopen_file);
+        } else {
+                $result = "(none)";
+        }
+        return $result;
+}
+
+
+/**
+ * Display currenty date/time
+ *
+ * @return string Current system date/time or none
+ */
+
+function get_datetime() {
+        if ($today = date("F j, Y, g:i a")) {
+                $result = $today;
+        } else {
+                $result = "(none)";
+        }
+        return $result;
+}
+
+
+/**
+ * Get Memory System MemTotal|MemFree
+ *
+ * @return array Memory System MemTotal|MemFree
+ */
+function get_memory() {
+        $file_name = "/proc/meminfo";
+        $mem_array = array();
+
+        $buffer = file($file_name);
+
+        while (list($key, $value) = each($buffer)) {
+                if (strpos($value, ':') !== false) {
+                        $match_line = explode(':', $value);
+                        $match_value = explode(' ', trim($match_line[1]));
+                        if (is_numeric($match_value[0])) {
+                                $mem_array[trim($match_line[0])] = trim($match_value[0]);
+                        }
+                }
+        }
+        return $mem_array;
+}
+
+
+
+
+/**
+ * Convert value to MB
+ *
+ * @param decimal $value
+ * @return int Memory MB
+ */
+function convert_ToMB($value) {
+        return round($value / 1024) . " MB\n";
+}
+
+
+/**
+ * Get all network names devices (eth[0-9])
+ *
+ * @return array Get list network name interfaces
+ */
+function get_interface_list() {
+        $devices = array();
+        $file_name = "/proc/net/dev";
+
+        if ($fopen_file = fopen($file_name, 'r')) {
+                while ($buffer = fgets($fopen_file, 4096)) {
+                        if (preg_match("/eth[0-9][0-9]*/i", trim($buffer), $match)) {
+                                $devices[] = $match[0];
+                        }
+                }
+                $devices = array_unique($devices);
+                sort($devices);
+                fclose ($fopen_file);
+        }
+        return $devices;
+}
+
+/**
+ * Get ip address
+ *
+ * @param string $ifname
+ * @return string Ip address or (none)
+ */
+function get_ip_addr($ifname) {
+        $command_name = "/sbin/ifconfig $ifname";
+        $ifip = "";
+
+        exec($command_name , $command_result);
+
+        $ifip = implode($command_result, "\n");
+        if (preg_match("/inet addr:[0-9\.]*/i", $ifip, $match)) {
+                $match = split(":", $match[0]);
+                return $match[1];
+        } else {
+                return "(none)";
+        }
+}
+
+/**
+ * Get mac address
+ *
+ * @param string $ifname
+ * @return string Mac address or (none)
+ */
+function get_mac_addr($ifname) {
+        $command_name = "/sbin/ifconfig $ifname";
+        $ifip = "";
+
+        exec($command_name , $command_result);
+
+        $ifmac = implode($command_result, "\n");
+        if (preg_match("/hwaddr [0-9A-F:]*/i", $ifmac, $match)) {
+                $match = split(" ", $match[0]);
+                return $match[1];
+        } else {
+                return "(none)";
+        }
+}
+
+
+/**
+ * Get netmask address
+ *
+ * @param string $ifname
+ * @return string Netmask address or (none)
+ */
+function get_mask_addr($ifname) {
+        $command_name = "/sbin/ifconfig $ifname";
+        $ifmask = "";
+
+        exec($command_name , $command_result);
+
+        $ifmask = implode($command_result, "\n");
+        if (preg_match("/mask:[0-9\.]*/i", $ifmask, $match)) {
+                $match = split(":", $match[0]);
+                return $match[1];
+        } else {
+                return "(none)";
+        }
+}
 
 ?>
 
+
+<?php
+echo "<h3>General Information</h3>";
+?>
+
+<table class='summarySection'>
+  <tr>
+    <td class='summaryKey'> Uptime </td>
+    <td class='summaryValue'><span class='sleft'><?php echo uptime(); ?></span> </td>
+  </tr>
+  <tr>
+    <td class='summaryKey'> Hostname </td>
+    <td class='summaryValue'><span class='sleft'><?php echo get_hostname(); ?></span> </td>
+  </tr>
+  <tr>
+    <td class='summaryKey'> Current Date </td>
+    <td class='summaryValue'><span class='sleft'><?php echo get_datetime(); ?></span> </td>
+  </tr>
+</table>
+
+
+<?php
+echo "<h3>Memory Information</h3>";
+$meminfo = get_memory();
+?>
+
+
+<table class='summarySection'>
+  <tr>
+    <td class='summaryKey'> Mem. Total </td>
+    <td class='summaryValue'><span class='sleft'><?php echo convert_ToMB ($meminfo[MemTotal]); ?></span> </td>
+  </tr>
+  <tr>
+    <td class='summaryKey'> Mem. Free </td>
+    <td class='summaryValue'><span class='sleft'><?php echo convert_ToMB ($meminfo[MemFree]); ?></span> </td>
+  </tr>
+  <tr>
+    <td class='summaryKey'> Mem. Used </td>
+    <td class='summaryValue'>
+                <span class='sleft'>
+                        <?php 
+                                $memused = ($meminfo[MemTotal] - $meminfo[MemFree]); 
+                                echo convert_ToMB ($memused);  
+                        ?>
+                </span> </td>
+  </tr>
+</table>
+
+
+<?php
+echo "<h3>Network Interfaces</h3>";
+$iflist = get_interface_list();
+
+foreach ($iflist as $ifname) {
+        echo "\t<table class='summarySection'>\n";
+        echo "\t<tr>\n";
+        echo "\t\t<td class='summaryKey'></td>\n";
+        echo "\t\t<td class='stitle'>\n";
+        echo "\t\t\t$ifname\n";
+        echo "\t\t</td>\n";
+        echo "\t</tr>\n";
+        echo "\t<tr>\n";
+                echo "\t<tr>\n";
+                echo "\t\t<td class='summaryKey'>\n";
+                echo "\t\t\tIp\n";
+                echo "\t\t</td>\n";
+                echo "\t\t<td class='summaryValue'>\n";
+                echo "\t\t\t".get_ip_addr($ifname)."\n";
+                echo "\t\t</td>\n";
+                echo "\t</tr>\n";
+
+                echo "\t<tr>\n";
+                echo "\t\t<td class='summaryKey'>\n";
+                echo "\t\t\tMask\n";
+                echo "\t\t</td>\n";
+                echo "\t\t<td class='summaryValue'>\n";
+                echo "\t\t\t".get_mask_addr($ifname)."\n";
+                echo "\t\t</td>\n";
+                echo "\t</tr>\n";
+
+		echo "\t<tr>\n";
+		echo "\t\t<td class='summaryKey'>\n";
+		echo "\t\t\tMAC address\n";
+		echo "\t\t<td class='summaryValue'>\n";
+		echo "\t\t\t".get_mac_addr($ifname)."\n";
+		echo "\t\t</td>\n";
+		echo "\t</tr>\n";
+
+        }
+        echo "\t</table>\n";
+?>
