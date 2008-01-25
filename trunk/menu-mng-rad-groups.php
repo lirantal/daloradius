c56cff66d9e2e3fe42d9f6e1dc19f222beb23c57
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-01-25 21:07:22 +0000

    html code cleanup

--- trunk/menu-mng-rad-groups.php
+++ trunk/menu-mng-rad-groups.php
@@ -11,59 +11,54 @@
     include_once ("include/menu/menu-items.php");
 	include_once ("include/menu/management-subnav.php");
 ?>
+
+<div id="sidebar">
+
+	<h2>Management</h2>
+
+	<h3>Group Reply Management</h3>
+	<ul class="subnav">
+
+		<li><a href="mng-rad-groupreply-list.php"><b>&raquo;</b>List Group Reply Mappings</a></li>
+		<li><a href="javascript:document.mngradgroupreplysearch.submit();""><b>&raquo;</b>Search Group Reply<a>
+			<form name="mngradgroupreplysearch" action="mng-rad-groupreply-search.php" method="get" 
+				class="sidebar">
+			<input name="groupname" type="text" 
+				value="<?php if (isset($search_groupname)) echo $search_groupname; ?>" tabindex=2>
+			</form></li>
+
+		<li><a href="mng-rad-groupreply-new.php"><b>&raquo;</b>New Group Reply Mapping</a></li>
+		<li><a href="javascript:document.mngradgrprplyedit.submit();""><b>&raquo;</b>Edit Group Reply Mapping<a>
+			<form name="mngradgrprplyedit" action="mng-rad-groupreply-edit.php" method="get" class="sidebar">
+			<input name="groupname" type="text" value="[groupname]">
+			<input name="value" type="text" value="[attribute]">
+			</form></li>
+		<li><a href="mng-rad-groupreply-del.php"><b>&raquo;</b>Remove Groupl Reply Mapping</a></li>
 		
-		<div id="sidebar">
+	</ul>
+
+	<h3>Group Check Management</h3>
+	<ul class="subnav">
+
+		<li><a href="mng-rad-groupcheck-list.php"><b>&raquo;</b>List Group Check Mappings</a></li>
+		<li><a href="javascript:document.mngradgroupchecksearch.submit();""><b>&raquo;</b>Search Group Check<a>
+			<form name="mngradgroupchecksearch" action="mng-rad-groupcheck-search.php" method="get" 
+				class="sidebar">
+			<input name="groupname" type="text" 
+				value="<?php if (isset($search_groupname)) echo $search_groupname; ?>" tabindex=2>
+			</form></li>
+
+		<li><a href="mng-rad-groupcheck-new.php"><b>&raquo;</b>New Group Check Mapping</a></li>
+		<li><a href="javascript:document.mngradgrpchkedit.submit();""><b>&raquo;</b>Edit Group Check Mapping<a>
+			<form name="mngradgrpchkedit" action="mng-rad-groupcheck-edit.php" method="get" class="sidebar">
+			<input name="groupname" type="text" value="[groupname]">
+			<input name="value" type="text" value="[attribute]">
+			</form></li>
+		<li><a href="mng-rad-groupcheck-del.php"><b>&raquo;</b>Remove Group Check Mapping</a></li>
 		
-	
-                                <h2>Management</h2>
+	</ul>
 
-                                <h3>Group Reply Management</h3>
-                                <ul class="subnav">
-
-                                                <li><a href="mng-rad-groupreply-list.php"><b>&raquo;</b>List Group Reply Mappings</a></li>
-                                                <li><a href="javascript:document.mngradgroupreplysearch.submit();""><b>&raquo;</b>Search Group Reply<a>
-                                                        <form name="mngradgroupreplysearch" action="mng-rad-groupreply-search.php" method="get" class="sidebar">
-                                                        <input name="groupname" type="text" value="<?php if (isset($search_groupname)) echo $search_groupname; ?>" tabindex=2>
-                                                        </form></li>
-
-
-                                                <li><a href="mng-rad-groupreply-new.php"><b>&raquo;</b>New Group Reply Mapping</a></li>
-                                                <li><a href="javascript:document.mngradgrprplyedit.submit();""><b>&raquo;</b>Edit Group Reply Mapping<a>
-                                                        <form name="mngradgrprplyedit" action="mng-rad-groupreply-edit.php" method="get" class="sidebar">
-                                                        <input name="groupname" type="text" value="[groupname]">
-                                                        <input name="value" type="text" value="[attribute]">
-                                                        </form></li>
-
-
-                                                <li><a href="mng-rad-groupreply-del.php"><b>&raquo;</b>Remove Groupl Reply Mapping</a></li>
-                                </ul>
-
-                                <h3>Group Check Management</h3>
-                                <ul class="subnav">
-
-                                                <li><a href="mng-rad-groupcheck-list.php"><b>&raquo;</b>List Group Check Mappings</a></li>
-                                                <li><a href="javascript:document.mngradgroupchecksearch.submit();""><b>&raquo;</b>Search Group Check<a>
-                                                        <form name="mngradgroupchecksearch" action="mng-rad-groupcheck-search.php" method="get" class="sidebar">
-                                                        <input name="groupname" type="text" value="<?php if (isset($search_groupname)) echo $search_groupname; ?>" tabindex=2>
-                                                        </form></li>
-
-                                                <li><a href="mng-rad-groupcheck-new.php"><b>&raquo;</b>New Group Check Mapping</a></li>
-                                                <li><a href="javascript:document.mngradgrpchkedit.submit();""><b>&raquo;</b>Edit Group Check Mapping<a>
-                                                        <form name="mngradgrpchkedit" action="mng-rad-groupcheck-edit.php" method="get" class="sidebar">
-                                                        <input name="groupname" type="text" value="[groupname]">
-                                                        <input name="value" type="text" value="[attribute]">
-                                                        </form></li>
-
-
-                                                <li><a href="mng-rad-groupcheck-del.php"><b>&raquo;</b>Remove Group Check Mapping</a></li>
-                                </ul>
-
-
-
-
-
-	
-		</div>
+</div>
 
 <?php
 
