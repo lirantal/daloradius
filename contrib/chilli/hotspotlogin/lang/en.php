559cbc902923805846237961d5563de082f5539c
Liran Tal <liran.tal@gmail.com>
2008-02-23 21:18:19 +0000

    added hotspotlogin package for easy templating of the login form, logout, failed login and other windows. The template used in this form belongs to TemplateFusion and is provided it's due credit in the form of a link as stated by it's license

---
+++ contrib/chilli/hotspotlogin/lang/en.php
@@ -0,0 +1,21 @@
+<?php
+
+//$l['all']['HotspotName'] = "ChilliSpot";
+
+$ChilliSpot="ChilliSpot";
+$title="$ChilliSpot Login";
+$centerUsername="Username";
+$centerPassword="Password";
+$centerLogin="Login";
+$centerPleasewait="Please wait.......";
+$centerLogout="Logout";
+$h1Login="$ChilliSpot Login";
+$h1Failed="$ChilliSpot Login Failed";  
+$h1Loggedin="Logged in to $ChilliSpot";
+$h1Loggingin="Logging in to $ChilliSpot";
+$h1Loggedout="Logged out from $ChilliSpot";
+$centerdaemon="Login must be performed through $ChilliSpot daemon";
+$centerencrypted="Login must use encrypted connection";
+
+
+?>
