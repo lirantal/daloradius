c4c84bdc483d0bc0382ed890fc2bd642d849dee1
Liran Tal <liran.tal@gmail.com>
2008-03-10 17:38:42 +0000

    adding new management pages to handle realms support

---
+++ mng-rad-realms-del.php
@@ -0,0 +1,125 @@
+<?php 
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+	include('library/check_operator_perm.php');
+
+        isset($_REQUEST['realmname']) ? $realmnameArray = $_REQUEST['realmname'] : $realmnameArray = "";
+	$logDebugSQL = "";
+
+        if (isset($_REQUEST['realmname'])) {
+
+                if (!is_array($realmnameArray))
+                        $realmnameArray = array($realmnameArray, NULL);
+
+		$allRealms = "";
+
+		include 'library/opendb.php';
+	
+                foreach ($realmnameArray as $variable=>$value) {
+			if (trim($value) != "") {
+
+                                $realmname = $value;
+                                $allRealms .= $realmname . ", ";
+
+				// delete all realms
+				$sql = "DELETE FROM ".$configValues['CONFIG_DB_TBL_DALOREALMS']." WHERE realmname='".$dbSocket->escapeSimple($realmname)."'";
+				$res = $dbSocket->query($sql);
+				$logDebugSQL .= $sql . "\n";
+				
+				$actionStatus = "success";
+				$actionMsg = "Deleted realm(s): <b> $allRealms </b>";
+				$logAction = "Successfully deleted realm(s) [$allRealms] on page: ";
+				
+			} else { 
+				$actionStatus = "failure";
+				$actionMsg = "no realm was entered, please specify a realm name to remove from database";
+				$logAction = "Failed deleting realm(s) [$allRealms] on page: ";
+			}
+
+		} //foreach
+
+		include 'library/closedb.php';
+
+	} 
+
+
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+
+?>
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+</head>
+<script src="library/javascript/pages_common.js" type="text/javascript"></script>
+<?php
+
+	include ("menu-mng-rad-realms.php");
+	
+?>		
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><?php echo $l['Intro']['mngradrealmsdel.php'] ?>
+				<h144>+</h144></a></h2>
+				
+				<div id="helpPage" style="display:none;visibility:visible" >
+					<?php echo $l['helpPage']['mngradrealmsdel'] ?>
+					<br/>
+				</div>
+				<br/>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+
+        <fieldset>
+
+                <h302> <?php echo $l['title']['RealmInfo'] ?> </h302>
+		<br/>
+
+                <label for='realmname' class='form'><?php echo $l['all']['RealmName'] ?></label>
+                <input name='realmname[]' type='text' id='realmname' value='<?php echo $realmname ?>' tabindex=100 />
+                <br/>
+
+                <br/><br/>
+                <hr><br/>
+
+                <input type='submit' name='submit' value='<?php echo $l['buttons']['apply'] ?>' tabindex=1000 
+			class='button' />
+
+	</fieldset>
+
+				</form>
+
+
+<?php
+	include('include/config/logging.php');
+?>
+		
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
+
+
+
+
+
---
+++ mng-rad-realms-list.php
@@ -0,0 +1,156 @@
+<?php
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+	include('library/check_operator_perm.php');
+
+
+	//setting values for the order by and order type variables
+	isset($_REQUEST['orderBy']) ? $orderBy = $_REQUEST['orderBy'] : $orderBy = "RealmName";
+	isset($_REQUEST['orderType']) ? $orderType = $_REQUEST['orderType'] : $orderType = "ASC";
+
+
+
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+    $logQuery = "performed query for listing of records on page: ";
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+
+<script src="library/javascript/pages_common.js" type="text/javascript"></script>
+
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+
+</head>
+ 
+ 
+<?php
+	include ("menu-mng-rad-realms.php");
+?>
+		
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><?php echo $l['Intro']['mngradrealms.php'] ?>
+				<h144>+</h144></a></h2>
+
+				<div id="helpPage" style="display:none;visibility:visible" >				
+					<?php echo $l['helpPage']['mngradrealmslist'] ?>
+					<br/>
+				</div>	
+				<br/>
+				
+<?php
+
+	include 'library/opendb.php';
+	include 'include/management/pages_numbering.php';		// must be included after opendb because it needs to read the CONFIG_IFACE_TABLES_LISTING variable from the config file
+
+	//orig: used as maethod to get total rows - this is required for the pages_numbering.php page	
+	$sql = "SELECT * FROM ".$configValues['CONFIG_DB_TBL_DALOREALMS']." ";
+	$res = $dbSocket->query($sql);
+	$numrows = $res->numRows();
+
+	$sql = "SELECT * FROM ".$configValues['CONFIG_DB_TBL_DALOREALMS'].
+		" ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage;";
+	$res = $dbSocket->query($sql);
+	$logDebugSQL = "";
+	$logDebugSQL .= $sql . "\n";
+	
+	/* START - Related to pages_numbering.php */
+	$maxPage = ceil($numrows/$rowsPerPage);
+	/* END */
+
+        echo "<form name='listrealms' method='post' action='mng-rad-realms-del.php'>";
+	
+	echo "<table border='0' class='table1'>\n";
+	echo "
+					<thead>
+							<tr>
+							<th colspan='10'>".$l['all']['Records']."</th>
+							</tr>
+                                                        <tr>
+                                                        <th colspan='10' align='left'>
+
+                                Select:
+                                <a class=\"table\" href=\"javascript:SetChecked(1,'realmname[]','listrealms')\">All</a>
+
+                                <a class=\"table\" href=\"javascript:SetChecked(0,'realmname[]','listrealms')\">None</a>
+                                <br/>
+                                <input class='button' type='button' value='Delete' onClick='javascript:removeCheckbox(\"listrealms\",\"mng-rad-realms-del.php\")' />
+                                <br/><br/>
+
+
+                ";
+
+        if ($configValues['CONFIG_IFACE_TABLES_LISTING_NUM'] == "yes")
+                setupNumbering($numrows, $rowsPerPage, $pageNum, $orderBy, $orderType);
+
+        echo " </th></tr>
+                                        </thead>
+
+                        ";
+
+
+	echo "<thread> <tr>
+		<th scope='col'>
+		<a class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=realmname&orderType=asc\">
+			<img src='images/icons/arrow_up.png' alt='>' border='0' /></a>
+		".$l['all']['RealmName']."
+		<a class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=realmname&orderType=desc\">
+			<img src='images/icons/arrow_down.png' alt='<' border='0' /></a>
+		</th>
+	</tr> </thread>";
+	while($row = $res->fetchRow()) {
+		echo "<tr>
+				<td> <input type='checkbox' name='realmname[]' value='$row[1]'>
+					<a class='tablenovisit' href='mng-rad-realms-edit.php?realmname=$row[1]'> $row[1] </a> </td>
+		</tr>";
+	}
+
+        echo "
+                                        <tfoot>
+                                                        <tr>
+                                                        <th colspan='10' align='left'>
+        ";
+        setupLinks($pageNum, $maxPage, $orderBy, $orderType);
+        echo "
+                                                        </th>
+                                                        </tr>
+                                        </tfoot>
+                ";
+
+
+	echo "</table>";
+	echo "</form>";
+
+	include 'library/closedb.php';
+?>
+
+
+<?php
+	include('include/config/logging.php');
+?>
+				
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
