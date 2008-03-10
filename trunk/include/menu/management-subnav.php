0a1c04893162f81a4bad68bdd282cc8e0784e1e4
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-03-10 17:46:36 +0000

    added realms management sub nav

--- trunk/include/menu/management-subnav.php
+++ trunk/include/menu/management-subnav.php
@@ -3,11 +3,12 @@
 						<li><a href="mng-users.php"><em>U</em>sers</a></li>
 						<li><a href="mng-hs.php"><em>H</em>otspots</a></li>
 						<li><a href="mng-rad-nas.php"><em>N</em>as</a></li>
-                        <li><a href="mng-rad-usergroup.php"><em>U</em>ser-group Mapping</a></li>
+                        <li><a href="mng-rad-usergroup.php"><em>U</em>ser-Groups</a></li>
                          <li><a href="mng-rad-profiles.php"><em>P</em>rofiles</a></li>
 						<li><a href="mng-rad-groups.php"><em>G</em>roups</a></li>
 
 						<li><a href="mng-rad-attributes.php"><em>A</em>ttributes</a></li>
+						<li><a href="mng-rad-realms.php"><em>R</em>ealms/Proxys	</a></li>
 
 <div id="logindiv" style="text-align: right;">
 
