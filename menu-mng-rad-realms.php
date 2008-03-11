7fec0e701ee54ff323a625cb3ceb677bbd011d54
Liran Tal <liran.tal@gmail.com>
2008-03-11 21:43:38 +0000

    adding new proxy management pages

--- menu-mng-rad-realms.php
+++ menu-mng-rad-realms.php
@@ -25,12 +25,28 @@
 		<li><a href="javascript:document.mngradrealmedit.submit();""><b>&raquo;</b><?php echo $l['button']['EditRealm'] ?><a>
 			<form name="mngradrealmedit" action="mng-rad-realms-edit.php" method="get" class="sidebar">
 			<?php   
-				include 'include/management/populate_selectbox.php';
+				include_once('include/management/populate_selectbox.php');
 				populate_realms("Select Realm","realmname","");
 			?>
 			</form></li>
 
 		<li><a href="mng-rad-realms-del.php"><b>&raquo;</b><?php echo $l['button']['RemoveRealm'] ?></a></li>
+	</ul>
+
+
+	<h3>Proxys Management</h3>
+	<ul class="subnav">
+
+		<li><a href="mng-rad-proxys-list.php"><b>&raquo;</b><?php echo $l['button']['ListProxys'] ?></a></li>
+		<li><a href="mng-rad-proxys-new.php"><b>&raquo;</b><?php echo $l['button']['NewProxy'] ?></a></li>
+		<li><a href="javascript:document.mngradproxyedit.submit();""><b>&raquo;</b><?php echo $l['button']['EditProxy'] ?><a>
+			<form name="mngradproxyedit" action="mng-rad-proxys-edit.php" method="get" class="sidebar">
+			<?php   
+				populate_proxys("Select Proxy","proxyname","");
+			?>
+			</form></li>
+
+		<li><a href="mng-rad-proxys-del.php"><b>&raquo;</b><?php echo $l['button']['RemoveProxy'] ?></a></li>
 	</ul>
 
 </div>
