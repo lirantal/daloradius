7b5e8cec4978ddba912455326093335e63a3dd54
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-03-26 22:13:03 +0000

    code cleanup

--- trunk/library/check_operator_perm.php
+++ trunk/library/check_operator_perm.php
@@ -1,8 +1,8 @@
 <?php
 
     include 'library/opendb.php';
-    $sql = "SELECT * FROM ".$configValues['CONFIG_DB_TBL_DALOOPERATOR']." WHERE 
-UserName='".$dbSocket->escapeSimple($operator)."'";
+    $sql = "SELECT * FROM ".$configValues['CONFIG_DB_TBL_DALOOPERATOR']." WHERE UserName='".
+		$dbSocket->escapeSimple($operator)."'";
     $res = $dbSocket->query($sql);
     $row = $res->fetchRow(DB_FETCHMODE_ASSOC);
 
@@ -11,13 +11,12 @@ UserName='".$dbSocket->escapeSimple($operator)."'";
 	$currFile = str_replace(".php", "", $currFile);
 
 	// the importance of the following is not to be discarded.
-	// the following tests if the page is defined and valid in the  include/management/operator_tables.php array and if it isn't
-	// it will force the page to not be displayed. meaning that all pages (for example newer pages) must be defined in that array
-	// otherwise they will not be accessible.
+	// the following tests if the page is defined and valid in the  include/management/operator_tables.php array 
+	// and if it isn't it will force the page to not be displayed. meaning that all pages (for example newer pages) 
+	// must be defined in that array otherwise they will not be accessible.
 	isset($row[$currFile]) ? $test = $row[$currFile] : $test="no";
 	
 	if (!( (strcasecmp($test, "y") == 0) || (strcasecmp($test, "yes") == 0) || (strcasecmp($test, "on") == 0)   )) {
-//		echo "<br/><br/> permission denied! <br/><br/>";
 		header('Location: msg-error-permissions.php');
 		exit;
 	}
