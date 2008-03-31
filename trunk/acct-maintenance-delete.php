476bbc8a0bf6e1d5a920f55f65c2c330d3928138
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-03-31 11:57:19 +0000

    adding new accounting maintenance pages

---
+++ trunk/acct-maintenance-delete.php
@@ -0,0 +1,152 @@
+<?php 
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+	include('library/check_operator_perm.php');
+
+	isset($_REQUEST['enddate']) ? $enddate = $_REQUEST['enddate'] : $enddate = "";
+	isset($_REQUEST['startdate']) ? $startdate = $_REQUEST['startdate'] : $startdate = "";
+	isset($_REQUEST['username']) ? $username = $_REQUEST['username'] : $username = "";
+
+	$logDebugSQL = "";
+
+        if (isset($_POST['submit'])) {
+
+		if ( (trim($startdate) != "") || (trim($enddate) != "") || (trim($username) != "") ) {
+			
+			include 'library/opendb.php';
+
+			$deleteUsername = "";
+			if (trim($username) != "")
+				$deleteUsername = " AND Username='$username'";
+
+			$deleteEnddate = "";
+			if (trim($enddate) != "")
+				$deleteEnddate = " AND AcctStartTime<'".$dbSocket->escapeSimple($enddate)."'";
+
+			$deleteStartdate = "";
+			if (trim($startdate) != "")
+				$deleteStartdate = " AND AcctStartTime>'".$dbSocket->escapeSimple($startdate)."'";
+
+
+			$sql = "DELETE FROM ".$configValues['CONFIG_DB_TBL_RADACCT'].
+				" WHERE 1=1".
+				" $deleteStartdate".
+				" $deleteEnddate".
+				" $deleteUsername ";
+			$res = $dbSocket->query($sql);
+			$logDebugSQL .= $sql . "\n";
+
+			$actionStatus = "success";
+			$actionMsg = "Deleted records between <b>$startdate</b> to <b>$enddate</b> for user <b>$username</b>";
+			$logAction = "Successfully deleted records between [$startdate] and [$enddate] for user [$username] on page: ";
+
+			include 'library/closedb.php';
+
+		}  else { 
+			$actionStatus = "failure";
+			$actionMsg = "no username, ending date or starting date was provided, please at least one of those";
+			$logAction = "Failed deleting records from database, missing fields on page: ";
+		}
+
+	}
+
+
+	include_once('library/config_read.php');
+	$log = "visited page: ";
+
+?>
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+<link rel="stylesheet" type="text/css" href="library/js_date/datechooser.css">
+<!--[if lte IE 6.5]>
+<link rel="stylesheet" type="text/css" href="library/js_date/select-free.css"/>
+<![endif]-->
+</head>
+
+<script src="library/js_date/date-functions.js" type="text/javascript"></script>
+<script src="library/js_date/datechooser.js" type="text/javascript"></script>
+<script src="library/javascript/pages_common.js" type="text/javascript"></script>
+ 
+<?php
+	include ("menu-accounting-maintenance.php");	
+?>
+		
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><?php echo $l['Intro']['acctmaintenancedelete.php'] ?>
+				<h144>+</h144></a></h2>
+				
+				<div id="helpPage" style="display:none;visibility:visible" >
+					<?php echo $l['helpPage']['acctmaintenancedelete'] ?>
+					<br/>
+				</div>
+				
+				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <fieldset>
+
+                <h302> <?php echo $l['title']['DeleteRecords'] ?> </h302>
+		<br/>
+
+                <label for='username' class='form'><?php echo $l['all']['Username']?></label>
+                <input name='username' type='text' id='username' value='<?php echo $username ?>' tabindex=100 />
+                <br />
+
+                <label for='startdate' class='form'><?php echo $l['all']['StartingDate']?></label>
+                <input name='startdate' type='text' id='startdate' value='<?php echo $startdate ?>' tabindex=100 />
+			<img src="library/js_date/calendar.gif" onclick=
+				"showChooser(this, 'startdate', 'chooserSpan', 1950, 2010, 'Y-m-d h:i:s', true);" >
+                <br />
+
+                <label for='enddate' class='form'><?php echo $l['all']['EndingDate']?></label>
+                <input name='enddate' type='text' id='enddate' value='<?php echo $enddate ?>' tabindex=100 />
+			<img src="library/js_date/calendar.gif" onclick=
+				"showChooser(this, 'enddate', 'chooserSpan', 1950, 2010, 'Y-m-d h:i:s', true);" >
+                <br />
+
+		<br/><br/>
+		<hr><br/>
+		<input type="submit" name="submit" value="<?php echo $l['buttons']['apply'] ?>" tabindex=1000 
+			class='button' />
+
+	</fieldset>
+
+	<div id="chooserSpan" class="dateChooser select-free" 
+		style="display: none; visibility: hidden; width: 160px;">
+	</div>
+
+				</form>
+
+<?php
+	include('include/config/logging.php');
+?>
+		
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
+
+
+
+
+
---
+++ trunk/menu-accounting-maintenance.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+<link rel="stylesheet" type="text/css" href="library/js_date/datechooser.css">
+<!--[if lte IE 6.5]>
+<link rel="stylesheet" type="text/css" href="library/js_date/select-free.css"/>
+<![endif]-->
+
+</head>
+<script src="library/js_date/date-functions.js" type="text/javascript"></script>
+<script src="library/js_date/datechooser.js" type="text/javascript"></script>
+<script src="library/javascript/pages_common.js" type="text/javascript"></script>
+
+<body>
+
+<?php
+    include_once ("lang/main.php");
+?>
+
+<div id="wrapper">
+<div id="innerwrapper">
+
+<?php
+	$m_active = "Accounting";
+	include_once ("include/menu/menu-items.php");
+	include_once ("include/menu/accounting-subnav.php");
+?>
+
+<div id="sidebar">
+
+	<h2>Accounting</h2>
+	
+	<h3>Maintenance</h3>
+	<ul class="subnav">
+
+		<li><a href="acct-maintenance-cleanup.php"><b>&raquo;</b><?php echo $l['button']['CleanupStaleSessions'] ?>
+			</a></li>
+		<li><a href="acct-maintenance-delete.php"><b>&raquo;</b><?php echo $l['button']['DeleteAccountingRecords'] ?>
+			</a></li>
+	</ul>
+
+	<br/><br/>
+	<h2>Search</h2>
+	<input name="" type="text" value="Search" />
+			
+</div>
+
+
+
+
+<?php
+
+        if ((isset($actionStatus)) && ($actionStatus == "success")) {
+                echo <<<EOF
+                        <div id="contentnorightbar">
+                        <h9 id="Intro"> Success </h9>
+                        <br/><br/>
+                        <font color='#0000FF'>
+EOF;
+        echo $actionMsg;
+
+        echo "</font></div>";
+
+        }
+
+
+        if ((isset($actionStatus)) && ($actionStatus == "failure")) {
+                echo <<<EOF
+                        <div id="contentnorightbar">
+                        <h8 id="Intro"> Failure </h8>
+                        <br/><br/>
+                        <font color='#FF0000'>
+EOF;
+        echo $actionMsg;
+
+        echo "</font></div>";
+
+        }
+
+
+?>
+
