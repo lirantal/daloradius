d1ff16952516e4ec014f6c7a8b01dd6ee04b16b7
Liran Tal <liran.tal@gmail.com>
2008-04-13 20:45:39 +0000

    updated the auto-complete ajax attributes management

--- mng-rad-profiles-new.php
+++ mng-rad-profiles-new.php
@@ -133,7 +133,7 @@ AND GroupName='".$dbSocket->escapeSimple($group)."'";
 <title>daloRADIUS</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
-
+<link rel="stylesheet" href="css/auto-complete.css" media="screen" type="text/css">
 </head>
  
 <?php
@@ -173,44 +173,64 @@ AND GroupName='".$dbSocket->escapeSimple($group)."'";
 
         <br/>
 
- 	<fieldset>
+        <fieldset>
 
                 <h302> <?php echo $l['title']['Attributes']; ?> </h302>
-		<br/>
+                <br/>
 
-		<label for='vendor' class='form'>Vendor:</label>
-                <select id='dictVendors0' onchange="getAttributesList(this,'dictAttributesDatabase')" 
-			style='width: 215px' class='form' >
+                <label for='vendor' class='form'>Vendor:</label>
+                <select id='dictVendors0' onchange="getAttributesList(this,'dictAttributesDatabase')"
+                        style='width: 215px' class='form' >
                         <option value=''>Select Vendor...</option>
-			<?php
-			        include 'library/opendb.php';
-	
-			        $sql = "SELECT distinct(Vendor) as Vendor FROM dictionary WHERE Vendor>'' ORDER BY Vendor ASC";
-			        $res = $dbSocket->query($sql);
-	
-			        while($row = $res->fetchRow()) {
-			                echo "<option value=$row[0]>$row[0]</option>";
-			        }
+                        <?php
+                                include 'library/opendb.php';
 
-			        include 'library/closedb.php';
-			?>
+                                $sql = "SELECT distinct(Vendor) as Vendor FROM dictionary WHERE Vendor>'' ".
+					" ORDER BY Vendor ASC";
+                                $res = $dbSocket->query($sql);
+
+                                while($row = $res->fetchRow()) {
+                                        echo "<option value=$row[0]>$row[0]</option>";
+                                }
+
+                                include 'library/closedb.php';
+                        ?>
                 </select>
-		<input type='button' name='reloadAttributes' value='Reload Vendors' 
-			onclick="javascript:getVendorsList('dictVendors0');" class='button'>
-		<br/>
+                <input type='button' name='reloadAttributes' value='Reload Vendors'
+                        onclick="javascript:getVendorsList('dictVendors0');" class='button'>
+                <br/>
 
-		<label for='attribute' class='form'>
-			Attribute:</label>
+                <label for='attribute' class='form'>
+                        Attribute:</label>
                 <select id='dictAttributesDatabase' style='width: 270px' class='form' >
                 </select>
-		<input type='button' name='addAttributes' value='Add Attribute' 
-			onclick="javascript:parseAttribute(1);" class='button'>
-		<br/>
+                <input type='button' name='addAttributes' value='Add Attribute'
+                        onclick="javascript:parseAttribute(1);" class='button'>
+                <br/>
 
-		<label for='attribute' class='form'>
-			Custom Attribute:</label>
-		<input type='text' id='dictAttributesCustom' style='width: 264px' />
-		<br/>
+                <label for='attribute' class='form'>
+                        Custom Attribute:</label>
+                <input type='text' id='dictAttributesCustom' style='width: 264px' />
+                <br/>
+
+<?php
+
+        include_once('library/config_read.php');
+
+        if ( (isset($configValues['CONFIG_IFACE_AUTO_COMPLETE'])) &&
+                (strtolower($configValues['CONFIG_IFACE_AUTO_COMPLETE']) == "yes") ) {
+
+                echo "
+                        <script type=\"text/javascript\" src=\"library/javascript/dhtmlSuite-common.js\"></script>
+                        <script type=\"text/javascript\" src=\"library/javascript/auto-complete.js\"></script>
+
+                        <script type=\"text/javascript\">
+                                autoCom = new DHTMLSuite.autoComplete();
+                                autoCom.add('dictAttributesCustom','include/management/dynamic_attributes.php','_large');
+                        </script>
+                ";
+        }
+?>
 
 
 	<br/>
