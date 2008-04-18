7c553663b63e9458f797d0caf09387bbc3709f54
Liran Tal <liran.tal@gmail.com>
2008-04-18 20:40:19 +0000

    closes up feature request 1929012 - added multi purpose button to clear stale sessions from database | also added the tooltip popup provided by the dhtmlgoodies library to allow extra user-specific functionality

--- rep-online.php
+++ rep-online.php
@@ -22,9 +22,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
+<script src="library/javascript/pages_common.js" type="text/javascript"></script>
+<script src="library/javascript/rounded-corners.js" type="text/javascript"></script>
+<script src="library/javascript/form-field-tooltip.js" type="text/javascript"></script>
 <title>daloRADIUS</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+<link rel="stylesheet" href="css/form-field-tooltip.css" type="text/css" media="screen,projection" />
 <link rel="stylesheet" type="text/css" href="library/js_date/datechooser.css">
 <!--[if lte IE 6.5]>
 <link rel="stylesheet" type="text/css" href="library/js_date/select-free.css"/>
@@ -85,13 +89,22 @@
 	$maxPage = ceil($numrows/$rowsPerPage);
 	/* END */
 
+	echo "<form name='usersonline' method='get' >";
+
 	echo "<table border='0' class='table1'>\n";
 	echo "
 		<thead>
 			<tr>
 			<th colspan='10' align='left'>
-			<br/>
-	";
+
+                                Select:
+                                <a class=\"table\" href=\"javascript:SetChecked(1,'clearSessionsUsers[]','usersonline')\">All</a>
+
+                                <a class=\"table\" href=\"javascript:SetChecked(0,'clearSessionsUsers[]','usersonline')\">None</a>
+                        <br/>
+                                <input class='button' type='button' value='".$l['button']['ClearSessions']."' onClick='javascript:removeCheckbox(\"usersonline\",\"mng-del.php\")' />
+                                <br/><br/>
+                ";
 
 	if ($configValues['CONFIG_IFACE_TABLES_LISTING_NUM'] == "yes")
 		setupNumbering($numrows, $rowsPerPage, $pageNum, $orderBy, $orderType);
@@ -131,6 +144,7 @@
 		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=nasipaddress&orderType=$orderType\">
 		".$l['all']['NASIPAddress']."</a>
 		</th>
+
 	</tr> </thread>";
 
 	while($row = $res->fetchRow()) {
@@ -139,8 +153,15 @@
 		$totalTime = seconds2time($dateDiff);
 
 		echo "<tr>
-				<td> <a class='tablenovisit' href='mng-edit.php?username=$row[0]' 
-					title='".$l['Tooltip']['UserEdit']."'>$row[0]</a>
+				<td> <input type='checkbox' name='clearSessionsUsers[]' value='$row[0]'>
+					<a class='tablenovisit' href='javascript:return;'
+					onclick=\"javascript:__displayTooltip();\" 
+					tooltipText=\"
+						<a class='toolTip' href='mng-edit.php?username=$row[0]'>".$l['Tooltip']['UserEdit']."</a>
+						<a class='toolTip' href='config-maint-disconnect-user.php?username=$row[0]&nasaddr=$row[5]'>
+						".$l['all']['Disconnect']."</a>
+						<br/>\"
+					>$row[0]</a>
 					</td>
 				<td> <b>IP:</b> $row[1]<br/> <b>MAC:</b> $row[2]</td>
 				<td> $row[3] </td>
@@ -198,6 +219,14 @@
         include 'page-footer.php';
 ?>
 
+
+<script type="text/javascript">
+var tooltipObj = new DHTMLgoodies_formTooltip();
+tooltipObj.setTooltipPosition('right');
+tooltipObj.setPageBgColor('#EEEEEE');
+tooltipObj.setTooltipCornerSize(15);
+tooltipObj.initFormFieldTooltip();
+</script>
 		
 		</div>
 		
