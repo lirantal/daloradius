1b8156a255886f97be27d1eb93660f04ba675df9
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-05-02 06:36:02 +0000

    fixed up code so that it's modular for other csv export types

--- trunk/include/management/fileExport.php
+++ trunk/include/management/fileExport.php
@@ -11,25 +11,25 @@
 
 session_start();
 
-$reportTable = $_SESSION['reportTable'];			// get table name (radacct/radcheck/etc)
-
 if (isset($_GET['reportFormat'])) {
-
-	include_once('../../library/opendb.php');
 
 	$reportFormat = $_GET['reportFormat'];			// reportFormat is either CSV or PDF
 	$reportType = $_SESSION['reportType'];			// reportType defines the sql query string
 	$reportQuery = $_SESSION['reportQuery'];		// reportQuery adds the WHERE fields for page-specific 
 								// reports
-	$outputHeader = "Id,NAS/Hotspot,UserName,IP Address,Start Time,Stop Time,Total Session Time (seconds),".
-			"Total Upload (bytes),Total Downloads (bytes),Termination Cause,NAS IP Address".
-			"\n";
-	$outputContent = "";
+	$reportTable = $_SESSION['reportTable'];		// get table name (radacct/radcheck/etc)
 
 	switch ($reportType) {
 
 		case "accountingGeneric":
 
+				include_once('../../library/opendb.php');
+
+				$outputHeader = "Id,NAS/Hotspot,UserName,IP Address,Start Time,Stop Time,".
+						"Total Session Time (seconds),Total Upload (bytes),Total Downloads (bytes),".
+						"Termination Cause,NAS IP Address".
+						"\n";
+				$outputContent = "";
 
 				$sql = "SELECT ".$configValues['CONFIG_DB_TBL_RADACCT'].
 					".RadAcctId, ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].
@@ -42,31 +42,32 @@ if (isset($_GET['reportFormat'])) {
 			                ".AcctInputOctets, ".$configValues['CONFIG_DB_TBL_RADACCT'].
 			                ".AcctOutputOctets, ".$configValues['CONFIG_DB_TBL_RADACCT'].
 			                ".AcctTerminateCause, ".$configValues['CONFIG_DB_TBL_RADACCT'].
-			                ".NASIPAddress FROM ".$configValues['CONFIG_DB_TBL_RADACCT'].
+			                ".NASIPAddress FROM $reportTable".
 			                " LEFT JOIN ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].
 			                " ON ".$configValues['CONFIG_DB_TBL_RADACCT'].
 			                ".calledstationid = ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].
 			                ".mac $reportQuery ORDER BY RadAcctId DESC";
+
+				if ($reportFormat == "csv") {
+
+				        $res = $dbSocket->query($sql);
+
+				        while($row = $res->fetchRow()) {
+						$outputContent .= "$row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],".
+									"$row[7],$row[8],$row[9],$row[10]\n";
+					}
+
+
+					$output = $outputHeader . $outputContent;
+					exportCSVFile($output);	
+
+					include_once('../../library/closedb.php');
+				}
+
 				break;
+
 	}
 
-
-	if ($reportFormat == "csv") {
-
-	        $res = $dbSocket->query($sql);
-
-	        while($row = $res->fetchRow()) {
-			$outputContent .= "$row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],".
-					"$row[7],$row[8],$row[9],$row[10]\n";
-	        }
-
-
-		$output = $outputHeader . $outputContent;
-		exportCSVFile($output);	
-	
-	}
-
-	include_once('../../library/closedb.php');
 
 	exit;
 }
@@ -78,7 +79,7 @@ if (isset($_GET['reportFormat'])) {
 function exportCSVFile($output) {
 
 	header("Content-type: application/vnd.ms-excel");
-	header("Content-disposition: csv; filename=document_" . date("Ymd") . ".csv; size=" . strlen($output));
+	header("Content-disposition: csv; filename=daloradius__" . date("Ymd") . ".csv; size=" . strlen($output));
 	print $output;
 	
 }
