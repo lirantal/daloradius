3e1a0cf0e1029227213747be91f79daf46ecfa34
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-05-08 08:08:34 +0000

    improved security handling of php sessions

--- trunk/dologin.php
+++ trunk/dologin.php
@@ -1,6 +1,22 @@
 <?php
+
+// first we create a random session key
+$REMOTE_ADDR = $_SERVER['REMOTE_ADDR'];						// get client ip address
+srand((double)microtime()*1000000 );						// initialize random seed
+$rand = rand(1,9);								// generate a random number between 1 to 9
+$session_id = $rand.substr(md5($REMOTE_ADDR), 0, 11+$rand);			// append the random number to the beginning
+										// of the session_id string followed by a
+										// substring of the md5 ip address hash with
+										// a dynamic length of anything between 11
+										// to 16 digits (the max length of the md5
+										// hash)
+$session_id .= substr(md5(rand(1,1000000)), rand(1,32-$rand), 21-$rand);	// further add a dynamic length digits to 
+										// to the session_id string composed of the
+										// md5 hash for random number
+session_id($session_id);							// apply the session_id that we created
+
 // we must never forget to start the session
-session_start();
+session_start();								// initiate the session
 
 $errorMessage = '';
    include 'library/opendb.php';
@@ -15,6 +31,7 @@ AND password = '".$dbSocket->escapeSimple($operator_pass)."'";
    $res = $dbSocket->query($sql);
 
    if ($res->numRows() == 1) {
+
       // the user id and password match,
       // set the session
       $_SESSION['logged_in'] = true;
--- trunk/library/checklogin.php
+++ trunk/library/checklogin.php
@@ -1,13 +1,38 @@
 <?php
-session_start();
 
-// is the one accessing this page logged in or not?
-if (!isset($_SESSION['logged_in'])
-   || $_SESSION['logged_in'] != true) {
 
-   // not logged in, move to login page
-   header('Location: login.php');
-   exit;
+// internal function to verify the custom session_id which we create upon logging in
+function session_verify() {
+
+	session_start();
+
+	$REMOTE_ADDR = $_SERVER['REMOTE_ADDR'];
+	if (substr(md5($REMOTE_ADDR), 0, 10+substr(session_id(), 0, 1)) == 
+		substr(session_id(), 1, 10+substr(session_id(), 0, 1))) {
+		$session_valid="yes";
+	} else {
+		$session_valid="no";
+	}
+
+
+	return $session_valid;
+
 }
+
+
+
+if (session_verify() == "yes") {
+
+	if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != true) {
+		header('Location: login.php');
+		exit;
+	}
+
+} else {
+	// maybe the session is verified but the user is not logged in
+	header('Location: login.php');
+	exit;
+}
+
 
 ?>
