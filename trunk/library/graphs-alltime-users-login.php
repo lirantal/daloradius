35b6a1aac0ef39fe2915a0a2cd54e276b4538f4c
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-05-19 17:08:01 +0000

    added copyright notice | code cleanup

--- trunk/library/graphs-alltime-users-login.php
+++ trunk/library/graphs-alltime-users-login.php
@@ -1,15 +1,26 @@
 <?php
-/*******************************************************************
-* Extension name: graphs-overall-users-login.php
-*                                                                  
-* Description:    
-* this graph extension procduces a query of the overall logins 
-* made by a particular user on a daily, monthly and yearly basis.                                                
-*                                                                  
-* Author: Liran Tal <liran@enginx.com>                             
-*                                                                  
-*******************************************************************/
-
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ * Description:
+ *		this graph extension procduces a query of the overall logins 
+ *
+ * Authors:	Liran Tal <liran@enginx.com>
+ *
+ *********************************************************************************************************
+ */
 
 $type = $_REQUEST['type'];
 
@@ -33,8 +44,9 @@ function daily() {
 
 	$chart = new VerticalChart(680,500);
 
-	$sql = "SELECT count(AcctStartTime), DAY(AcctStartTime) AS Day from ".$configValues['CONFIG_DB_TBL_RADACCT']." group by Day;";
-$res = $dbSocket->query($sql);
+	$sql = "SELECT count(AcctStartTime), DAY(AcctStartTime) AS Day from ".
+			$configValues['CONFIG_DB_TBL_RADACCT']." group by Day;";
+	$res = $dbSocket->query($sql);
 
 	while($row = $res->fetchRow()) {
 		$chart->addPoint(new Point("$row[1]", "$row[0]"));
@@ -63,7 +75,8 @@ function monthly() {
 
 	$chart = new VerticalChart(680,500);
 
-	$sql = "SELECT count(AcctStartTime), MONTHNAME(AcctStartTime) AS Month from ".$configValues['CONFIG_DB_TBL_RADACCT']." group by Month;";
+	$sql = "SELECT count(AcctStartTime), MONTHNAME(AcctStartTime) AS Month from ".
+			$configValues['CONFIG_DB_TBL_RADACCT']." group by Month;";
 	$res = $dbSocket->query($sql);
 
 	while($row = $res->fetchRow()) {
@@ -93,7 +106,8 @@ function yearly() {
 
 	$chart = new VerticalChart(680,500);
 
-	$sql = "SELECT count(AcctStartTime), YEAR(AcctStartTime) AS Year from ".$configValues['CONFIG_DB_TBL_RADACCT']." group by Year;";
+	$sql = "SELECT count(AcctStartTime), YEAR(AcctStartTime) AS Year from ".
+			$configValues['CONFIG_DB_TBL_RADACCT']." group by Year;";
 	$res = $dbSocket->query($sql);
 
 	while($row = $res->fetchRow()) {
--- trunk/library/graphs-hotspot-compare-time.php
+++ trunk/library/graphs-hotspot-compare-time.php
@@ -1,31 +1,48 @@
 <?php
-/*******************************************************************
-* Extension name: hotspot compare time                             *
-*                                                                  *
-* Description:    this extension creates a pie chart of the        *
-* comparison of hotspots per unique users                          *
-*                                                                  *
-* Author: Liran Tal <liran@enginx.com>                             *
-*                                                                  *
-*******************************************************************/
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ * Description:
+ *		this extension creates a pie chart of the comparison of hotspots per unique users
+ *
+ * Authors:	Liran Tal <liran@enginx.com>
+ *
+ *********************************************************************************************************
+ */
 
 	include 'opendb.php';
 	include 'libchart/libchart.php';
 
-        header("Content-type: image/png");
+	header("Content-type: image/png");
 
-        $chart = new PieChart(620,320);
+	$chart = new PieChart(620,320);
 
 	// getting total downloads of days in a month
-	$sql = "select ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].".name, count(distinct(UserName)), count(radacctid), avg(AcctSessionTime), sum(AcctSessionTime) from ".$configValues['CONFIG_DB_TBL_RADACCT']." join ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS']." on (".$configValues['CONFIG_DB_TBL_RADACCT'].".calledstationid like ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].".mac) group by ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].".name;";
+	$sql = "SELECT ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].".name, count(distinct(UserName)), count(radacctid), ".
+			" avg(AcctSessionTime), sum(AcctSessionTime) FROM ".$configValues['CONFIG_DB_TBL_RADACCT'].
+			" JOIN ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].
+			" ON (".$configValues['CONFIG_DB_TBL_RADACCT'].".calledstationid LIKE ".
+			$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].".mac) GROUP BY ".$configValues['CONFIG_DB_TBL_DALOHOTSPOTS'].".name;";
 	$res = $dbSocket->query($sql);
 
 	while($row = $res->fetchRow()) {
-                $chart->addPoint(new Point("$row[0] ($row[3] seconds)", "$row[3]"));
-        }
+		$chart->addPoint(new Point("$row[0] ($row[3] seconds)", "$row[3]"));
+	}
 
-        $chart->setTitle("Distribution of Time usage per Hotspot");
-        $chart->render();
+	$chart->setTitle("Distribution of Time usage per Hotspot");
+	$chart->render();
 
 	include 'closedb.php';
 
