fc7b3376cf84254d27af74dddec6259c5021b551
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-05-30 06:28:22 +0000

    added support for user graphs to the daloradius-users portal

---
+++ trunk/daloradius-users/graphs-overall_logins.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ *
+ * Authors:     Liran Tal <liran@enginx.com>
+ *
+ *********************************************************************************************************
+ */
+
+    include ("library/checklogin.php");
+    $login = $_SESSION['login_user'];
+
+	//setting values for the order by and order type variables
+	isset($_REQUEST['orderBy']) ? $orderBy = $_REQUEST['orderBy'] : $orderBy = "username";
+	isset($_REQUEST['orderType']) ? $orderType = $_REQUEST['orderType'] : $orderType = "asc";
+
+	$username = $login;
+	$type = $_REQUEST['type'];
+
+	//feed the sidebar variables
+	$overall_logins_username = $username;
+
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+    $logQuery = "performed query for user [$username] of type [$type] on page: ";
+
+
+?>
+
+<?php
+        include_once ("library/tabber/tab-layout.php");
+?>
+
+<?php
+	
+	include ("menu-graphs.php");
+	
+?>
+		
+		
+		<div id="contentnorightbar">
+		
+		<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><? echo $l['Intro']['graphsoveralllogins.php']; ?>
+		<h144>+</h144></a></h2>
+
+		<div id="helpPage" style="display:none;visibility:visible" >
+			<?php echo $l['helpPage']['graphsoveralllogins'] ?>
+			<br/>
+		</div>
+		<br/>
+
+<div class="tabber">
+
+     <div class="tabbertab" title="Graph">
+        <br/>
+
+<?php
+	echo "<center>";
+	echo "<img src=\"library/graphs-overall-users-login.php?type=$type&user=$username\" />";
+	echo "</center>";
+?>
+	</div>
+	<div class="tabbertab" title="Statistics">
+	<br/>
+
+<?php
+	include 'library/tables-overall-users-login.php';
+?>
+
+	</div>
+</div>		
+
+
+<?php
+	include('include/config/logging.php');
+?>
+
+		</div>
+		
+		<div id="footer">
+		
+								<?php
+        include 'page-footer.php';
+?>
+
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
