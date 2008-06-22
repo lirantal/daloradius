aa2dfed4f4e9e81809c06c84a4a4a9df99d83d1d
Liran Tal <liran.tal@gmail.com>
2008-06-22 07:28:46 +0000

    added more icons (profiles and groups pages)

--- menu-mng-rad-groups.php
+++ menu-mng-rad-groups.php
@@ -19,42 +19,62 @@
 	<h3>Group Reply Management</h3>
 	<ul class="subnav">
 
-		<li><a href="mng-rad-groupreply-list.php"><b>&raquo;</b><?php echo $l['button']['ListGroupReply'] ?></a></li>
-		<li><a href="javascript:document.mngradgroupreplysearch.submit();""><b>&raquo;</b><?php echo $l['button']['SearchGroupReply'] ?><a>
+		<li><a href="mng-rad-groupreply-list.php"><b>&raquo;</b>
+			<img src='images/icons/groupsList.png' border='0'>
+			<?php echo $l['button']['ListGroupReply'] ?></a></li>
+		<li><a href="javascript:document.mngradgroupreplysearch.submit();""><b>&raquo;</b>
+			<img src='images/icons/groupsList.png' border='0'>
+			<?php echo $l['button']['SearchGroupReply'] ?><a>
 			<form name="mngradgroupreplysearch" action="mng-rad-groupreply-search.php" method="get" 
 				class="sidebar">
 			<input name="groupname" type="text" 
 				value="<?php if (isset($search_groupname)) echo $search_groupname; ?>" tabindex=2>
 			</form></li>
 
-		<li><a href="mng-rad-groupreply-new.php"><b>&raquo;</b><?php echo $l['button']['NewGroupReply'] ?></a></li>
-		<li><a href="javascript:document.mngradgrprplyedit.submit();""><b>&raquo;</b><?php echo $l['button']['EditGroupReply'] ?><a>
+		<li><a href="mng-rad-groupreply-new.php"><b>&raquo;</b>
+			<img src='images/icons/groupsAdd.png' border='0'>
+			<?php echo $l['button']['NewGroupReply'] ?></a></li>
+		<li><a href="javascript:document.mngradgrprplyedit.submit();""><b>&raquo;</b>
+			<img src='images/icons/groupsEdit.png' border='0'>
+			<?php echo $l['button']['EditGroupReply'] ?><a>
 			<form name="mngradgrprplyedit" action="mng-rad-groupreply-edit.php" method="get" class="sidebar">
 			<input name="groupname" type="text" value="[groupname]">
 			<input name="attribute" type="text" value="[attribute]">
 			</form></li>
-		<li><a href="mng-rad-groupreply-del.php"><b>&raquo;</b><?php echo $l['button']['RemoveGroupReply'] ?></a></li>
+		<li><a href="mng-rad-groupreply-del.php"><b>&raquo;</b>
+			<img src='images/icons/groupsRemove.png' border='0'>
+			<?php echo $l['button']['RemoveGroupReply'] ?></a></li>
 		
 	</ul>
 
 	<h3>Group Check Management</h3>
 	<ul class="subnav">
 
-		<li><a href="mng-rad-groupcheck-list.php"><b>&raquo;</b><?php echo $l['button']['ListGroupCheck'] ?></a></li>
-		<li><a href="javascript:document.mngradgroupchecksearch.submit();""><b>&raquo;</b><?php echo $l['button']['SearchGroupCheck'] ?><a>
+		<li><a href="mng-rad-groupcheck-list.php"><b>&raquo;</b>
+			<img src='images/icons/groupsList.png' border='0'>
+			<?php echo $l['button']['ListGroupCheck'] ?></a></li>
+		<li><a href="javascript:document.mngradgroupchecksearch.submit();""><b>&raquo;</b>
+			<img src='images/icons/groupsList.png' border='0'>
+			<?php echo $l['button']['SearchGroupCheck'] ?><a>
 			<form name="mngradgroupchecksearch" action="mng-rad-groupcheck-search.php" method="get" 
 				class="sidebar">
 			<input name="groupname" type="text" 
 				value="<?php if (isset($search_groupname)) echo $search_groupname; ?>" tabindex=2>
 			</form></li>
 
-		<li><a href="mng-rad-groupcheck-new.php"><b>&raquo;</b><?php echo $l['button']['NewGroupCheck'] ?></a></li>
-		<li><a href="javascript:document.mngradgrpchkedit.submit();""><b>&raquo;</b><?php echo $l['button']['EditGroupCheck'] ?><a>
+		<li><a href="mng-rad-groupcheck-new.php"><b>&raquo;</b>
+			<img src='images/icons/groupsAdd.png' border='0'>
+			<?php echo $l['button']['NewGroupCheck'] ?></a></li>
+		<li><a href="javascript:document.mngradgrpchkedit.submit();""><b>&raquo;</b>
+			<img src='images/icons/groupsEdit.png' border='0'>
+			<?php echo $l['button']['EditGroupCheck'] ?><a>
 			<form name="mngradgrpchkedit" action="mng-rad-groupcheck-edit.php" method="get" class="sidebar">
 			<input name="groupname" type="text" value="[groupname]">
 			<input name="attribute" type="text" value="[attribute]">
 			</form></li>
-		<li><a href="mng-rad-groupcheck-del.php"><b>&raquo;</b><?php echo $l['button']['RemoveGroupCheck'] ?></a></li>
+		<li><a href="mng-rad-groupcheck-del.php"><b>&raquo;</b>
+			<img src='images/icons/groupsRemove.png' border='0'>
+			<?php echo $l['button']['RemoveGroupCheck'] ?></a></li>
 		
 	</ul>
 
