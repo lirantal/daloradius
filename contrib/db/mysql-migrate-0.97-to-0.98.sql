930f10bc61f4131a18efea7d1ea79910f67d8df2
Liran Tal <liran.tal@gmail.com>
2008-07-03 06:02:52 +0000

    added database updates for SVN changes

---
+++ contrib/db/mysql-migrate-0.97-to-0.98.sql
@@ -0,0 +1,7 @@
+-- MySQL dump 10.10
+--
+-- Host: localhost    Database: radius
+-- ------------------------------------------------------
+-- Server version	5.0.22-Debian_0ubuntu6.06-log
+
+ALTER TABLE userinfo ADD changeuserinfo VARCHAR(128) AFTER notes;
