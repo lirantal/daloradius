4959ef75f43bba3e33633fd5f56970917024416b
Liran Tal <liran.tal@gmail.com>
2008-08-01 09:04:46 +0000

    added new configuration sub-category for backups

--- include/menu/config-subnav.php
+++ include/menu/config-subnav.php
@@ -4,6 +4,7 @@
 					<li><a href="config-main.php"><em>G</em>eneral</a></li>
 					<li><a href="config-maint.php"><em>M</em>aintenance</a></li>
 					<li><a href="config-operators.php"><em>O</em>perators</a></li>
+					<li><a href="config-backup.php"><em>B</em>ackup</a></li>
 					
 					<div id="logindiv" style="text-align: right;">
                                                 <li>Welcome, <?php echo $operator; ?></li>
---
+++ menu-config-backup.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+</head>
+<script src="library/javascript/pages_common.js" type="text/javascript"></script>
+<body>
+<?php
+	include_once ("lang/main.php");
+?>
+
+<div id="wrapper">
+<div id="innerwrapper">
+		
+<?php
+	$m_active = "Config";
+	include_once ("include/menu/menu-items.php");
+	include_once ("include/menu/config-subnav.php");
+?>      
+
+<div id="sidebar">
+
+	<h2>Configuration</h2>
+	
+	<h3>Backup</h3>
+	
+
+	<ul class="subnav">
+
+		<li><a href="config-backup-managebackups.php"><b>&raquo;</b>
+			<img src='images/icons/configMaintenance.png' border='0'>
+			<?php echo $l['button']['ManageBackups'] ?></a>
+		</li>
+		<li><a href="config-backup-createbackups.php"><b>&raquo;</b>
+			<img src='images/icons/configMaintenance.png' border='0'>
+			<?php echo $l['button']['CreateBackups'] ?></a>
+		</li>
+		
+	</ul>
+	
+</div>
