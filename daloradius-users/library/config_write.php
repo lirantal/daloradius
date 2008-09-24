40f2cf18370ac0e4b5318f9354e2c1061521a603
Liran Tal <liran.tal@gmail.com>
2008-09-24 20:16:30 +0000

    backported support for protected php configuration file to daloradius-users web files

--- daloradius-users/library/config_write.php
+++ daloradius-users/library/config_write.php
@@ -22,21 +22,45 @@
  *********************************************************************************************************
  */
 
-$configFile = dirname(__FILE__).'/daloradius.conf';
-$commentChar = "#";
+$configFile = dirname(__FILE__).'/daloradius.conf.php';
+$date = date("D M j G:i:s T Y");
 
 $fp = fopen($configFile, "w");
 if ($fp) {
+	fwrite($fp, 
+		"<?php\n".
+		"/*\n".
+		" *********************************************************************************************************\n".
+		" * daloRADIUS - RADIUS Web Platform\n".
+		" * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.\n".
+		" *\n".
+		" * This program is free software; you can redistribute it and/or\n".
+		" * modify it under the terms of the GNU General Public License\n".
+		" * as published by the Free Software Foundation; either version 2\n".
+		" * of the License, or (at your option) any later version.\n".
+		" *\n".
+		" * You should have received a copy of the GNU General Public License\n".
+		" * along with this program; if not, write to the Free Software\n".
+		" * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.\n".
+		" *\n".
+		" *********************************************************************************************************\n".
+		" * Description:\n".
+		" *              daloRADIUS Configuration File\n".
+		" *\n".
+		" * Modification Date:\n".
+		" *              $date\n".
+		" *********************************************************************************************************\n".
+		" */\n".
+		"\n\n");
 	foreach ($configValues as $_configOption => $_configElem) {
-        fwrite($fp, $_configOption . " = " . $configValues[$_configOption] . "\n");
+	        fwrite($fp, "\$configValues['" . $_configOption . "'] = '" . $configValues[$_configOption] . "';\n");
 	}
+	fwrite($fp, "\n\n?>");
 	fclose($fp);
-	$actionStatus = "success";
-	$actionMsg = "Updated database settings for configuration file";
+	$successMsg = "Updated database settings for configuration file";
 } else {
-	$actionStatus = "failure";
-	$actionMsg = "could not open the file for writing:<b> $configFile </b>
-	<br/> Check file permissions. The file should be writable by the webserver's user/group";
+	$failureMsg = "Could not open the file for writing: <b>$configFile</b>
+	<br/>Check file permissions. The file should be writable by the webserver's user/group";
 }
 
 ?>
