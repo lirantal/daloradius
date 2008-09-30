3021aaedf1d37fa9c61ed7293e5e804f5d461ea2
Liran Tal <liran.tal@gmail.com>
2008-09-30 13:10:52 +0000

    added more fields for userinfo contatc information (address, city, state and zip code) - updating all relevant files with the proper change

--- contrib/db/mysql-migrate-0.97-to-0.98.sql
+++ contrib/db/mysql-migrate-0.97-to-0.98.sql
@@ -17,3 +17,8 @@ UPDATE operators SET config_backup_createbackups='yes' WHERE username='administr
 
 ALTER TABLE operators ADD config_backup_managebackups VARCHAR(32) AFTER config_backup_createbackups;
 UPDATE operators SET config_backup_managebackups='yes' WHERE username='administrator';
+
+ALTER TABLE userinfo ADD address VARCHAR(200) AFTER mobilephone;
+ALTER TABLE userinfo ADD city VARCHAR(200) AFTER address;
+ALTER TABLE userinfo ADD state VARCHAR(200) AFTER city;
+ALTER TABLE userinfo ADD zip VARCHAR(200) AFTER state;
