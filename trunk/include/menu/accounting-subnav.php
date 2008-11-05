30c8b006e261cf92ac64c26529467cc735ac8be0
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-11-05 20:29:26 +0000

    added printing login location

--- trunk/include/menu/accounting-subnav.php
+++ trunk/include/menu/accounting-subnav.php
@@ -6,6 +6,8 @@
                                                 <li><a href="acct-maintenance.php"><em>M</em>aintenance</a></li>
 
 <div id="logindiv" style="text-align: right;">
+
+                                                <li>Location: <b><?php echo $_SESSION['location_name'] ?></b></li><br/>
                                                 <li>Welcome, <?php echo $operator; ?></li>
 
                                                 <li><a href="logout.php">[logout]</a></li>
--- trunk/include/menu/gis-subnav.php
+++ trunk/include/menu/gis-subnav.php
@@ -2,6 +2,7 @@
                                 <ul id="subnav">
 
 								<div id="logindiv" style="text-align: right;">
+                                                <li>Location: <b><?php echo $_SESSION['location_name'] ?></b></li><br/>
                                                 <li>Welcome, <?php echo $operator; ?></li>
                                                 <li><a href="logout.php">[logout]</a></li>
 
