d646e7af158269ff623c1183cf346ce816d29894
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-11-30 21:40:32 +0000

    re-ordered the sub-category options

--- trunk/include/menu/billing-subnav.php
+++ trunk/include/menu/billing-subnav.php
@@ -2,9 +2,9 @@
                                 <ul id="subnav">
 
                                                 <li><a href="bill-pos.php"><em>P</em>OS</a></li>
+                                                <li><a href="bill-plans.php"><em>P</em>lans</a></li>
                                                 <li><a href="bill-rates.php"><em>R</em>ates</a></li>
                                                 <li><a href="bill-paypal.php"><em>P</em>aypal-Transactions</a></li>
-                                                <li><a href="bill-plans.php"><em>P</em>lans</a></li>
 
 						<div id="logindiv" style="text-align: right;">
 
