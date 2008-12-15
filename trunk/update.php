24687b6a9e475ac900ef8c03a92b3d32f213fa73
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2008-12-15 18:18:10 +0000

    updated support for upgrading from 0.9-6 to 0.9-7

--- trunk/update.php
+++ trunk/update.php
@@ -60,6 +60,332 @@ if (isset($_POST['submit'])) {
 	include('library/opendb.php');
 	$dbSocket->setErrorHandling(PEAR_ERROR_CALLBACK, 'updateErrorHandler');			// set our own callback for error handling
 
+	/* perform conversion procedure to upgrade to version 0.9-6 of the database schema */
+	if ($databaseVersion == "0.9-6") {
+
+		/* Begining set of SQL entries */
+		$sql = "
+					CREATE TABLE realms (
+					        id bigint(20) NOT NULL auto_increment,
+					        realmname VARCHAR(128) default NULL,
+					        type VARCHAR(32) default NULL,
+					        authhost VARCHAR(256) default NULL,
+					        accthost VARCHAR(256) default NULL,
+					        secret VARCHAR(128) default NULL,
+					        ldflag VARCHAR(64) default NULL,
+					        nostrip INT(8) default NULL,
+					        hints INT(8) default NULL,
+					        notrealm INT(8) default NULL,
+						creationdate DATETIME default NULL,
+						creationby VARCHAR(128) default NULL,
+						updatedate DATETIME default NULL,
+						updateby VARCHAR(128) default NULL,
+					        PRIMARY KEY (id)
+					);
+				";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		}
+
+		$sql = "
+					CREATE TABLE proxys (
+					        id bigint(20) NOT NULL auto_increment,
+					        proxyname VARCHAR(128) default NULL,
+					        retry_delay INT(8) default NULL,
+					        retry_count INT(8) default NULL,
+					        dead_time INT(8) default NULL,
+					        default_fallback INT(8) default NULL,
+						creationdate DATETIME default NULL,
+						creationby VARCHAR(128) default NULL,
+						updatedate DATETIME default NULL,
+						updateby VARCHAR(128) default NULL,
+					        PRIMARY KEY (id)
+					)
+				";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_attributes_list VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_attributes_list='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_attributes_new VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_attributes_new='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+		$sql = "ALTER TABLE operators ADD mng_rad_attributes_edit VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_attributes_edit='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_attributes_search VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_attributes_search='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_attributes_del VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_attributes_del='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_realms_list VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_realms_list='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_realms_new VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_realms_new='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_realms_edit VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_realms_edit='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_realms_del VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_realms_del='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_proxys_list VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_proxys_list='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_proxys_new VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_proxys_new='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_proxys_edit VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_proxys_edit='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_proxys_del VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_proxys_del='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD acct_maintenance_cleanup VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET acct_maintenance_cleanup='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD acct_maintenance_delete VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET acct_maintenance_delete='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD rep_history VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET rep_history='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "
+					ALTER TABLE hotspots CHANGE website companywebsite VARCHAR(128);
+					ALTER TABLE hotspots ADD companyemail VARCHAR(128) AFTER companywebsite;
+					ALTER TABLE hotspots ADD companycontact VARCHAR(32) AFTER companyemail;
+					ALTER TABLE hotspots ADD companyphone VARCHAR(32) AFTER companycontact;
+				";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		}
+
+		$sql = "
+					ALTER TABLE hotspots ADD creationdate DATETIME AFTER companyphone;
+					ALTER TABLE hotspots ADD creationby VARCHAR(128) AFTER creationdate;
+					ALTER TABLE hotspots ADD updatedate DATETIME AFTER creationby;
+					ALTER TABLE hotspots ADD updateby VARCHAR(128) AFTER updatedate;
+				";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		}
+
+		$sql = "
+					ALTER TABLE userinfo ADD creationby VARCHAR(128) AFTER creationdate;
+					ALTER TABLE userinfo ADD updatedate DATETIME AFTER creationby;
+					ALTER TABLE userinfo ADD updateby VARCHAR(128) AFTER updatedate;
+				";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		}
+
+		$sql = "
+					ALTER TABLE operators ADD creationby VARCHAR(128) AFTER creationdate;
+					ALTER TABLE operators ADD updatedate DATETIME AFTER creationby;
+					ALTER TABLE operators ADD updateby VARCHAR(128) AFTER updatedate;
+			";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_ippool_list VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_ippool_list='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_ippool_new VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_ippool_new='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_ippool_edit VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_ippool_edit='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		$sql = "ALTER TABLE operators ADD mng_rad_ippool_del VARCHAR(32);";
+		$res = $dbSocket->query($sql);
+	
+		if (DB::isError($res)) {
+			$errorMsg = $res->getMessage() ." ". $res->getDebugInfo();
+			array_push($upgradeErrors, $errorMsg);
+		} else {
+			$sql = "UPDATE operators SET mng_rad_ippool_del='yes' WHERE username='administrator';";
+			$res = $dbSocket->query($sql);
+		}
+
+		/* Ending set of SQL entries */
+		$databaseVersion = "0.9-7";
+	} // 0.9-6
+	
+	/* perform conversion procedure to upgrade to version 0.9-7 of the database schema */
 	if ($databaseVersion == "0.9-7") {
 
 		/* Begining set of SQL entries */
@@ -479,9 +805,7 @@ if (isset($_POST['submit'])) {
 		}
 
 		/* Ending set of SQL entries */
-
 		$databaseVersion = "0.9-8";
-
 	} // 0.9-7
 
 	include 'library/closedb.php';
