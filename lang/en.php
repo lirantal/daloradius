30a2e8ccb5473800c4bc92d4a891133bf9633143
Liran Tal <liran.tal@gmail.com>
2008-12-16 20:35:07 +0000

    updated language file with title information

--- lang/en.php
+++ lang/en.php
@@ -24,7 +24,7 @@
  
 $l['all']['daloRADIUS'] = "daloRADIUS 0.9-8-SVN";
 $l['all']['daloRADIUSVersion'] = "version 0.9-8-SVN";
-$l['all']['copyright1'] = "Radius Management, Reporting and Accounting by <a href=\"http://www.enginx.com\">Enginx</a>";
+$l['all']['copyright1'] = "RADIUS Management, Reporting, Accounting and Billing by <a href=\"http://www.enginx.com\">Enginx</a>";
 $l['all']['copyright2'] = "Copyright of <a href=\"http://www.enginx.com\">Enginx</a> and Liran Tal<br/>
 Template design by <a href=\"http://www.sixshootermedia.com\">Six Shooter Media</a>.";
 $l['all']['ID'] = "ID";
