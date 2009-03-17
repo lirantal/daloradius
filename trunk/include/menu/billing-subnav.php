afad67fa97bc18898e617f57e05e21465b4de9f8
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2009-03-17 10:31:39 +0000

    applied merchant transactions page instead of the older paypal transactions page

--- trunk/include/menu/billing-subnav.php
+++ trunk/include/menu/billing-subnav.php
@@ -4,7 +4,7 @@
                                                 <li><a href="bill-pos.php"><em>P</em>OS</a></li>
                                                 <li><a href="bill-plans.php"><em>P</em>lans</a></li>
                                                 <li><a href="bill-rates.php"><em>R</em>ates</a></li>
-                                                <li><a href="bill-paypal.php"><em>P</em>aypal-Transactions</a></li>
+                                                <li><a href="bill-merchant.php"><em>M</em>erchant-Transactions</a></li>
                                                 <li><a href="bill-history.php"><em>B</em>illing-History</a></li>
 
 						<div id="logindiv" style="text-align: right;">
