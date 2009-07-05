2877ddc039042d3f4cff7440632d3aa56fb5ef86
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2009-07-05 15:52:09 +0000

    added feature request to list user's plan usage

--- trunk/include/menu/accounting-subnav.php
+++ trunk/include/menu/accounting-subnav.php
@@ -1,6 +1,7 @@
 
                                 <ul id="subnav">
                                                 <li><a href="acct-main.php"><em>G</em>eneral</a></li>
+                                                <li><a href="acct-plans.php"><em>P</em>lans</a></li>
                                                 <li><a href="acct-custom.php"><em>C</em>ustom</a></li>
                                                 <li><a href="acct-hotspot.php"><em>H</em>otspot</a></li>
                                                 <li><a href="acct-maintenance.php"><em>M</em>aintenance</a></li>
