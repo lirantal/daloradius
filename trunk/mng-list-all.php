25df4803cb98e696c8a43b0d8678b1f91e91c3c5
screwloose <screwloose@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2009-08-24 01:38:35 +0000

    added flags that show if a user is enabled or not. Added Disable button to the page.

--- trunk/mng-list-all.php
+++ trunk/mng-list-all.php
@@ -67,13 +67,12 @@
 				<h144>+</h144></a></h2>
 				
                 <div id="helpPage" style="display:none;visibility:visible" >
-			<?php echo $l['helpPage']['mnglistall'] ?>
-			<br/>
-		</div>
-
-		<div id="returnMessages">
-		</div>
-
+					<?php echo $l['helpPage']['mnglistall'] ?>
+					<br/>
+				</div>
+					<div id="returnMessages">
+					</div>
+				<br/>
 
 <?php
 
@@ -89,21 +88,11 @@
 
 	
 	//orig: used as maethod to get total rows - this is required for the pages_numbering.php page
-	$sql = "SELECT * FROM (".
-		"SELECT distinct(".$configValues['CONFIG_DB_TBL_RADCHECK'].".username),".$configValues['CONFIG_DB_TBL_RADCHECK'].".value,
-		".$configValues['CONFIG_DB_TBL_RADCHECK'].".id,".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".groupname as groupname, ". 
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".attribute, ".$configValues['CONFIG_DB_TBL_DALOUSERINFO'].".firstname, ".
-		$configValues['CONFIG_DB_TBL_DALOUSERINFO'].".lastname,  MAX(".$configValues['CONFIG_DB_TBL_RADACCT'].".AcctStartTime) AS LastLoggedIn
-		FROM ".$configValues['CONFIG_DB_TBL_RADCHECK']." ".
-		" LEFT JOIN ".$configValues['CONFIG_DB_TBL_DALOUSERINFO']." ON ".
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_DALOUSERINFO'].".username ".
-		" LEFT JOIN ".$configValues['CONFIG_DB_TBL_RADUSERGROUP']." ON ".
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".username ".
-		" LEFT JOIN ".$configValues['CONFIG_DB_TBL_RADACCT']." ON ".
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_RADACCT'].".username ".
-		" WHERE (Attribute LIKE '%-Password') OR (Attribute='Auth-Type') 
-		GROUP BY UserName ORDER BY ".$configValues['CONFIG_DB_TBL_RADCHECK'].".attribute ) AS sorted ".
-		" GROUP BY UserName ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage";
+	$sql = "SELECT distinct(".$configValues['CONFIG_DB_TBL_RADCHECK'].".username),".$configValues['CONFIG_DB_TBL_RADCHECK'].".value,
+		".$configValues['CONFIG_DB_TBL_RADCHECK'].".id,".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".groupname as groupname, attribute FROM 
+		".$configValues['CONFIG_DB_TBL_RADCHECK']." LEFT JOIN ".$configValues['CONFIG_DB_TBL_RADUSERGROUP']." ON 
+		".$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".username 
+		WHERE (Attribute='Auth-Type') or (Attribute LIKE '%-Password') GROUP BY UserName";
 	$res = $dbSocket->query($sql);
 	$numrows = $res->numRows();
 
@@ -111,21 +100,14 @@
 	/* we are searching for both kind of attributes for the password, being User-Password, the more
 	   common one and the other which is Password, this is also done for considerations of backwards
 	   compatibility with version 0.7        */
-	$sql = "SELECT * FROM (".
-		"SELECT distinct(".$configValues['CONFIG_DB_TBL_RADCHECK'].".username),".$configValues['CONFIG_DB_TBL_RADCHECK'].".value,
-		".$configValues['CONFIG_DB_TBL_RADCHECK'].".id,".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".groupname as groupname, ". 
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".attribute, ".$configValues['CONFIG_DB_TBL_DALOUSERINFO'].".firstname, ".
-		$configValues['CONFIG_DB_TBL_DALOUSERINFO'].".lastname,  MAX(".$configValues['CONFIG_DB_TBL_RADACCT'].".AcctStartTime) AS LastLoggedIn
-		FROM ".$configValues['CONFIG_DB_TBL_RADCHECK']." ".
-		" LEFT JOIN ".$configValues['CONFIG_DB_TBL_DALOUSERINFO']." ON ".
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_DALOUSERINFO'].".username ".
-		" LEFT JOIN ".$configValues['CONFIG_DB_TBL_RADUSERGROUP']." ON ".
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".username ".
-		" LEFT JOIN ".$configValues['CONFIG_DB_TBL_RADACCT']." ON ".
-		$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_RADACCT'].".username ".
-		" WHERE (Attribute LIKE '%-Password') OR (Attribute='Auth-Type') 
-		GROUP BY UserName ORDER BY ".$configValues['CONFIG_DB_TBL_RADCHECK'].".attribute ) AS sorted ".
-		" GROUP BY UserName ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage";
+
+	$sql = "SELECT distinct(".$configValues['CONFIG_DB_TBL_RADCHECK'].".username),".$configValues['CONFIG_DB_TBL_RADCHECK'].".value,
+		".$configValues['CONFIG_DB_TBL_RADCHECK'].".id,".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".groupname as groupname, attribute FROM 
+		".$configValues['CONFIG_DB_TBL_DALOUSERINFO']." ,".$configValues['CONFIG_DB_TBL_RADCHECK']." LEFT JOIN ".$configValues['CONFIG_DB_TBL_RADUSERGROUP']." ON 
+		".$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_RADUSERGROUP'].".username
+		 LEFT JOIN ".$configValues['CONFIG_DB_TBL_DALOUSERBILLINFO']."
+		 ON ".$configValues['CONFIG_DB_TBL_RADCHECK'].".username=".$configValues['CONFIG_DB_TBL_DALOUSERBILLINFO'].".username
+ 		WHERE (radcheck.username=userinfo.username AND (Attribute LIKE '%-Password') OR (Attribute='Auth-Type')) GROUP by Username ORDER BY $orderBy $orderType LIMIT $offset, $rowsPerPage";
 	$res = $dbSocket->query($sql);
 	$logDebugSQL = "";
 	$logDebugSQL .= $sql . "\n";
@@ -146,13 +128,10 @@
 				
 				<a class=\"table\" href=\"javascript:SetChecked(0,'username[]','listallusers')\">None</a>
 			<br/>
-				<input class='button' type='button' value='Delete' 
-					onClick='javascript:removeCheckbox(\"listallusers\",\"mng-del.php\")' />
-        	                <input class='button' type='button' value='Disable'
-					onClick='javascript:disableCheckbox(\"listallusers\",\"include/management/userOperations.php\")' />
-        	                <input class='button' type='button' value='CSV Export'
-	                        	onClick=\"javascript:window.location.href='include/management/fileExport.php?reportFormat=csv'\"
-                		        />
+				<input class='button' type='button' value='Delete' onClick='javascript:removeCheckbox(\"listallusers\",\"mng-del.php\")' />
+				<input class='button' type='button' value='Disable' onClick='javascript:disableCheckbox(\"listallusers\",\"include/management/userOperations.php\")' />
+	  	              <input class='button' type='button' value='CSV Export'onClick=\"javascript:window.location.href='include/management/fileExport.php?reportFormat=csv'\"/>
+
 				<br/><br/>
 		";
 
@@ -166,61 +145,56 @@
 			</tr>
 			</thead>
 			";
-
+	  $curOrderType = $orderType;
         if ($orderType == "asc") {
-                $orderTypeNextPage = "desc";
+                $orderType = "desc";
         } else  if ($orderType == "desc") {
-                $orderTypeNextPage = "asc";
+                $orderType = "asc";
         }
 	
 	echo "<thread> <tr>
 		<th scope='col'> 
-		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=id&orderType=$orderTypeNextPage\">
+		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=id&orderType=$orderType\">
 		".$l['all']['ID']."</a>
 		</th>
 
 		<th scope='col'> 
-		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=Username&orderType=$orderTypeNextPage\">
+		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=Username&orderType=$orderType\">
 		".$l['all']['Username']."</a>
 		</th>
 
 		<th scope='col'> 
-		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=firstname&orderType=$orderTypeNextPage\">
-		".$l['all']['Name']."</a>
-		</th>
-
-		<th scope='col'> 
-		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=Value&orderType=$orderTypeNextPage\">
+		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=Value&orderType=$orderType\">
 		".$l['all']['Password']."</a>
 		</th>
 
 		<th scope='col'> 
-		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=Groupname&orderType=$orderTypeNextPage\">
+		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=Groupname&orderType=$orderType\">
 		".$l['title']['Groups']."</a>
 		</th>
-
-		<th scope='col'> 
-		<a title='Sort' class='novisit' href=\"" . $_SERVER['PHP_SELF'] . "?orderBy=LastLoggedIn&orderType=$orderTypeNextPage\">
-		".$l['all']['LastLoginTime']."</a>
-		</th>
-
 		</tr> </thread>";
 
 	while($row = $res->fetchRow()) {
-		printqn("
-			<tr>
-			<td> <input type='checkbox' name='username[]' value='$row[0]'>$row[2]</td>
-			");
 
-		echo "<td>";
+
+
+
+
+		printqn("
+			<td> <input type='checkbox' name='username[]' value='$row[0]'>$row[2]</td>
+			<td> 
+");
+
+
 
 		if ( ($row[1] == "Reject") && ($row[4] == "Auth-Type") )
 			echo "<img title='user is disabled' src='images/icons/userStatusDisabled.gif' alt='[disabled]'>";
 		else
 			echo "<img title='user is enabled' src='images/icons/userStatusActive.gif' alt='[enabled]'>";
 
-		printqn("
-			<a class='tablenovisit' href='javascript:return;'
+
+printqn("
+<a class='tablenovisit' href='javascript:return;'
                                 onClick='javascript:ajaxGeneric(\"include/management/retUserInfo.php\",\"retBandwidthInfo\",\"divContainerUserInfo\",\"username=$row[0]\");
 					javascript:__displayTooltip();'
                                 tooltipText='
@@ -236,22 +210,13 @@
 			</td>
 			");
 
-		echo "<td>$row[5] $row[6]</td>";
-
 		if ($configValues['CONFIG_IFACE_PASSWORD_HIDDEN'] == "yes") {
 			echo "<td>[Password is hidden]</td>";
 		} else {
 			echo "<td>$row[1]</td>";
 		}
-		
-		if (!$row[7])
-			$lastloggedin = $l['all']['Never'];
-		else
-			$lastloggedin = $row[7];
-		
 		echo "
 			<td>$row[3]</td>
-			<td>$lastloggedin</td>
 		</tr>";
 	}
 	
@@ -260,7 +225,7 @@
 							<tr>
 							<th colspan='10' align='left'> 
 	";
-	setupLinks($pageNum, $maxPage, $orderBy, $orderType);
+	setupLinks($pageNum, $maxPage, $orderBy, $curOrderType);
 	echo "							</th>
 							</tr>
 					</tfoot>
