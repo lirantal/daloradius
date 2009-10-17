eb9b86b8274586761e741006b428ec8b26fe90c0
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2009-10-17 00:27:42 +0000

    removed groups navigation links (groups are deprecated now)

--- trunk/include/menu/management-subnav.php
+++ trunk/include/menu/management-subnav.php
@@ -5,7 +5,6 @@
 						<li><a href="mng-rad-nas.php"><em>N</em>as</a></li>
                         <li><a href="mng-rad-usergroup.php"><em>U</em>ser-Groups</a></li>
                          <li><a href="mng-rad-profiles.php"><em>P</em>rofiles</a></li>
-						<li><a href="mng-rad-groups.php"><em>G</em>roups</a></li>
 
 						<li><a href="mng-rad-attributes.php"><em>A</em>ttributes</a></li>
 						<li><a href="mng-rad-realms.php"><em>R</em>ealms/Proxys	</a></li>
