afd6af83d7fee1efafe458898027c751aa896932
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2010-03-31 15:37:35 +0000

    added notifications system (still in beta) with templating support | also added the dompdf library to generate the pdf reports/notifications

---
+++ trunk/config-mail.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ *
+ * Authors:	Liran Tal <liran@enginx.com>
+ *
+ *********************************************************************************************************
+ */
+
+    include ("library/checklogin.php");
+    $operator = $_SESSION['operator_user'];
+
+	include('library/check_operator_perm.php');
+
+	include_once('library/config_read.php');
+    $log = "visited page: ";
+    include('include/config/logging.php');
+
+
+    include ("library/config_read.php");
+
+    if (isset($_REQUEST['submit'])) {
+
+		if (isset($_REQUEST['config_mail_smtpaddr']))
+			$configValues['CONFIG_MAIL_SMTPADDR'] = $_REQUEST['config_mail_smtpaddr'];
+		
+		// this should probably move to some other page at some point
+		if (isset($_REQUEST['config_mail_smtpport']))
+			$configValues['CONFIG_MAIL_SMTPPORT'] = $_REQUEST['config_mail_smtpport'];
+		
+        include ("library/config_write.php");
+    }	
+
+	
+?>		
+		
+<?php
+    include ("menu-config.php");
+?>
+
+<?php
+        include_once ("library/tabber/tab-layout.php");
+?>
+		
+			
+		<div id="contentnorightbar">
+		
+				<h2 id="Intro"><a href="#" onclick="javascript:toggleShowDiv('helpPage')"><?php echo $l['Intro']['configmail.php']; ?>
+				<h144>+</h144></a></h2>
+
+                <div id="helpPage" style="display:none;visibility:visible" >
+					<?php echo $l['helpPage']['configmail'] ?>
+					<br/>
+				</div>
+                <?php
+					include_once('include/management/actionMessages.php');
+                ?>
+
+				<form name="mailsettings" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+
+<div class="tabber">
+
+     <div class="tabbertab" title="<?php echo $l['title']['Settings']; ?>">
+
+        <fieldset>
+
+                <h302><?php echo $l['title']['Settings']; ?></h302>
+		<br/>
+
+		<ul>
+
+		<li class='fieldset'>
+		<label for='config_mail_smtpaddr' class='form'><?php echo $l['all']['SMTPServerAddress'] ?></label>
+		<input type='text' value="<?php echo $configValues['CONFIG_MAIL_SMTPADDR'] ?>" name="config_mail_smtpaddr" />
+		</li>
+
+		<li class='fieldset'>
+		<label for='config_mail_smtpport' class='form'><?php echo $l['all']['SMTPServerPort'] ?></label>
+		<input type='text' value="<?php echo $configValues['CONFIG_MAIL_SMTPPORT'] ?>" name="config_mail_smtpport" />
+		</li>
+
+
+
+		<li class='fieldset'>
+		<br/>
+		<hr><br/>
+		<input type='submit' name='submit' value='<?php echo $l['buttons']['apply'] ?>' class='button' />
+		</li>
+
+		</ul>
+	
+	</fieldset>
+
+	</div>
+
+</div>
+
+
+				</form>
+
+	
+				<br/><br/>
+
+
+
+
+
+
+		</div>
+		
+		<div id="footer">
+		
+<?php
+        include 'page-footer.php';
+?>
+		
+		</div>
+		
+</div>
+</div>
+
+
+</body>
+</html>
---
+++ trunk/notifications/processNotificationWelcome.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ *
+ * Authors:	Liran Tal <liran@enginx.com>
+ *
+ *********************************************************************************************************
+ */
+	//include the dompdf class
+	require_once("dompdf/dompdf_config.inc.php");
+
+	//include the Pear Mail classes for sending out emails
+	@require_once('Mail.php');
+	@require_once('Mail/mime.php');
+	
+	$base = dirname(__FILE__);
+	
+	/**
+	 * sendWelcomeNotification()
+	 * wrapper-function to send notification to the customer
+	 * 
+	 * @param		array			customer information array
+	 * @param		array			smtp server information
+	 * @param		string			from email address of the sender identity
+	 */
+	function sendWelcomeNotification($customerInfo, $smtpInfo, $from) {
+
+		global $base;
+		
+		if (empty($customerInfo['customer_email']))
+			return;
+		
+		$headers = array(	"From"	=>	$from, 
+							"Subject"	=>	"Welcome new customer!",
+							"Reply-To"=> $from
+					);
+		
+		$html = prepareNotificationTemplate($customerInfo);
+		$pdfDocument = createPDF($html);
+
+		file_put_contents("$base/out.pdf", $pdfDocument);
+		
+		$mime = new Mail_mime(); 
+		$mime->setTXTBody("Notification letter of service"); 
+		$mime->addAttachment($pdfDocument, "application/pdf", "notification.pdf", false, 'base64');
+		$body = $mime->get();
+		$headers = $mime->headers($headers);
+		$mail =& Mail::factory("smtp", $smtpInfo);
+		$mail->send($customerInfo['customer_email'], $headers, $body);		
+		
+	}
+
+	
+	/**
+	 * prepareNotificationTemplate()
+	 * reads the notification (html) template and returns the formatted (html) notification
+	 * 
+	 * @param		array			Array of notification information
+	 * @return		string			HTML notification
+	 */
+	function prepareNotificationTemplate($customerInfo) {
+	
+		global $base;
+		
+		// the HTML template
+		$notification_template = "$base/templates/welcome.html";
+		$notification_html_template = file_get_contents($notification_template);
+	
+		$date = date("Y-m-d");
+	
+		$customer_name = $customerInfo['customer_name'];
+		$customer_address = $customerInfo['customer_address']; 
+		$customer_phone = $customerInfo['customer_phone'];
+		$customer_email = $customerInfo['customer_email'];
+	
+		$notification_html_template = str_replace("####__INVOICE_CREATION_DATE__####", $date, $notification_html_template);
+		$notification_html_template = str_replace("####__CUSTOMER_NAME__####", $customer_name, $notification_html_template);
+		$notification_html_template = str_replace("####__CUSTOMER_ADDRESS__####", $customer_address, $notification_html_template);
+		$notification_html_template = str_replace("####__CUSTOMER_PHONE__####", $customer_phone, $notification_html_template);
+		$notification_html_template = str_replace("####__CUSTOMER_EMAIL__####", $customer_email, $notification_html_template);
+
+		return $notification_html_template;
+	}
+	
+	/**
+	 * createPDF()
+	 * creates a PDF document for a given html file
+	 * 
+	 * @param 		string			the html file to convert to pdf			
+	 * @return 		string			returns the pdf in binary/string stream
+	 */
+	function createPDF($html) {
+	
+		global $base;
+		
+		// instansiate the pdf document
+		$dompdf = new DOMPDF();
+		$dompdf->set_base_path("$base/templates/");
+		$dompdf->load_html($html);
+		$dompdf->render();
+	
+		$notification_pdf = $dompdf->output();
+		
+		return $notification_pdf;
+
+	}
+
+?>
