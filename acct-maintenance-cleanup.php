6efd267c1896443722fe5ea23a8f7c64b11457ed
Liran Tal <liran.tal@gmail.com>
2010-04-01 10:52:59 +0000

    applied fix for cleaning stale sessions where AcctStopTime=NULL which might be set for FR2 type of schemas

--- acct-maintenance-cleanup.php
+++ acct-maintenance-cleanup.php
@@ -37,7 +37,8 @@
 
 			// delete all stale sessions in the database that occur until $enddate
 			$sql = "DELETE FROM ".$configValues['CONFIG_DB_TBL_RADACCT'].
-					" WHERE AcctStopTime='0000-00-00 00:00:00' AND AcctStartTime<'".$dbSocket->escapeSimple($enddate)."'";
+					" WHERE AcctStartTime<'".$dbSocket->escapeSimple($enddate)."'".
+					" AND (AcctStopTime='0000-00-00 00:00:00' OR AcctStopTime=NULL)";
 			$res = $dbSocket->query($sql);
 			$logDebugSQL .= $sql . "\n";
 
