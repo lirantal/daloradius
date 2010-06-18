87746c4c71bf965b06251048e0c491b355d99b4a
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2010-06-18 23:02:31 +0000

    commiting heartbeat dashboard pages

---
+++ trunk/menu-reports-hb.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>daloRADIUS</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
+<link rel="stylesheet" href="css/form-field-tooltip.css" type="text/css" media="screen,projection" />
+</head>
+
+<script src="library/javascript/pages_common.js" type="text/javascript"></script>
+<script src="library/javascript/rounded-corners.js" type="text/javascript"></script>
+<script src="library/javascript/form-field-tooltip.js" type="text/javascript"></script>
+ 
+<body>
+<?php
+include_once ("lang/main.php");
+?>
+
+<div id="wrapper">
+<div id="innerwrapper">
+
+<?php
+	$m_active = "Reports";
+    include_once ("include/menu/menu-items.php");
+	include_once ("include/menu/reports-subnav.php");
+?>      
+
+<div id="sidebar">
+
+	<h2>Heartbeat</h2>
+		<ul class="subnav">
+
+		<h3>Dashboard</h3>
+
+			<li><a href="rep-hb-dashboard.php"><b>&raquo;</b><?php echo $l['button']['Dashboard'] ?></a></li>
+
+		</ul>
+
+	
+	<br/><br/>
+	<h2>Search</h2>
+	<input name="" type="text" value="Search" />
+	
+
+</div>
+
