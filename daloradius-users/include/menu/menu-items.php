443c7b988628f0e794fa6b4c4cdb0f4579a96a50
Liran Tal <liran.tal@gmail.com>
2011-03-15 20:58:07 +0000

    added user pages for invoice and items preview

--- daloradius-users/include/menu/menu-items.php
+++ daloradius-users/include/menu/menu-items.php
@@ -18,6 +18,7 @@
 				<li><a href="index.php" <?php echo ($m_active == "Home") ? "class=\"active\"" : ""?>><?php echo $l['menu']['Home']; ?></a></li>
 				<li><a href="pref-main.php" <?php echo ($m_active == "Preferences") ? "class=\"active\"" : ""?>><?php echo $l['menu']['Preferences']; ?></li>
 				<li><a href="acct-main.php" <?php echo ($m_active == "Accounting") ? "class=\"active\"" : "" ?>><?php echo $l['menu']['Accounting']; ?></a></li>
+				<li><a href="billing-main.php" <?php echo ($m_active == "Billing") ? "class=\"active\"" : "" ?>><?php echo $l['menu']['Billing']; ?></a></li>
 				<li><a href="graph-main.php" <?php echo ($m_active == "Graphs") ? "class=\"active\"" : ""?>><?php echo $l['menu']['Graphs']; ?></li>
 
                                 </ul>
