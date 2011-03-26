c3178b708e3bdf0aaf03ab5da004efed9854bd85
Liran Tal <liran.tal@gmail.com>
2011-03-26 15:28:07 +0000

    fixed bug #2924004 which resolves an xss vul: http://seclists.org/bugtraq/2009/Dec/223

--- daloradius-users/login.php
+++ daloradius-users/login.php
@@ -21,6 +21,9 @@
  */
  
 	isset($_REQUEST['error']) ? $error = $_REQUEST['error'] : $error = "";
+
+	// clean up error code to avoid XSS
+	$error = strip_tags(htmlspecialchars($error));
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
