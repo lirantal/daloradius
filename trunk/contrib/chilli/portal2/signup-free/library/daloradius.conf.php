56c67af628457d2de9db43cd92f87be4d8ff7eee
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2011-04-28 23:45:58 +0000

    applying changes to add port number support | correcting usergroup table name to fit FR2 schema radusergroup table name | correcting 127.0.0.1 default host to localhost

--- trunk/contrib/chilli/portal2/signup-free/library/daloradius.conf.php
+++ trunk/contrib/chilli/portal2/signup-free/library/daloradius.conf.php
@@ -24,7 +24,8 @@
 
 
 $configValues['CONFIG_DB_ENGINE'] = 'mysql';
-$configValues['CONFIG_DB_HOST'] = '127.0.0.1';
+$configValues['CONFIG_DB_HOST'] = 'localhost';
+$configValues['CONFIG_DB_PORT'] = '3306';
 $configValues['CONFIG_DB_USER'] = 'root';
 $configValues['CONFIG_DB_PASS'] = '';
 $configValues['CONFIG_DB_NAME'] = 'radius';
@@ -32,7 +33,7 @@ $configValues['CONFIG_DB_TBL_RADCHECK'] = 'radcheck';
 $configValues['CONFIG_DB_TBL_RADREPLY'] = 'radreply';
 $configValues['CONFIG_DB_TBL_RADGROUPREPLY'] = 'radgroupreply';
 $configValues['CONFIG_DB_TBL_RADGROUPCHECK'] = 'radgroupcheck';
-$configValues['CONFIG_DB_TBL_RADUSERGROUP'] = 'usergroup';
+$configValues['CONFIG_DB_TBL_RADUSERGROUP'] = 'radusergroup';
 $configValues['CONFIG_DB_TBL_RADNAS'] = 'nas';
 $configValues['CONFIG_DB_TBL_RADPOSTAUTH'] = 'radpostauth';
 $configValues['CONFIG_DB_TBL_RADACCT'] = 'radacct';
