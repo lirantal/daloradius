0d560c5cc914d25ce0c9df26bb9041c6ada16eaf
lirantal <lirantal@e3f118d3-2fd6-40e1-a881-7fdf01391c53>
2011-05-19 20:38:50 +0000

    fixed missing SHA1-Password and CHAP-Password

--- trunk/mng-list-all.php
+++ trunk/mng-list-all.php
@@ -118,7 +118,7 @@
 		LEFT JOIN ".$configValues['CONFIG_DB_TBL_RADUSERGROUP']." disabled
 		 ON disabled.username=".$configValues['CONFIG_DB_TBL_DALOUSERINFO'].".username AND disabled.groupname = 'daloRADIUS-Disabled-Users' 
  		WHERE (".$configValues['CONFIG_DB_TBL_RADCHECK'].".username=userinfo.username) AND Attribute IN ('Cleartext-Password', 'Auth-Type','User-Password', 
- 			'Crypt-Password', 'MD5-Password', 'SMD5-Password', 'SHA-Password', 'SSHA-Password', 'NT-Password', 'LM-Password', 
+ 			'Crypt-Password', 'MD5-Password', 'SMD5-Password', 'SHA-Password', 'SSHA-Password', 'NT-Password', 'LM-Password', 'SHA1-Password', 'CHAP-Password', 
  			'NS-MTA-MD5-Password') GROUP by ".$configValues['CONFIG_DB_TBL_RADCHECK'].".Username ORDER BY '$orderBy' '$orderType' LIMIT $offset, $rowsPerPage";
 	$res = $dbSocket->query($sql);
 	$logDebugSQL = "";
