fdea65c7f1032cba4cd92b1c34155a5736125006
theFra985 <superfra99@gmail.com>
2018-03-02 21:51:34 +0100

    Permission check mysql
    Added mysql dirs chown
    Removed the &>/dev/null

--- init.sh
+++ init.sh
@@ -3,10 +3,11 @@
 # GitHub: git@github.com:lirantal/daloradius.git
 
 # Start the MySQL service
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 /etc/init.d/mysql start
 sleep 3
 
 # Start Apache2 in the foreground and in debug mode
-&>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start -e debug
+/usr/sbin/apachectl -DFOREGROUND -k start -e debug
 # Or with the local httpd.conf use
 #&>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start -e debug -d . -f httpd.conf
