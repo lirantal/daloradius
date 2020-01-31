2db6f2badfa5b90d0b8cd2be168bea8aa287cc8f
Filippo Lauria <filippo.lauria@iit.cnr.it>
2020-01-31 10:16:22 +0000

    Improved language control system

--- daloradius-users/lang/main.php
+++ daloradius-users/lang/main.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *********************************************************************************************************
+ *******************************************************************************
  * daloRADIUS - RADIUS Web Platform
  * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
  *
@@ -13,56 +13,65 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- *********************************************************************************************************
+ *******************************************************************************
  *
  * Authors:	Liran Tal <liran@enginx.com>
  *
- *********************************************************************************************************
+ *******************************************************************************
  */
+$langDir = dirname(__FILE__);
 
-	include_once('library/config_read.php');
+$langList = array_filter(scandir($langDir), function($fileName) {
+    global $langDir;
+    
+    $skipList = array(
+        ".", "..", "main.php",
+        "ro.php" // FIXME ro.php is currently broken and needs a fix...
+    );
+    
+    if (in_array($fileName, $skipList)) {
+        return false;
+    }
+    
+    $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    return !($ext == "php" and is_file("$langDir/$fileName"));
+});
 
-	switch($configValues['CONFIG_LANG']) {
+include_once("$langDir/../library/daloradius.conf.php");
+$langFile = $configValues["CONFIG_LANG"] . ".php";
 
-		case "en":
-			include (dirname(__FILE__)."/en.php");
-			break;
-		case "ru":
-			include (dirname(__FILE__)."/ru.php");
-			break;
-		case "ro":
-			include (dirname(__FILE__)."/ro.php");
-			break;
-		default:
-			include (dirname(__FILE__)."/en.php");
-			break;
-	}
+if (!in_array($langFile, $langList)) {
+    $langFile = "en.php"; // default language is english
+}
 
-	// Translation function
-	function t($a, $b = null, $c = null, $d = null)
-	{
-		global $l;
+// $langCode can be used in html tag elements like lang and/or xml:lang
+$langCode = str_replace("_", "-", pathinfo($langFile, PATHINFO_FILENAME));
+include("$langDir/$langFile");
 
-		$t = null;
+// Translation function
+function t($a, $b = null, $c = null, $d = null) {
+    global $l;
 
-		if($b === null) {
-			$t = isset($l[$a]) ? $l[$a] : null;
-		}
-		else if($c === null) {
-			$t = isset($l[$a][$b]) ? $l[$a][$b] : null;
-		}
-		else if($d === null) {
-			$t = isset($l[$a][$b][$c]) ? $l[$a][$b][$c] : null;
-		}
-		else {
-			$t = isset($l[$a][$b][$c][$d]) ? $l[$a][$b][$c][$d] : null;
-		}
+    $t = null;
 
-		if($t === null) {
-			$t = 'Lang Error!';
-		}
+    if($b === null) {
+        $t = isset($l[$a]) ? $l[$a] : null;
+    }
+    else if($c === null) {
+        $t = isset($l[$a][$b]) ? $l[$a][$b] : null;
+    }
+    else if($d === null) {
+        $t = isset($l[$a][$b][$c]) ? $l[$a][$b][$c] : null;
+    }
+    else {
+        $t = isset($l[$a][$b][$c][$d]) ? $l[$a][$b][$c][$d] : null;
+    }
 
-		return $t;
-	}
+    if($t === null) {
+        $t = "Lang Error!";
+    }
+
+    return $t;
+}
 
 ?>
--- lang/main.php
+++ lang/main.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *********************************************************************************************************
+ *******************************************************************************
  * daloRADIUS - RADIUS Web Platform
  * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
  *
@@ -13,79 +13,69 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- *********************************************************************************************************
+ *******************************************************************************
  * Description:
  *		Main language file control
  *
- * Authors:	Liran Tal <liran@enginx.com>
+ * Authors:
+ *      Liran Tal <liran@enginx.com>
  *
- *********************************************************************************************************
+ *******************************************************************************
  */
- 
-	include_once(dirname(__FILE__).'/../library/config_read.php');
-	
-	switch($configValues['CONFIG_LANG']) {
-	
-		case "en":
-			include (dirname(__FILE__)."/en.php");
-			break;
-		case "ru":
-			include (dirname(__FILE__)."/ru.php");
-			break;
-		case "hu":
-			include (dirname(__FILE__)."/hu.php");
-			break;
-		case "it":
-			include (dirname(__FILE__)."/it.php");
-			break;
-		case "es_VE":
-			include (dirname(__FILE__)."/es_VE.php");
-			break;
-		/*
-		 * file is currently broken and needs a fix...
-		case "ro":
-			include (dirname(__FILE__)."/ro.php");
-			break;
-		*/
-		case "pt_br":
-			include (dirname(__FILE__)."/pt_br.php");
-			break;
-		case "ja":
-			include (dirname(__FILE__)."/ja.php");
-			break;
-        case "zh":
-            include (dirname(__FILE__)."/zh.php");
-            break;
-		default:
-			include (dirname(__FILE__)."/en.php");
-			break;
-	}
-	
-	// Translation function
-	function t($a, $b = null, $c = null, $d = null)
-	{
-		global $l;
 
-		$t = null;
+$langDir = dirname(__FILE__);
 
-		if($b === null) {
-			$t = isset($l[$a]) ? $l[$a] : null;
-		}
-		else if($c === null) {
-			$t = isset($l[$a][$b]) ? $l[$a][$b] : null;
-		}
-		else if($d === null) {
-			$t = isset($l[$a][$b][$c]) ? $l[$a][$b][$c] : null;
-		}
-		else {
-			$t = isset($l[$a][$b][$c][$d]) ? $l[$a][$b][$c][$d] : null;
-		}
+$langList = array_filter(scandir($langDir), function($fileName) {
+    global $langDir;
+    
+    $skipList = array(
+        ".", "..", "main.php",
+        "ro.php" // FIXME ro.php is currently broken and needs a fix...
+    );
+    
+    if (in_array($fileName, $skipList)) {
+        return false;
+    }
+    
+    $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    return !($ext == "php" and is_file("$langDir/$fileName"));
+});
 
-		if($t === null) {
-			$t = 'Lang Error!';
-		}
+include_once("$langDir/../library/daloradius.conf.php");
+$langFile = $configValues["CONFIG_LANG"] . ".php";
 
-		return $t;
-	}
+if (!in_array($langFile, $langList)) {
+    $langFile = "en.php"; // default language is english
+}
+
+// $langCode can be used in html tag elements like lang and/or xml:lang
+$langCode = str_replace("_", "-", pathinfo($langFile, PATHINFO_FILENAME));
+include("$langDir/$langFile");
+
+// Translation function
+function t($a, $b = null, $c = null, $d = null) {
+    global $l;
+
+    $t = null;
+
+    if($b === null) {
+        $t = isset($l[$a]) ? $l[$a] : null;
+    }
+    else if($c === null) {
+        $t = isset($l[$a][$b]) ? $l[$a][$b] : null;
+    }
+    else if($d === null) {
+        $t = isset($l[$a][$b][$c]) ? $l[$a][$b][$c] : null;
+    }
+    else {
+        $t = isset($l[$a][$b][$c][$d]) ? $l[$a][$b][$c][$d] : null;
+    }
+
+    if($t === null) {
+        $t = "Lang Error!";
+    }
+
+    return $t;
+}
 
 ?>
