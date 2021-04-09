ae34e8c7b05111bd5523385fa9001a084ef12955
Liran Tal <liran.tal@gmail.com>
2021-04-09 11:07:51 +0300

    Update version.php

--- library/version.php
+++ library/version.php
@@ -22,5 +22,5 @@
  *********************************************************************************************************
  */
 
-$configValues['DALORADIUS_VERSION'] = '1.1-5 BETA';
+$configValues['DALORADIUS_VERSION'] = '1.1-5';
 $configValues['DALORADIUS_DATE'] = '09 Apr 2021';
