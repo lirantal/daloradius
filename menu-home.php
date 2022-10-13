0ca4f5c6e46c9fe4ca3b2cb6419916530e58954c
Filippo Lauria <filippo.lauria@iit.cnr.it>
2022-10-13 18:00:47 +0200

    Improved handling of menu subnav entries

--- menu-home.php
+++ menu-home.php
@@ -1,59 +1,100 @@
+<?php
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ *
+ * Authors:    Liran Tal <liran@enginx.com>
+ *             Filippo Lauria <filippo.lauria@iit.cnr.it>
+ *
+ *********************************************************************************************************
+ */
 
+// prevent this file to be directly accessed
+$this_file = '/menu-home.php';
+if (strpos($_SERVER['PHP_SELF'], $this_file) !== false) {
+    header("Location: /index.php");
+    exit;
+}
+
+include_once("lang/main.php");
+
+if (preg_match("/^\/menu\-([a-z]+)\.php$/", $this_file, $matches) !== false) {
+    $title = ucfirst($matches[1]);
+}
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= $langCode ?>" lang="<?= $langCode ?>">
 <head>
-<title>daloRADIUS</title>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/1.css" type="text/css" media="screen,projection" />
-
+    <title>daloRADIUS <?= (isset($title)) ? ":: $title" : "" ?></title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="css/1.css" media="screen">
 </head>
  
 <body>
 
-<?php
-        include_once ("lang/main.php");
-?>
-
-<div id="wrapper">
-<div id="innerwrapper">
+    <div id="wrapper">
+        <div id="innerwrapper">
 		
 <?php
 	$m_active = "Home";
-	include_once ("include/menu/menu-items.php");
-	include_once ("include/menu/home-subnav.php");
+	include_once("include/menu/menu-items.php");
+	include_once("include/menu/home-subnav.php");
+    
+    $status_subnav = array(
+                            'rep-stat-server.php' => t('button','ServerStatus'),
+                            'rep-stat-services.php' => t('button','ServicesStatus'),
+                            'rep-lastconnect.php' => t('button','LastConnectionAttempts')
+                          );
+                          
+    $logs_subnav = array(
+                            'rep-logs-radius.php' => t('button','RadiusLog'),
+                            'rep-logs-system.php' => t('button','SystemLog')
+                        );
+    
 ?>      
 
-<div id="sidebar">
+            <div id="sidebar">
+                <h2>Home</h2>
+                
+                <h3>Status</h3>
+                <ul class="subnav">
+<?php
+foreach ($status_subnav as $href => $label) {
+    printf('<li><a href="%s" title="%s"><b>&raquo;</b>%s</a></li>', $href, strip_tags($label), $label);
+}
+?>
 
-	<h2>Home</h2>
+                </ul><!-- .subnav -->
 
-	<h3>Status</h3>
+                <h3>Logs</h3>
+                <ul class="subnav">
+<?php
+foreach ($logs_subnav as $href => $label) {
+    printf('<li><a href="%s" title="%s"><b>&raquo;</b>%s</a></li>', $href, strip_tags($label), $label);
+}
+?>
 
-	<ul class="subnav">
-
-		<li><a href="rep-stat-server.php"><b>&raquo;</b><?php echo t('button','ServerStatus') ?></a></li>
-		<li><a href="rep-stat-services.php"><b>&raquo;</b><?php echo t('button','ServicesStatus') ?></a></li>
-		<li><a href="rep-lastconnect.php"><b>&raquo;</b><?php echo t('button','LastConnectionAttempts') ?></a></li>
-
-	<h3>Logs</h3>
-
-	        <li><a href="rep-logs-radius.php"><b>&raquo;</b><?php echo t('button','RadiusLog') ?></a></li>
-	        <li><a href="rep-logs-system.php"><b>&raquo;</b><?php echo t('button','SystemLog') ?></a></li>
-
-	</ul>
+                </ul><!-- .subnav -->
 	
-	<h3>Support</h3>
+                <h3>Support</h3>
 
-	<p class="news">
-		daloRADIUS <br/>
-		RADIUS Management 
-		<a href="https://github.com/lirantal/daloradius" class="more">Read More &raquo;</a>
-	</p>
-
-
-	
-
-	
-
-</div>
+                <p class="news">
+                    daloRADIUS<br>
+                    RADIUS Management 
+                    <a target="_blank" href="https://github.com/lirantal/daloradius" class="more">Read More &raquo;</a>
+                </p>
+            </div><!-- #sidebar -->
 
--- menu-mng-rad-usergroup.php
+++ menu-mng-rad-usergroup.php
@@ -1,77 +1,116 @@
-<script src="library/javascript/rounded-corners.js" type="text/javascript"></script>
-<script src="library/javascript/form-field-tooltip.js" type="text/javascript"></script>
-<link rel="stylesheet" href="css/form-field-tooltip.css" type="text/css" media="screen,projection" />
+<?php
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ *
+ * Authors:    Liran Tal <liran@enginx.com>
+ *             Filippo Lauria <filippo.lauria@iit.cnr.it>
+ *
+ *********************************************************************************************************
+ */
+
+// prevent this file to be directly accessed
+if (strpos($_SERVER['PHP_SELF'], '/menu-mng-rad-usergroup.php') !== false) {
+    header("Location: /index.php");
+    exit;
+}
+
+include_once("lang/main.php");
+
+?>
+<script src="library/javascript/rounded-corners.js"></script>
+<script src="library/javascript/form-field-tooltip.js"></script>
+<link rel="stylesheet" href="css/form-field-tooltip.css" media="screen">
 
 <body>
-<?php
-    include_once ("lang/main.php");
-?>
-<div id="wrapper">
-<div id="innerwrapper">
+    <div id="wrapper">
+        <div id="innerwrapper">
 
 <?php
     $m_active = "Management";
-    include_once ("include/menu/menu-items.php");
-	include_once ("include/menu/management-subnav.php");
-        include_once("include/management/autocomplete.php");
+    include_once("include/menu/menu-items.php");
+	include_once("include/menu/management-subnav.php");
+    include_once("include/management/autocomplete.php");
 ?>
 
-<div id="sidebar">
+            <div id="sidebar">
+                <h2>Management</h2>
 
+                <h3>User-Group Management</h3>
+                <ul class="subnav">
 
-	<h2>Management</h2>
+                    <li>
+                        <a title="<?= strip_tags(t('button','ListUserGroup')) ?>" href="mng-rad-usergroup-list.php">
+                            <b>&raquo;</b><?= t('button','ListUserGroup') ?>
+                        </a>
+                    </li>
+                        
+                    <li>
+                        <a title="<?= strip_tags(t('button','ListUsersGroup')) ?>" href="javascript:document.mngradusrgrplist.submit();">
+                            <b>&raquo;</b><?= t('button','ListUsersGroup') ?>
+                        <a>
+                        <form name="mngradusrgrplist" action="mng-rad-usergroup-list-user.php" method="GET" class="sidebar">
+                            <input name="username" type="text" id="usernameList" <?= ($autoComplete) ? 'autocomplete="off"' : "" ?>
+                                tooltipText="<?= t('Tooltip','Username'); ?><br>" value="<?= (isset($usernameList)) ? $usernameList : "" ?>">
+                        </form>
+                    </li>
 
-	<h3>User-Group Management</h3>
-	<ul class="subnav">
+                    <li>
+                        <a title="<?= strip_tags(t('button','NewUserGroup')) ?>" href="mng-rad-usergroup-new.php">
+                            <b>&raquo;</b><?= t('button','NewUserGroup') ?>
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a title="<?= strip_tags(t('button','EditUserGroup')) ?>" href="javascript:document.mngradusrgrpedit.submit();">
+                            <b>&raquo;</b><?= t('button','EditUserGroup') ?>
+                        </a>
+                        <form name="mngradusrgrpedit" action="mng-rad-usergroup-edit.php" method="GET" class="sidebar">
+                            <input name="username" type="text" value="" id="usernameEdit" <?= ($autoComplete) ? 'autocomplete="off"' : "" ?>
+                                tooltipText="<?= t('Tooltip','Username'); ?><br>">
+                            <input name="group" type="text" value="" id="groupnameEdit" <?= ($autoComplete) ? 'autocomplete="off"' : "" ?>
+                                tooltipText="<?= t('Tooltip','GroupName'); ?><br>">
+                        </form>
+                    </li>
 
-		<li><a href="mng-rad-usergroup-list.php"><b>&raquo;</b><?php echo t('button','ListUserGroup') ?></a></li>
-		<li><a href="javascript:document.mngradusrgrplist.submit();""><b>&raquo;</b><?php echo t('button','ListUsersGroup') ?><a>
-			<form name="mngradusrgrplist" action="mng-rad-usergroup-list-user.php" method="get" 
-				class="sidebar">
-			<input name="username" type="text" id="usernameList" <?php if ($autoComplete) echo "autocomplete='off'"; ?>
-                                tooltipText='<?php echo t('Tooltip','Username'); ?> <br/>'
-				/>
-			</form></li>
+                    <li>
+                        <a title="<?= strip_tags(t('button','RemoveUserGroup')) ?>" href="mng-rad-usergroup-del.php">
+                            <b>&raquo;</b><?= t('button','RemoveUserGroup') ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
 
-		<li><a href="mng-rad-usergroup-new.php"><b>&raquo;</b><?php echo t('button','NewUserGroup') ?></a></li>
-		<li><a href="javascript:document.mngradusrgrpedit.submit();""><b>&raquo;</b><?php echo t('button','EditUserGroup') ?><a>
-			<form name="mngradusrgrpedit" action="mng-rad-usergroup-edit.php" method="get" class="sidebar">
-			<input name="username" type="text" value="" id="usernameEdit" <?php if ($autoComplete) echo "autocomplete='off'"; ?>
-                                tooltipText='<?php echo t('Tooltip','Username'); ?> <br/>'
-				/>
-			<input name="group" type="text" value="" id="groupnameEdit" <?php if ($autoComplete) echo "autocomplete='off'"; ?>
-                                tooltipText='<?php echo t('Tooltip','GroupName'); ?> <br/>'
-				/>
-			</form></li>
-
-
-		<li><a href="mng-rad-usergroup-del.php"><b>&raquo;</b><?php echo t('button','RemoveUserGroup') ?></a></li>
-	</ul>
-
-</div>
-
+<script>
 <?php
-        include_once("include/management/autocomplete.php");
-
-        if ($autoComplete) {
-                echo "<script type=\"text/javascript\">
-                      autoComEdit = new DHTMLSuite.autoComplete();
-                      autoComEdit.add('usernameList','include/management/dynamicAutocomplete.php','_small','getAjaxAutocompleteUsernames');
-
-                      autoComEdit = new DHTMLSuite.autoComplete();
-                      autoComEdit.add('usernameEdit','include/management/dynamicAutocomplete.php','_small','getAjaxAutocompleteUsernames');
-
-                      autoComEdit = new DHTMLSuite.autoComplete();
-                      autoComEdit.add('groupnameEdit','include/management/dynamicAutocomplete.php','_small','getAjaxAutocompleteGroupName');
-                      </script>";
-        }
-
+    if ($autoComplete) {
 ?>
+    var autoComEdit = new DHTMLSuite.autoComplete();
+    autoComEdit.add('usernameList','include/management/dynamicAutocomplete.php','_small','getAjaxAutocompleteUsernames');
 
-<script type="text/javascript">
-        var tooltipObj = new DHTMLgoodies_formTooltip();
-        tooltipObj.setTooltipPosition('right');
-        tooltipObj.setPageBgColor('#EEEEEE');
-        tooltipObj.setTooltipCornerSize(15);
-        tooltipObj.initFormFieldTooltip();
+    autoComEdit = new DHTMLSuite.autoComplete();
+    autoComEdit.add('usernameEdit','include/management/dynamicAutocomplete.php','_small','getAjaxAutocompleteUsernames');
+
+    autoComEdit = new DHTMLSuite.autoComplete();
+    autoComEdit.add('groupnameEdit','include/management/dynamicAutocomplete.php','_small','getAjaxAutocompleteGroupName');
+<?php
+    }
+?>
+    var tooltipObj = new DHTMLgoodies_formTooltip();
+    tooltipObj.setTooltipPosition('right');
+    tooltipObj.setPageBgColor('#EEEEEE');
+    tooltipObj.setTooltipCornerSize(15);
+    tooltipObj.initFormFieldTooltip();
 </script>
