c23dcc1058c3c026a7a36425c2075db0fd977707
Filippo Lauria <filippo.lauria@iit.cnr.it>
2022-12-07 15:49:02 +0100

    Implemented csrf and new layout printing feature

--- config-db.php
+++ config-db.php
@@ -34,77 +34,85 @@
     include("library/validation.php");
     
     $db_tbl_param_label = array(
-                                'CONFIG_DB_TBL_RADCHECK' => t('all','radcheck'), 
-                                'CONFIG_DB_TBL_RADREPLY' => t('all','radreply'),
-                                'CONFIG_DB_TBL_RADGROUPREPLY' => t('all','radgroupreply'), 
-                                'CONFIG_DB_TBL_RADGROUPCHECK' => t('all','radgroupcheck'), 
-                                'CONFIG_DB_TBL_RADUSERGROUP' => t('all','usergroup'), 
-                                'CONFIG_DB_TBL_RADACCT' => t('all','radacct'), 
-                                'CONFIG_DB_TBL_RADNAS' => t('all','nas'),
-                                'CONFIG_DB_TBL_RADHG' => t('all','hunt'), 
-                                'CONFIG_DB_TBL_RADPOSTAUTH' => t('all','radpostauth'), 
-                                'CONFIG_DB_TBL_RADIPPOOL' => t('all','radippool'), 
-                                'CONFIG_DB_TBL_DALOUSERINFO' => t('all','userinfo'), 
-                                'CONFIG_DB_TBL_DALODICTIONARY' => t('all','dictionary'), 
-                                'CONFIG_DB_TBL_DALOREALMS' => t('all','realms'), 
-                                'CONFIG_DB_TBL_DALOPROXYS' => t('all','proxys'), 
-                                'CONFIG_DB_TBL_DALOBILLINGMERCHANT' => t('all','billingmerchant'), 
-                                'CONFIG_DB_TBL_DALOBILLINGPAYPAL' => t('all','billingpaypal'), 
-                                'CONFIG_DB_TBL_DALOBILLINGPLANS' => t('all','billingplans'), 
-                                'CONFIG_DB_TBL_DALOBILLINGRATES' => t('all','billingrates'), 
-                                'CONFIG_DB_TBL_DALOBILLINGHISTORY' => t('all','billinghistory'), 
-                                'CONFIG_DB_TBL_DALOUSERBILLINFO' => t('all','billinginfo'), 
-                                'CONFIG_DB_TBL_DALOBILLINGINVOICE' => t('all','Invoice'), 
-                                'CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS' => t('all','InvoiceItems'), 
-                                'CONFIG_DB_TBL_DALOBILLINGINVOICESTATUS' => t('all','InvoiceStatus'), 
-                                'CONFIG_DB_TBL_DALOBILLINGINVOICETYPE' => t('all','InvoiceType'), 
-                                'CONFIG_DB_TBL_DALOPAYMENTTYPES' => t('all','payment_type'), 
-                                'CONFIG_DB_TBL_DALOPAYMENTS' => t('all','payments'), 
-                                'CONFIG_DB_TBL_DALOOPERATORS' => t('all','operators'), 
-                                'CONFIG_DB_TBL_DALOOPERATORS_ACL' => t('all','operators_acl'), 
-                                'CONFIG_DB_TBL_DALOOPERATORS_ACL_FILES' => t('all','operators_acl_files'), 
-                                'CONFIG_DB_TBL_DALOHOTSPOTS' => t('all','hotspots'), 
-                                'CONFIG_DB_TBL_DALONODE' => t('all','node'), 
-                            );
+                                    'CONFIG_DB_TBL_RADCHECK' => t('all','radcheck'), 
+                                    'CONFIG_DB_TBL_RADREPLY' => t('all','radreply'),
+                                    'CONFIG_DB_TBL_RADGROUPREPLY' => t('all','radgroupreply'), 
+                                    'CONFIG_DB_TBL_RADGROUPCHECK' => t('all','radgroupcheck'), 
+                                    'CONFIG_DB_TBL_RADUSERGROUP' => t('all','usergroup'), 
+                                    'CONFIG_DB_TBL_RADACCT' => t('all','radacct'), 
+                                    'CONFIG_DB_TBL_RADNAS' => t('all','nas'),
+                                    'CONFIG_DB_TBL_RADHG' => t('all','hunt'), 
+                                    'CONFIG_DB_TBL_RADPOSTAUTH' => t('all','radpostauth'), 
+                                    'CONFIG_DB_TBL_RADIPPOOL' => t('all','radippool'), 
+                                    'CONFIG_DB_TBL_DALOUSERINFO' => t('all','userinfo'), 
+                                    'CONFIG_DB_TBL_DALODICTIONARY' => t('all','dictionary'), 
+                                    'CONFIG_DB_TBL_DALOREALMS' => t('all','realms'), 
+                                    'CONFIG_DB_TBL_DALOPROXYS' => t('all','proxys'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGMERCHANT' => t('all','billingmerchant'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGPAYPAL' => t('all','billingpaypal'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGPLANS' => t('all','billingplans'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGRATES' => t('all','billingrates'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGHISTORY' => t('all','billinghistory'), 
+                                    'CONFIG_DB_TBL_DALOUSERBILLINFO' => t('all','billinginfo'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGINVOICE' => t('all','Invoice'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGINVOICEITEMS' => t('all','InvoiceItems'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGINVOICESTATUS' => t('all','InvoiceStatus'), 
+                                    'CONFIG_DB_TBL_DALOBILLINGINVOICETYPE' => t('all','InvoiceType'), 
+                                    'CONFIG_DB_TBL_DALOPAYMENTTYPES' => t('all','payment_type'), 
+                                    'CONFIG_DB_TBL_DALOPAYMENTS' => t('all','payments'), 
+                                    'CONFIG_DB_TBL_DALOOPERATORS' => t('all','operators'), 
+                                    'CONFIG_DB_TBL_DALOOPERATORS_ACL' => t('all','operators_acl'), 
+                                    'CONFIG_DB_TBL_DALOOPERATORS_ACL_FILES' => t('all','operators_acl_files'), 
+                                    'CONFIG_DB_TBL_DALOHOTSPOTS' => t('all','hotspots'), 
+                                    'CONFIG_DB_TBL_DALONODE' => t('all','node'), 
+                                );
     
     $generic_db_conf_params = array(
-                                    'CONFIG_DB_HOST' => t('all','DatabaseHostname'),
-                                    'CONFIG_DB_USER' => t('all','DatabaseUser'),
-                                    'CONFIG_DB_PASS' => t('all','DatabasePass'),
-                                    'CONFIG_DB_NAME' => t('all','DatabaseName'),
-                               );
+                                        'CONFIG_DB_HOST' => t('all','DatabaseHostname'),
+                                        'CONFIG_DB_USER' => t('all','DatabaseUser'),
+                                        'CONFIG_DB_PASS' => t('all','DatabasePass'),
+                                        'CONFIG_DB_NAME' => t('all','DatabaseName'),
+                                   );
     
     $tbl_name_regex = '^[a-zA-Z0-9_]+$';
     
-    // if the form has been submitted we validate and store the configuration
-    if (array_key_exists('submit', $_POST) && isset($_POST['submit'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (array_key_exists('csrf_token', $_POST) && isset($_POST['csrf_token']) && dalo_check_csrf_token($_POST['csrf_token'])) {
         
-        if (array_key_exists('CONFIG_DB_ENGINE', $_POST) && isset($_POST['CONFIG_DB_ENGINE']) &&
-            in_array(strtolower($_POST['CONFIG_DB_ENGINE']), array_keys($valid_db_engines))) {
-            $configValues['CONFIG_DB_ENGINE'] = $_POST['CONFIG_DB_ENGINE']; 
+            // if the form has been submitted we validate and store the configuration
+            if (array_key_exists('CONFIG_DB_ENGINE', $_POST) && isset($_POST['CONFIG_DB_ENGINE']) &&
+                in_array(strtolower($_POST['CONFIG_DB_ENGINE']), array_keys($valid_db_engines))) {
+                $configValues['CONFIG_DB_ENGINE'] = $_POST['CONFIG_DB_ENGINE']; 
+            }
+        
+            if (array_key_exists('CONFIG_DB_PORT', $_POST) && isset($_POST['CONFIG_DB_PORT']) &&
+                intval($_POST['CONFIG_DB_PORT']) >= 0 && intval($_POST['CONFIG_DB_PORT']) <= 65535) {
+                $configValues['CONFIG_DB_PORT'] = intval($_POST['CONFIG_DB_PORT']);
+            }
+
+            foreach ($generic_db_conf_params as $param => $caption) {
+                if (array_key_exists($param, $_POST) && isset($_POST[$param])) {
+                    $configValues[$param] = $_POST[$param];
+                }
+            }
+
+            // validate table name
+            foreach ($db_tbl_param_label as $param => $label) {
+                if (array_key_exists($param, $_POST) && isset($_POST[$param]) && preg_match($tbl_name_regex, $_POST[$param]) !== false) {
+                    $configValues[$param] = $_POST[$param];
+                }
+            }
+
+            include("library/config_write.php");
+        
+        } else {
+            // csrf
+            $failureMsg = "CSRF token error";
+            $logAction .= "$failureMsg on page: ";
         }
+    }
     
-        if (array_key_exists('CONFIG_DB_PORT', $_POST) && isset($_POST['CONFIG_DB_PORT']) &&
-            intval($_POST['CONFIG_DB_PORT']) >= 0 && intval($_POST['CONFIG_DB_PORT']) <= 65535) {
-            $configValues['CONFIG_DB_PORT'] = intval($_POST['CONFIG_DB_PORT']);
-        }
-
-        foreach ($generic_db_conf_params as $param => $caption) {
-            if (array_key_exists($param, $_POST) && isset($_POST[$param])) {
-                $configValues[$param] = $_POST[$param];
-            }
-        }
-
-        // validate table name
-        foreach ($db_tbl_param_label as $param => $label) {
-            if (array_key_exists($param, $_POST) && isset($_POST[$param]) && preg_match($tbl_name_regex, $_POST[$param]) !== false) {
-                $configValues[$param] = $_POST[$param];
-            }
-        }
-
-        include("library/config_write.php");
-    }    
-
+    
     include("library/layout.php");
 
     // print HTML prologue
@@ -130,110 +138,110 @@
     
     include_once('include/management/actionMessages.php');
     
-    // set navbar stuff
-    $navbuttons = array(
-                          'Settings-tab' => t('title','Settings'),
-                          'DatabaseTables-tab' => t('title','DatabaseTables'),
-                       );
-
-    print_tab_navbuttons($navbuttons);
-    
     
     $submit_descriptor = array(
                                 "type" => "submit",
                                 "name" => "submit",
                                 "value" => t('buttons','apply')
                               );
-?>
-
-<form name="dbsettings" method="POST">
-        
-    <div id="Settings-tab" class="tabcontent" title="<?= t('title','Settings') ?>" style="display: block">
-        <fieldset>
-            <h302><?= t('title','Settings') ?></h302>
-            
-            <br/>
-
-            <ul>
-
-                <li class="fieldset">
-                    <label for="CONFIG_DB_ENGINE" class="form"><?= t('all','DBEngine')?></label>
-                    <select class="form" name="CONFIG_DB_ENGINE" id="CONFIG_DB_ENGINE">
-<?php
-    foreach ($valid_db_engines as $value => $caption) {
-        $selected = (strtolower($configValues['CONFIG_DB_ENGINE']) === $value) ? " selected" : "";
-        printf('<option value="%s"%s>%s</option>', $value, $selected, $caption);
-    }
-?>
-                    </select>
-                </li>
-
-                <li class="fieldset">
-                    <label for="CONFIG_DB_PORT" class="form"><?= t('all','DatabasePort') ?></label>
-                    <input type="number" min="0" max="65535" value="<?= $configValues['CONFIG_DB_PORT'] ?>"
-                        name="CONFIG_DB_PORT" id="CONFIG_DB_PORT">
-                </li>
-
-<?php
-        foreach ($generic_db_conf_params as $param => $label) {
-            $value = htmlspecialchars($configValues[$param], ENT_QUOTES, 'UTF-8');
-            
-            echo '<li class="fieldset">';
-            printf('<label for="%s" class="form">%s</label>', $param, $label);
-            printf('<input type="text" value="%s" name="%s" id="%s">', $value, $param, $param);
-            echo '</li>';
-        }
-?>
-
-            </ul>
-
-        </fieldset>
-
-<?php
-        print_form_component($submit_descriptor);
-?>
-
-    </div><!-- #Settings-tab -->
-
-     <div id="DatabaseTables-tab" class="tabcontent" title="<?= t('title','DatabaseTables') ?>">
-        <fieldset>
-            <h302><?= t('title','DatabaseTables') ?></h302>
     
-            <br>
+    // first tab
+    $input_descriptors1 = array();
+    
+    $input_descriptors1[] = array(
+                                        "name" => "CONFIG_DB_ENGINE",
+                                        "caption" => t('all','DBEngine'),
+                                        "selected_value" => $configValues['CONFIG_DB_ENGINE'],
+                                        "type" => "select",
+                                        "options" => $valid_db_engines,
+                                     );
+    
+    $input_descriptors1[] = array(
+                                        "name" => "CONFIG_DB_PORT",
+                                        "caption" => t('all','DatabasePort'),
+                                        "value" => $configValues['CONFIG_DB_PORT'],
+                                        "type" => "number",
+                                        "min" => "0",
+                                        "max" => "65535",
+                                     );
+    
+    foreach ($generic_db_conf_params as $name => $caption) {
+        $input_descriptors1[] = array(
+                                        "name" => $name,
+                                        "caption" => $caption,
+                                        "value" => $configValues[$name],
+                                        "type" => "text"
+                                     );
+    }
 
-            <ul>
+    $input_descriptors1[] = array(
+                                    "name" => "csrf_token",
+                                    "type" => "hidden",
+                                    "value" => dalo_csrf_token(),
+                                 );
 
-<?php
-        foreach ($db_tbl_param_label as $param => $label) {
-            $value = htmlspecialchars($configValues[$param], ENT_QUOTES, 'UTF-8');
-            
-            echo '<li class="fieldset">';
-            printf('<label for="%s" class="form">%s</label>', $param, $label);
-            printf('<input type="text" value="%s" name="%s" id="%s" pattern="%s">', $value, $param, $param, $tbl_name_regex);
-            echo '</li>';
-        }
-?>
+    $input_descriptors1[] = $submit_descriptor;
 
-            </ul>
-        </fieldset>
-        
-<?php
-        print_form_component($submit_descriptor);
-?>
-        
-    </div><!-- #DatabaseTables-tab -->
-</form>
+    $fieldset1_descriptor = array(
+                                    "title" => t('title','Settings'),
+                                 );
 
-        </div><!-- #contentnorightbar -->
-        
-        <div id="footer">
-<?php
+    // second tab
+    $input_descriptors2 = array();
+    foreach ($db_tbl_param_label as $name => $caption) {
+        $input_descriptors2[] = array(
+                                        "name" => $name,
+                                        "caption" => $caption,
+                                        "value" => $configValues[$name],
+                                        "pattern" => $tbl_name_regex,
+                                        "type" => "text"
+                                     );
+    }
+    
+    $input_descriptors2[] = $submit_descriptor;
+    
+    $fieldset2_descriptor = array(
+                                    "title" => t('title','DatabaseTables'),
+                                 );
+    
+    // set navbar stuff
+    $navkeys = array( 'Settings', 'DatabaseTables' );
+
+    // print navbar controls
+    print_tab_header($navkeys);
+
+    // open form
+    open_form();
+    
+    // open first tab (shown)
+    open_tab($navkeys, 0, true);
+    
+    open_fieldset($fieldset1_descriptor);
+
+    foreach ($input_descriptors1 as $input_descriptor) {
+        print_form_component($input_descriptor);
+    }
+    
+    close_fieldset();
+    
+    close_tab();
+    
+    // open second tab (hidden)
+    open_tab($navkeys, 1);
+    
+    open_fieldset($fieldset2_descriptor);
+
+    foreach ($input_descriptors2 as $input_descriptor) {
+        print_form_component($input_descriptor);
+    }
+    
+    close_fieldset();
+    
+    close_tab();
+    
+    close_form();
+    
     include('include/config/logging.php');
-    include('page-footer.php');
-?>
-        </div><!-- #footer -->
-    </div>
-</div>
+    print_footer_and_html_epilogue();
 
-</body>
-</html>
+?>
