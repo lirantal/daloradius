dc7f891f70d17ea44e33e9c55cd712c534aa4e4e
Filippo Lauria <filippo.lauria@iit.cnr.it>
2022-12-14 15:58:06 +0100

    New freeradius (3.0) mysql db schema

---
+++ contrib/db/fr3-mysql-freeradius.sql
@@ -0,0 +1,240 @@
+##############################################################################################
+#                                                                                            #
+#  schema.sql                       rlm_sql - FreeRADIUS SQL Module                          #
+#                                                                                            #
+#     Database schema for MySQL rlm_sql module                                               #
+#                                                                                            #
+#     To load:                                                                               #
+#         mysql -uroot -prootpass radius < schema.sql                                        #
+#                                                                                            #
+#                                   Mike Machado <mike@innercite.com>                        #
+#                                                                                            #
+#     some additions/adaptations for daloRADIUS have been made by:                           #
+#                                                                                            #
+#                                   Liran Tal <liran.tal@gmail.com>                          #
+#                                   Filippo Maria Del Prete <filippo.delprete@gmail.com>     #
+#                                   Filippo Lauria <filippo.lauria@iit.cnr.it>               #
+#                                                                                            #
+##############################################################################################
+
+#
+# Table structure for table 'radacct'
+#
+
+CREATE TABLE IF NOT EXISTS radacct (
+  radacctid BIGINT(21) NOT NULL AUTO_INCREMENT,
+  acctsessionid VARCHAR(64) NOT NULL DEFAULT '',
+  acctuniqueid VARCHAR(32) NOT NULL DEFAULT '',
+  username VARCHAR(64) NOT NULL DEFAULT '',
+  realm VARCHAR(64) DEFAULT '',
+  nasipaddress VARCHAR(15) NOT NULL DEFAULT '',
+  nasportid VARCHAR(32) DEFAULT NULL,
+  nasporttype VARCHAR(32) DEFAULT NULL,
+  acctstarttime DATETIME NULL DEFAULT NULL,
+  acctupdatetime DATETIME NULL DEFAULT NULL,
+  acctstoptime DATETIME NULL DEFAULT NULL,
+  acctinterval INT(12) DEFAULT NULL,
+  acctsessiontime INT(12) UNSIGNED DEFAULT NULL,
+  acctauthentic VARCHAR(32) DEFAULT NULL,
+  connectinfo_start VARCHAR(50) DEFAULT NULL,
+  connectinfo_stop VARCHAR(50) DEFAULT NULL,
+  acctinputoctets BIGINT(20) DEFAULT NULL,
+  acctoutputoctets BIGINT(20) DEFAULT NULL,
+  calledstationid VARCHAR(50) NOT NULL DEFAULT '',
+  callingstationid VARCHAR(50) NOT NULL DEFAULT '',
+  acctterminatecause VARCHAR(32) NOT NULL DEFAULT '',
+  servicetype VARCHAR(32) DEFAULT NULL,
+  framedprotocol VARCHAR(32) DEFAULT NULL,
+  framedipaddress VARCHAR(15) NOT NULL DEFAULT '',
+  framedipv6address VARCHAR(45) NOT NULL DEFAULT '',
+  framedipv6prefix VARCHAR(45) NOT NULL DEFAULT '',
+  framedinterfaceid VARCHAR(44) NOT NULL DEFAULT '',
+  delegatedipv6prefix VARCHAR(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (radacctid),
+  UNIQUE KEY acctuniqueid (acctuniqueid),
+  KEY username (username),
+  KEY framedipaddress (framedipaddress),
+  KEY framedipv6address (framedipv6address),
+  KEY framedipv6prefix (framedipv6prefix),
+  KEY framedinterfaceid (framedinterfaceid),
+  KEY delegatedipv6prefix (delegatedipv6prefix),
+  KEY acctsessionid (acctsessionid),
+  KEY acctsessiontime (acctsessiontime),
+  KEY acctstarttime (acctstarttime),
+  KEY acctinterval (acctinterval),
+  KEY acctstoptime (acctstoptime),
+  KEY nasipaddress (nasipaddress)
+);
+
+#
+# Table structure for table 'radcheck'
+#
+
+CREATE TABLE IF NOT EXISTS radcheck (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(64) NOT NULL DEFAULT '',
+  attribute VARCHAR(64) NOT NULL DEFAULT '',
+  op char(2) NOT NULL DEFAULT '==',
+  value VARCHAR(253) NOT NULL DEFAULT '',
+  PRIMARY KEY (id),
+  KEY username (username(32))
+);
+
+#
+# Table structure for table 'radgroupcheck'
+#
+
+CREATE TABLE IF NOT EXISTS radgroupcheck (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  groupname VARCHAR(64) NOT NULL DEFAULT '',
+  attribute VARCHAR(64) NOT NULL DEFAULT '',
+  op char(2) NOT NULL DEFAULT '==',
+  value VARCHAR(253) NOT NULL DEFAULT '',
+  PRIMARY KEY (id),
+  KEY groupname (groupname(32))
+);
+
+#
+# Table structure for table 'radgroupreply'
+#
+
+CREATE TABLE IF NOT EXISTS radgroupreply (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  groupname VARCHAR(64) NOT NULL DEFAULT '',
+  attribute VARCHAR(64) NOT NULL DEFAULT '',
+  op CHAR(2) NOT NULL DEFAULT '=',
+  value VARCHAR(253) NOT NULL DEFAULT '',
+  PRIMARY KEY (id),
+  KEY groupname (groupname(32))
+);
+
+#
+# Table structure for table 'radreply'
+#
+
+CREATE TABLE IF NOT EXISTS radreply (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(64) NOT NULL DEFAULT '',
+  attribute VARCHAR(64) NOT NULL DEFAULT '',
+  op CHAR(2) NOT NULL DEFAULT '=',
+  value VARCHAR(253) NOT NULL DEFAULT '',
+  PRIMARY KEY (id),
+  KEY username (username(32))
+);
+
+
+#
+# Table structure for table 'radusergroup'
+#
+
+CREATE TABLE IF NOT EXISTS radusergroup (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(64) NOT NULL DEFAULT '',
+  groupname VARCHAR(64) NOT NULL DEFAULT '',
+  priority INT(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (id),
+  KEY username (username(32))
+);
+
+#
+# Table structure for table 'radpostauth'
+#
+# Note: MySQL versions since 5.6.4 support fractional precision timestamps
+#        which we use here. Replace the authdate definition with the following
+#        if your software is too old:
+#
+#   authdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+#
+
+CREATE TABLE IF NOT EXISTS radpostauth (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(64) NOT NULL DEFAULT '',
+  pass VARCHAR(64) NOT NULL DEFAULT '',
+  reply VARCHAR(32) NOT NULL DEFAULT '',
+  authdate TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (id),
+  KEY username (username(32))
+);
+
+#
+# Table structure for table 'nas'
+#
+
+CREATE TABLE IF NOT EXISTS nas (
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  nasname VARCHAR(128) NOT NULL,
+  shortname VARCHAR(32),
+  type VARCHAR(30) DEFAULT 'other',
+  ports INT(5),
+  secret VARCHAR(60) DEFAULT 'secret' NOT NULL,
+  server VARCHAR(64),
+  community VARCHAR(50),
+  description VARCHAR(200) DEFAULT 'RADIUS Client',
+  PRIMARY KEY (id),
+  KEY nasname (nasname)
+);
+
+#
+# Table structure for table 'radippool'
+#
+
+CREATE TABLE radippool (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  pool_name VARCHAR(30) NOT NULL,
+  framedipaddress VARCHAR(15) NOT NULL DEFAULT '',
+  nasipaddress VARCHAR(15) NOT NULL DEFAULT '',
+  calledstationid VARCHAR(30) NOT NULL,
+  callingstationid VARCHAR(30) NOT NULL,
+  expiry_time DATETIME NULL DEFAULT NULL,
+  username VARCHAR(64) NOT NULL DEFAULT '',
+  pool_key VARCHAR(30) NOT NULL DEFAULT '',
+  PRIMARY KEY (id),
+  KEY radippool_poolname_expire (pool_name, expiry_time),
+  KEY framedipaddress (framedipaddress),
+  KEY radippool_nasip_poolkey_ipaddress (nasipaddress, pool_key, framedipaddress)
+);
+
+#
+# Table structure for table 'wimax' (WiMAX),
+# which replaces the "radpostauth" table.
+#
+
+CREATE TABLE wimax (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(64) NOT NULL DEFAULT '',
+  authdate TIMESTAMP NOT NULL,
+  spi VARCHAR(16) NOT NULL DEFAULT '',
+  mipkey VARCHAR(400) NOT NULL DEFAULT '',
+  lifetime INT(12) DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY username (username),
+  KEY spi (spi)
+) ;
+
+#
+# Table structure for table 'cui'
+#
+
+CREATE TABLE `cui` (
+  `clientipaddress` VARCHAR(46) NOT NULL DEFAULT '',
+  `callingstationid` VARCHAR(50) NOT NULL DEFAULT '',
+  `username` VARCHAR(64) NOT NULL DEFAULT '',
+  `cui` VARCHAR(32) NOT NULL DEFAULT '',
+  `creationdate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastaccounting` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`username`,`clientipaddress`,`callingstationid`)
+);
+
+#
+# Table structure for table 'radhuntgroup'
+# source: https://wiki.freeradius.org/guide/SQL-Huntgroup-HOWTO
+#
+
+CREATE TABLE radhuntgroup (
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    groupname VARCHAR(64) NOT NULL DEFAULT '',
+    nasipaddress VARCHAR(15) NOT NULL DEFAULT '',
+    nasportid VARCHAR(15) DEFAULT NULL,
+    PRIMARY KEY (id),
+    KEY nasipaddress (nasipaddress)
+);
