55464d1fa02bbbe124f6a8db57a14b0013087a16
Filippo Lauria <filippo.lauria@iit.cnr.it>
2023-01-04 13:53:19 +0100

    improving configuration read/write function

--- library/config_write.php
+++ library/config_write.php
@@ -14,59 +14,93 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  *********************************************************************************************************
- * Description:
- * 		writes configuration information from the $configValues array to daloradius.conf
  *
- * Authors:	Liran Tal <liran@enginx.com>
+ * Description:    writes configuration information from the $configValues array
+ *                 to the daloradius.conf.php configuration file
+ * 
+ * Authors:        Liran Tal <liran@enginx.com>
+ *                 Filippo Lauria <filippo.lauria@iit.cnr.it>
  *
  *********************************************************************************************************
  */
 
-$configFile = dirname(__FILE__).'/daloradius.conf.php';
+// prevent this file to be directly accessed
+if (strpos($_SERVER['PHP_SELF'], '/library/config_write.php') !== false) {
+    header("Location: ../index.php");
+    exit;
+}
+
+// useful variables
+$configFile = dirname(__FILE__) . '/daloradius.conf.php';
 $date = date("D M j G:i:s T Y");
 
-$fp = fopen($configFile, "w");
-if ($fp) {
-	fwrite($fp, 
-		"<?php\n".
-		"/*\n".
-		" *********************************************************************************************************\n".
-		" * daloRADIUS - RADIUS Web Platform\n".
-		" * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.\n".
-		" *\n".
-		" * This program is free software; you can redistribute it and/or\n".
-		" * modify it under the terms of the GNU General Public License\n".
-		" * as published by the Free Software Foundation; either version 2\n".
-		" * of the License, or (at your option) any later version.\n".
-		" *\n".
-		" * You should have received a copy of the GNU General Public License\n".
-		" * along with this program; if not, write to the Free Software\n".
-		" * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.\n".
-		" *\n".
-		" *********************************************************************************************************\n".
-		" * Description:\n".
-		" *              daloRADIUS Configuration File\n".
-		" *\n".
-		" * Modification Date:\n".
-		" *              $date\n".
-		" *********************************************************************************************************\n".
-		" */\n".
-		"\n\n");
-	foreach ($configValues as $_configOption => $_configElem) {
-		if (is_array($configValues[$_configOption])) {
-			$var = "\$configValues['" . $_configOption . "'] = \t\t";
-			$var .= var_export($configValues[$_configOption], true);
-			$var .= ";\n";
-			fwrite($fp, $var);
-		} else
-	        fwrite($fp, "\$configValues['" . $_configOption . "'] = '" . $configValues[$_configOption] . "';\n");
-	}
-	fwrite($fp, "\n\n?>");
-	fclose($fp);
-	$successMsg = "Updated database settings for configuration file";
-} else {
-	$failureMsg = "Could not open the file for writing: <b>$configFile</b>
-	<br/>Check file permissions. The file should be writable by the webserver's user/group";
+//
+// generating file contents
+//
+
+// 1. open
+$fileContents = <<<EOL
+<?php
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@enginx.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ * 
+ * Description:          daloRADIUS Configuration File
+ *
+ * Modification Date:    {$date}
+ *
+ *********************************************************************************************************
+ */
+
+// prevent this file to be directly accessed
+if (strpos(\$_SERVER['PHP_SELF'], '/library/daloradius.conf.php') !== false) {
+    header("Location: ../index.php");
+    exit;
 }
+
+
+EOL;
+
+// 2. body
+foreach ($configValues as $_configOption => $_configElem) {
+    $fileContents .= sprintf("\$configValues['%s'] =", $_configOption);
+    
+    if (is_array($configValues[$_configOption])) {
+        $fileContents .= str_repeat(" ", 8) . sprintf("%s;\n", var_export($configValues[$_configOption], true));
+    } else {
+        $fileContents .= sprintf(" '%s';\n", addslashes($configValues[$_configOption]));
+    }
+}
+
+// 3. close
+$fileContents .= <<<EOL
+
+?>
+
+EOL;
+
+//
+// putting contents into file
+//
+$writtenBytes = intval(file_put_contents($configFile, $fileContents));
+
+if ($writtenBytes > 0) {
+    $successMsg = "Configuration file has been successfully updated";
+} else {
+    $failureMsg = sprintf("Could not open the file for writing: <strong>%s</strong>", $configFile)
+                . "<br>Check file permissions. The file should be writable by the webserver's user/group";
+} 
 
 ?>
