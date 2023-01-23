730510556ea313ad69da914e1f73e7b45614c2e8
Filippo Lauria <filippo.lauria@iit.cnr.it>
2023-01-23 12:34:41 +0100

    removed deprecated function

--- library/exten-radius_server_info.php
+++ library/exten-radius_server_info.php
@@ -14,12 +14,12 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  *********************************************************************************************************
- * Description:
- *        this script uses pgrep to check if services stored
- *              in $services_to_check are running
+ * 
+ * Description:    this script uses pgrep to check if services stored
+ *                 in $services_to_check are running
  *
- * Authors:    Liran Tal <liran@enginx.com>
- *              Filippo Lauria <filippo.lauria@iit.cnr.it>
+ * Authors:        Liran Tal <liran@enginx.com>
+ *                 Filippo Lauria <filippo.lauria@iit.cnr.it>
  *
  *********************************************************************************************************
  */
@@ -30,28 +30,38 @@ if (strpos($_SERVER['PHP_SELF'], '/library/exten-radius_server_info.php') !== fa
     exit;
 }
 
-// given the $service_name, this function returns "Running" if that service is running
+// given the $service_name, this function returns true if that service is running
 function check_service($service_name) {
     if (empty($service_name)) {
-        return "no service name";
+        return false;
     }
 
     $command = sprintf("pgrep %s", escapeshellarg($service_name));
     exec($command, $output, $result_code);
-    return ($result_code === 0) ? "Running" : "Not running";
+    return $result_code === 0;
 }
 
-$services_to_check = array("FreeRADIUS", "MySQL", "MariaDB");
+$services_to_check = array("FreeRADIUS", "MySQL", "MariaDB", "SSHd");
 
 ?>
 
 <h3>Service Status</h3>
 <table class="summarySection">
 <?php
-    $format = '<tr><td class="summaryKey">%s</td><td class="summaryValue"><span class="sleft">%s'
-            . "</span></td></tr>\n";
+
+    $format = <<<EOF
+<tr>
+    <td class="summaryKey">%s</td>
+    <td class="summaryValue"><span class="sleft" style="color: %s">%s</span></td>
+</tr>
+EOF;
+    
     foreach ($services_to_check as $service_name) {
-        printf($format, $service_name, check_service(strtolower($service_name)));
+        $running = check_service(strtolower($service_name));
+        $color = ($running) ? "green" : "red";
+        $label = ($running) ? "running" : "not running";
+        
+        printf($format, $service_name, $color, $label);
     }
 ?>
 </table>
