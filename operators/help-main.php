33e7f75bdbd0dce1b0f4a7af58bdd200a5b86057
Filippo Lauria <filippo.lauria@iit.cnr.it>
2023-03-01 14:53:20 +0100

    added new installation guide and changed help page

--- operators/help-main.php
+++ operators/help-main.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  *********************************************************************************************************
  * daloRADIUS - RADIUS Web Platform
@@ -28,42 +28,34 @@
     $log = "visited page: ";
 
     include_once("lang/main.php");
-    
+
     include("../common/includes/layout.php");
 
     // print HTML prologue
     $title = "Help";
     $help = "";
-    
-    print_html_prologue($title, $langCode);
 
-    
-    
+    print_html_prologue($title, $langCode);
 
     print_title_and_help($title, $help);
 
 ?>
 
-            <p>
-                There are several communication media available at your disposal:<br/><br/>
-
-                <b>daloRADIUS website</b>: <a target="_blank" href="http://www.daloradius.com">daloRADIUS blog</a><br/><br/>
-                <b>daloRADIUS Project at GitHub</b>: <a target="_blank" href="https://github.com/lirantal/daloradius">GitHub project</a><br/>
-                At GitHub you may find the trackers for submitting tickets for support, bugs or features for next releases.<br/>
-                The official daloRADIUS package is available at
-                GitHub as well.<br/><br/>
-                <b>daloRADIUS Project at SourceForge</b>: <a target="_blank" href="http://sourceforge.net/projects/daloradius/">SourceForge project</a><br/>
-                Forums and the mailing list archive to review and search for issues.<br/>
-                The daloRADIUS packages here are old, use the GitHub ones instead.<br/><br/>
-                <b>daloRADIUS Mailing List</b>: Email to daloradius-users@lists.sourceforge.net, though registration to the mailing list<br/>
-                is required first <a target="_blank" href="https://lists.sourceforge.net/lists/listinfo/daloradius-users">here</a><br/><br/>
-
-                <b>daloRADIUS IRC</b>: you can find us at #daloradius on irc.freenode.net
-            </p>
+    <p>There are several communication media available at your disposal:</p>
+    <ul>
+        <li><p><strong>daloRADIUS website:</strong> <a class="text-decoration-none" href="http://www.daloradius.com" target="_blank">daloRADIUS blog</a>.</p></li>
+        <li><p><strong>daloRADIUS Project on GitHub:</strong> <a class="text-decoration-none" href="https://github.com/lirantal/daloradius" target="_blank">GitHub project</a>.<br>
+        At GitHub, you can find the trackers for submitting tickets for support, bugs, or features for the next release.<br>
+        The official daloRADIUS package is also available on GitHub.</p></li>
+        <li><p><strong>daloRADIUS Project on SourceForge:</strong> <a class="text-decoration-none" href="http://sourceforge.net/projects/daloradius/" target="_blank">SourceForge project</a>.<br>
+        You can use forums and the mailing list archive to review and search for issues.<br>
+        However, the daloRADIUS packages here are old, so it is better to use the ones on GitHub.</p></li>
+        <li><p><strong>daloRADIUS Mailing List:</strong> you can send an email to <a class="text-decoration-none" href="mailto:daloradius-users@lists.sourceforge.net">daloradius-users@lists.sourceforge.net</a>,<br>
+        but registration to the mailing list is required first. You can register <a class="text-decoration-none" href="https://lists.sourceforge.net/lists/listinfo/daloradius-users" target="_blank">here</a>.</p></li>
+        <li><strong>daloRADIUS IRC:</strong> You can find us at <a class="text-decoration-none" href="irc://irc.freenode.net/daloradius" target="_blank">#daloradius</a> on irc.freenode.net.</li>
+    </ul>
 
 <?php
 
     include('include/config/logging.php');
     print_footer_and_html_epilogue();
-
-?>
