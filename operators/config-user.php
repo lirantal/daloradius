8fad3bb9d30b0024da5f6e752cf7b181cdc96eeb
Filippo Lauria <filippo.lauria@iit.cnr.it>
2023-03-10 16:27:59 +0100

    password max/min length configuration

--- operators/config-user.php
+++ operators/config-user.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  *********************************************************************************************************
  * daloRADIUS - RADIUS Web Platform
@@ -26,21 +26,21 @@
 
     include('../common/includes/config_read.php');
     include('library/check_operator_perm.php');
-    
+
     include_once("lang/main.php");
     include("../common/includes/validation.php");
     include("../common/includes/layout.php");
-    
+
     // init logging variables
     $log = "visited page: ";
     $logAction = "";
     $logDebugSQL = "";
-    
+
     $invalid_input = array();
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (array_key_exists('csrf_token', $_POST) && isset($_POST['csrf_token']) && dalo_check_csrf_token($_POST['csrf_token'])) {
-            
+
             // validate allow Cleartext-Password attributes
             if (
                     array_key_exists('CONFIG_DB_PASSWORD_ENCRYPTION', $_POST) &&
@@ -51,7 +51,7 @@
             } else {
                 $invalid_input['CONFIG_DB_PASSWORD_ENCRYPTION'] = "Allow cleartext password in db";
             }
-            
+
             // validate allowed random characters
             if (
                     array_key_exists('CONFIG_USER_ALLOWEDRANDOMCHARS', $_POST) &&
@@ -62,14 +62,29 @@
             } else {
                 $invalid_input['CONFIG_USER_ALLOWEDRANDOMCHARS'] = t('all','RandomChars');
             }
-            
+
+            if (isset($_POST['CONFIG_DB_PASSWORD_MIN_LENGTH']) && intval($_POST['CONFIG_DB_PASSWORD_MIN_LENGTH']) > 0) {
+                $configValues['CONFIG_DB_PASSWORD_MIN_LENGTH'] = intval($_POST['CONFIG_DB_PASSWORD_MIN_LENGTH']);
+
+                if (isset($_POST['CONFIG_DB_PASSWORD_MAX_LENGTH']) &&
+                    intval($_POST['CONFIG_DB_PASSWORD_MAX_LENGTH']) > $configValues['CONFIG_DB_PASSWORD_MIN_LENGTH']) {
+                    $configValues['CONFIG_DB_PASSWORD_MAX_LENGTH'] = intval($_POST['CONFIG_DB_PASSWORD_MAX_LENGTH']);
+                } else {
+                    $configValues['CONFIG_DB_PASSWORD_MAX_LENGTH'] = $configValues['CONFIG_DB_PASSWORD_MIN_LENGTH'] + 4;
+                }
+
+            } else {
+                $invalid_input['CONFIG_DB_PASSWORD_MAX_LENGTH'] = "Password max length";
+                $invalid_input['CONFIG_DB_PASSWORD_MIN_LENGTH'] = "Password min length";
+            }
+
             if (count($invalid_input) > 0) {
                 $failureMsg = sprintf("Invalid input: [%s]", implode(", ", array_values($invalid_input)));
                 $logAction .= "$failureMsg on page: ";
             } else {
                 include("../common/includes/config_write.php");
             }
-            
+
         } else {
             // csrf
             $failureMsg = "CSRF token error";
@@ -77,26 +92,23 @@
         }
     }
 
-    
+
     // print HTML prologue
     $title = t('Intro','configuser.php');
     $help = t('helpPage','configuser');
-    
+
     print_html_prologue($title, $langCode);
-
-    
-
 
     print_title_and_help($title, $help);
 
     include_once('include/management/actionMessages.php');
-    
+
     $fieldset0_descriptor = array(
                                     "title" => t('title','Settings')
                                  );
-                                 
+
     $input_descriptors0 = array();
-    
+
     $input_descriptors0[] = array(
                                     "type" => "select",
                                     "options" => array( "yes", "no" ),
@@ -114,6 +126,22 @@
                                      );
 
     $input_descriptors0[] = array(
+                                        "name" => "CONFIG_DB_PASSWORD_MIN_LENGTH",
+                                        "caption" => "Password min length",
+                                        "value" => $configValues['CONFIG_DB_PASSWORD_MIN_LENGTH'],
+                                        "type" => "number",
+                                        "min" => "1",
+                                     );
+
+    $input_descriptors0[] = array(
+                                        "name" => "CONFIG_DB_PASSWORD_MAX_LENGTH",
+                                        "caption" => "Password max length",
+                                        "value" => $configValues['CONFIG_DB_PASSWORD_MAX_LENGTH'],
+                                        "type" => "number",
+                                        "min" => "2",
+                                     );
+
+    $input_descriptors0[] = array(
                                     "name" => "csrf_token",
                                     "type" => "hidden",
                                     "value" => dalo_csrf_token(),
@@ -126,20 +154,20 @@
                                  );
 
     open_form();
-    
+
     // open 0-th fieldset
     open_fieldset($fieldset0_descriptor);
-    
+
     foreach ($input_descriptors0 as $input_descriptor) {
         print_form_component($input_descriptor);
     }
-    
+
     close_fieldset();
-    
+
     close_form();
-    
+
     include('include/config/logging.php');
-    
+
     print_footer_and_html_epilogue();
 
-?>        
+?>
