e4064c9695d7c4eaec2abcb7b516402aca79e08f
mmsystems <mmsysmail@gmail.com>
2025-01-16 17:07:10 +0100

    Fix CSRF token validation in NAS deletion operation (#592)
    Co-authored-by: root <root@FREERADIUS.abi.ute>

--- app/operators/mng-rad-nas-list.php
+++ app/operators/mng-rad-nas-list.php
@@ -184,7 +184,7 @@
                                 'multiple_pages' => $drawNumberLinks
                            );
 
-        $descriptor = array( 'table_foot' => $table_foot );
+        $descriptor = array( 'form' => $form_descriptor, 'table_foot' => $table_foot );
         print_table_bottom($descriptor);
 
         // get and print "links"
