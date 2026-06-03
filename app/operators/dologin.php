6b0fec861a0c06401d732bef55daa5342c6f0daa
Kevin LOUS <kevin.lous29@gmail.com>
2026-06-03 23:29:41 +0200

    fix: restore operator login license header

--- app/operators/dologin.php
+++ app/operators/dologin.php
@@ -8,6 +8,19 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *******************************************************************************
+ * Description:
+ * 		performs the logging-in authorization. First creates a random
+ *      session_id to be assigned to this session and then validates
+ *      the operators credentials in the database
+ *
+ * Authors:	Liran Tal <liran@lirantal.com>
+ *
  *******************************************************************************
  */
 
