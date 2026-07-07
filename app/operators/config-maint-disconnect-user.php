6cac0dc283334df1dbebd0f131ebe660c55469e8
Abdul Malik <doel.malik@outlook.com>
2026-07-07 08:41:52 +0700

    Refactor user disconnection logic and UI components

--- app/operators/config-maint-disconnect-user.php
+++ app/operators/config-maint-disconnect-user.php
@@ -150,7 +150,7 @@
                 $failureMsg = "CSRF token error";
                 $logAction .= "$failureMsg on page: ";
             }
-        } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') { //input from online user page
             $username = (isset($_GET['username']) && !empty(trim($_GET['username']))) ? trim($_GET['username']) : "";
             $packetType = (isset($_GET['username']) && !empty(trim($_GET['username']))) ? "disconnect" : "";
             $nas_id = (isset($_GET['nasaddr']) && !empty(trim($_GET['nasaddr']))) ? trim($_GET['nasaddr']) : "";
