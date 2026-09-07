efeb8b84fec7504c3e7494017cc039c69af0c40b
filippolauria <filippo.lauria@iit.cnr.it>
2026-09-07 15:29:23 +0200

    fix(gis): replace API-key basemap and bump Leaflet to 1.9.4

--- app/operators/gis-viewmap.php
+++ app/operators/gis-viewmap.php
@@ -21,54 +21,38 @@
  *********************************************************************************************************
  */
 
-    include("library/checklogin.php");
+    include_once implode(DIRECTORY_SEPARATOR, [ __DIR__, '..', 'common', 'includes', 'config_read.php' ]);
+    include implode(DIRECTORY_SEPARATOR, [ $configValues['OPERATORS_LIBRARY'], 'checklogin.php' ]);
     $operator = $_SESSION['operator_user'];
 
-    include('library/check_operator_perm.php');
-    include_once('../common/includes/config_read.php');
+    include implode(DIRECTORY_SEPARATOR, [ $configValues['OPERATORS_LIBRARY'], 'check_operator_perm.php' ]);
 
-    include_once("lang/main.php");
-    include_once("../common/includes/validation.php");
-    include("../common/includes/layout.php");
-    
+    include_once implode(DIRECTORY_SEPARATOR, [ $configValues['OPERATORS_LANG'], 'main.php' ]);
+    include implode(DIRECTORY_SEPARATOR, [ $configValues['COMMON_INCLUDES'], 'validation.php' ]);
+    include implode(DIRECTORY_SEPARATOR, [ $configValues['COMMON_INCLUDES'], 'layout.php' ]);
+
     // init logging variables
     $log = "visited page: ";
     $logAction = "";
     $logDebugSQL = "";
 
-
     // print HTML prologue
     $title = t('Intro','gisviewmap.php');
     $help = t('helpPage','gisviewmap');
-    
-    $extra_css = array("https://unpkg.com/leaflet@1.9.3/dist/leaflet.css");
-    
+
+    $extra_css = array("https://unpkg.com/leaflet@1.9.4/dist/leaflet.css");
+
     // loaded at the bottom of the page
-    $extra_js = array("https://unpkg.com/leaflet@1.9.3/dist/leaflet.js");
+    $extra_js = array("https://unpkg.com/leaflet@1.9.4/dist/leaflet.js");
 
     print_html_prologue($title, $langCode, $extra_css);
-    
+
     print_title_and_help($title, $help);
-    
-    
+
     // print map div
     echo '<div id="map" style="width: 800px; height: 600px; margin: 20px auto"></div>' . "\n";
 
-    $inline_extra_js = <<<EOF
-window.onload = function() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    var group = L.featureGroup().addTo(map);
-
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20
-    }).addTo(map);
-    
-    
-EOF;
-    
-    include('../common/includes/db_open.php');
+    include implode(DIRECTORY_SEPARATOR, [ $configValues['COMMON_INCLUDES'], 'db_open.php' ]);
 
     $sql = sprintf("SELECT id, name, mac, geocode
                       FROM %s
@@ -76,15 +60,24 @@ EOF;
     $res = $dbSocket->query($sql);
     $logDebugSQL .= "$sql;\n";
 
+    $markers_js = "";
+    $first_geocode = "";
+    $marker_count = 0;
+
     while ($row = $res->fetchRow()) {
         $rowlen = count($row);
-        
+
         for ($i = 0; $i < $rowlen; $i++) {
             $row[$i] = htmlspecialchars($row[$i], ENT_QUOTES, 'UTF-8');
         }
-        
+
         list($id, $name, $mac, $geocode) = $row;
-        
+
+        if ($marker_count === 0) {
+            $first_geocode = $geocode;
+        }
+        $marker_count++;
+
         $popup = sprintf('<strong>Hotspot Name</strong>: %s<br>'
                        . '<strong>MAC Addr</strong>: %s<br>'
                        . '<strong>Geocode</strong>: %s<br>'
@@ -92,24 +85,40 @@ EOF;
                        . '<br>'
                        . '<a href="acct-hotspot-accounting.php?hotspot[]=%s">%s</a>',
                          $name, $mac, $geocode, t('Intro','accthotspotcompare.php'), $name, t('Intro','accthotspot.php'));
-        
-        
+
+
         // Now, create a simple popup.
         // The original program provided a tabbed popup.
-        $inline_extra_js .= sprintf("L.marker([%s]).addTo(group).bindTooltip('%s').bindPopup('%s');",
-                                    $geocode, $name, $popup);
+        $markers_js .= sprintf("L.marker([%s]).addTo(group).bindTooltip('%s').bindPopup('%s');",
+                               $geocode, $name, $popup);
     }
-    
-    include('../common/includes/db_close.php');
-    
-    $inline_extra_js .= <<<EOF
 
-    map.fitBounds(group.getBounds());
-}
+    include implode(DIRECTORY_SEPARATOR, [ $configValues['COMMON_INCLUDES'], 'db_close.php' ]);
+
+    // center on the first available hotspot coordinate, otherwise fall back to a default view
+    $default_geocode = "[43.71805, 10.42284]";
+    $map_center = ($marker_count > 0) ? "[{$first_geocode}]" : $default_geocode;
+
+    $inline_extra_js = <<<EOF
+window.onload = function() {
+    var map = L.map('map').setView({$map_center}, 16);
+    var group = L.featureGroup().addTo(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abc',
+        maxZoom: 19
+    }).addTo(map);
 
 EOF;
-    
-    include('include/config/logging.php');
+
+    $inline_extra_js .= $markers_js;
+
+    // when more than one hotspot is mapped, zoom to fit them all
+    if ($marker_count > 1) {
+        $inline_extra_js .= "\n    map.fitBounds(group.getBounds());\n";
+    }
+    $inline_extra_js .= "}\n";
+
+    include implode(DIRECTORY_SEPARATOR, [ $configValues['OPERATORS_INCLUDE_CONFIG'], 'logging.php' ]);
     print_footer_and_html_epilogue($inline_extra_js, $extra_js);
-    
-?>
