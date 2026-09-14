e672f8f65a1610528efe76a670934083ad3dc08e
filippolauria <filippo.lauria@iit.cnr.it>
2026-09-14 10:34:13 +0200

    chore: align acct_maintenance.php header with project convention

--- app/operators/library/acct_maintenance.php
+++ app/operators/library/acct_maintenance.php
@@ -1,7 +1,27 @@
 <?php
-/* Open-session maintenance: bounded previews and optimistic, per-row writes.
- * GPL-2.0-or-later. No NAS disconnection or inactivity detection is performed.
+/*
+ *********************************************************************************************************
+ * daloRADIUS - RADIUS Web Platform
+ * Copyright (C) 2007 - Liran Tal <liran@lirantal.com> All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *********************************************************************************************************
+ * Description:    Open-session maintenance: bounded previews and optimistic, per-row writes.
+ *                 No NAS disconnection or inactivity detection is performed.
+ * 
+ * Authors:    Kevin Lous
+ *
+ *********************************************************************************************************
  */
+
 const DALO_MAINTENANCE_LIMIT = 100;
 const DALO_MAINTENANCE_TTL = 600;
 
